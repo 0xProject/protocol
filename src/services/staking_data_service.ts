790ae56ce69e6f34f012476f2cce9794238effc5
fragosti <francesco.agosti93@gmail.com>
2019-11-19 11:09:25 -0800

    Remove long comment

--- src/services/staking_data_service.ts
+++ src/services/staking_data_service.ts
@@ -4,74 +4,7 @@ import { Connection } from 'typeorm';
 import { Epoch, Pool, PoolWithStats, RawEpoch, RawPool } from '../types';
 import { stakingUtils } from '../utils/staking_utils';
 import { utils } from '../utils/utils';
-// {
-//     currentEpoch: {
-//         epoch_id: number,
-//         epochStart: {
-//             startingTimestamp: timestamp,
-//             startingBlockNumber: number,
-//             startingTxHash: string,
-//         },
-//     },
-//     nextEpoch: {
-//         epoch_id: number,
-//         approximateStart: {
-//             startingTimestamp: timestamp,
-//             startingBlockNumber: number,
-//         },
-//     },
-//     poolInfo: [
-//         {
-//             poolId: string,
-//             operatorAddress: string,
-//             created: {
-//                 createdTimestamp: timestamp,
-//                 createdBlockNumber: number,
-//                 createdTxHash: string,
-//             },
-//             metadata: {
-//                 verified: boolean,
-//                 logoUrl?: string,
-//                 location?: string,
-//                 bio?: string,
-//                 websiteUrl?: string,
-//                 name?: string
-//             },
-//             forNextEpoch: {
-//                 epochId: number,
-//                 zrxStaked: number,
-//                 operatorShare: number,
-//                 approximateStakeRatio: number,
-//                 makerAddressesSet: string[],
-//                 delegators: [
-//                     {
-//                         delegatorAddress: string,
-//                         isOperator: boolean,
-//                         stakingPoolOwnershipShare: number,
-//                     }
-//                     ...
-//                 ]
-//             },
-//             forCurrentEpoch: {
-//                 epochId: number,
-//                 zrxStaked: number,
-//                 operatorShare: number,
-//                 makerAddressesSet: string[],
-//                 protocolFeesGeneratedInEth: number
-//             },
-//             historical: [
-//                 {
-//                     epochId: number,
-//                     operatorRewardInEth: number,
-//                     membersRewardInEth: number,
-//                     totalRewardInEth: number,
-//                 }
-//                 ...
-//             ],
-//         }
-//         ...
-//     ]
-// }
+
 export class StakingDataService {
     private readonly _connection: Connection;
     constructor(connection: Connection) {
