cd2d093f3428f042649b762315da6acc915424aa
Xianny <8582774+xianny@users.noreply.github.com>
2020-04-15 14:55:10 -0700

    Normalize addresses to lowercase (#155)
    * normalize addresses to lowercase
    
    * move address normalization to middleware
    
    * update test
    
    * use address normalizer in all runners
    
    * remove unnecessary `toLowerCase()`

--- test/app_test.ts
+++ test/app_test.ts
@@ -6,10 +6,12 @@ import * as HttpStatus from 'http-status-codes';
 import 'mocha';
 import * as request from 'supertest';
 
-import { getAppAsync, getDefaultAppDependenciesAsync } from '../src/app';
+import { AppDependencies, getAppAsync, getDefaultAppDependenciesAsync } from '../src/app';
 import * as config from '../src/config';
 import { DEFAULT_PAGE, DEFAULT_PER_PAGE, SRA_PATH } from '../src/constants';
+import { SignedOrderEntity } from '../src/entities';
 
+import * as orderFixture from './fixtures/order.json';
 import { expect } from './utils/expect';
 
 let app: Express.Application;
@@ -19,9 +21,10 @@ let provider: Web3ProviderEngine;
 let accounts: string[];
 let blockchainLifecycle: BlockchainLifecycle;
 
+let dependencies: AppDependencies;
 describe('app test', () => {
     before(async () => {
-        // start ganache and run contract migrations
+        // connect to ganache and run contract migrations
         const ganacheConfigs = {
             shouldUseInProcessGanache: false,
             shouldAllowUnlimitedContractSize: true,
@@ -35,7 +38,7 @@ describe('app test', () => {
         const owner = accounts[0];
         await runMigrationsOnceAsync(provider, { from: owner });
 
-        const dependencies = await getDefaultAppDependenciesAsync(provider, config);
+        dependencies = await getDefaultAppDependenciesAsync(provider, config);
         // start the 0x-api app
         app = await getAppAsync({ ...dependencies }, config);
     });
@@ -53,5 +56,32 @@ describe('app test', () => {
                 expect(response.body.total).to.equal(0);
                 expect(response.body.records).to.deep.equal([]);
             });
+    });
+    it('should normalize addresses to lowercase', async () => {
+        const metaData = {
+            hash: '123',
+            remainingFillableTakerAssetAmount: '1',
+        };
+        const expirationTimeSeconds = (new Date().getTime() / 1000 + 600).toString(); // tslint:disable-line:custom-no-magic-numbers
+        const orderModel = new SignedOrderEntity({
+            ...metaData,
+            ...orderFixture,
+            expirationTimeSeconds,
+        });
+
+        const apiOrderResponse = { chainId: config.CHAIN_ID, ...orderFixture, expirationTimeSeconds };
+        await dependencies.connection.manager.save(orderModel);
+        await request(app)
+            .get(`${SRA_PATH}/orders?makerAddress=${orderFixture.makerAddress.toUpperCase()}`)
+            .expect('Content-Type', /json/)
+            .expect(HttpStatus.OK)
+            .then(response => {
+                expect(response.body.perPage).to.equal(DEFAULT_PER_PAGE);
+                expect(response.body.page).to.equal(DEFAULT_PAGE);
+                expect(response.body.total).to.equal(1);
+                expect(response.body.records[0].order).to.deep.equal(apiOrderResponse);
+            });
+
+        await dependencies.connection.manager.remove(orderModel);
     });
 });
