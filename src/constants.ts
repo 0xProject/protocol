c7ec0ff80d13d141dbc062b88ef5a97fd5b387a3
Francesco Agosti <francesco@0xproject.com>
2020-05-20 13:07:24 -0700

    feat: lower default slippage percentage to 1% (#238)

--- src/constants.ts
+++ src/constants.ts
@@ -24,7 +24,7 @@ export const MESH_ORDERS_BATCH_HTTP_BYTE_LENGTH = 2500000;
 // Swap Quoter
 export const QUOTE_ORDER_EXPIRATION_BUFFER_MS = ONE_SECOND_MS * 90; // Ignore orders that expire in 90 seconds
 export const GAS_LIMIT_BUFFER_PERCENTAGE = 0.2; // Add 20% to the estimated gas limit
-export const DEFAULT_QUOTE_SLIPPAGE_PERCENTAGE = 0.03; // 3% Slippage
+export const DEFAULT_QUOTE_SLIPPAGE_PERCENTAGE = 0.01; // 1% Slippage
 export const DEFAULT_FALLBACK_SLIPPAGE_PERCENTAGE = 0.015; // 1.5% Slippage in a fallback route
 export const ETH_SYMBOL = 'ETH';
 export const WETH_SYMBOL = 'WETH';
