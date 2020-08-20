d295b1d6efe90672d06f98f1b30fbd90fc155b16
Daniel Pyrathon <pirosb3@gmail.com>
2020-08-19 22:35:53 -0700

    update asset swapper and re-enable logs (#321)

--- src/services/swap_service.ts
+++ src/services/swap_service.ts
@@ -76,12 +76,8 @@ export class SwapService {
             ...SWAP_QUOTER_OPTS,
             rfqt: {
                 ...SWAP_QUOTER_OPTS.rfqt,
-                // tslint:disable-next-line:no-empty
-                warningLogger: () => {},
-                // tslint:disable-next-line:no-empty
-                infoLogger: () => {},
-                // warningLogger: logger.warn.bind(logger),
-                // infoLogger: logger.info.bind(logger),
+                warningLogger: logger.warn.bind(logger),
+                infoLogger: logger.info.bind(logger),
             },
             contractAddresses,
         };
