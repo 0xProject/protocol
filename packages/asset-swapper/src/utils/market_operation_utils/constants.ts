6d6b1d0ed35c578c5833fde128da567673e20223
Jacob Evans <jacob@dekz.net>
2020-11-27 12:00:26 +1000

    Deploy CryptoCom bridge

--- packages/asset-swapper/src/utils/market_operation_utils/constants.ts
+++ packages/asset-swapper/src/utils/market_operation_utils/constants.ts
@@ -418,7 +418,7 @@ export const BRIDGE_ADDRESSES_BY_CHAIN: { [chainId in ChainId]: BridgeContractAd
         creamBridge: '0xb9d4bf2c8dab828f4ffb656acdb6c2b497d44f25',
         swerveBridge: '0xf9786d5eb1de47fa56a8f7bb387653c6d410bfee',
         snowswapBridge: '0xb1dbe83d15236ec10fdb214c6b89774b454754fd',
-        cryptoComBridge: '0xcccccccccccccccccccccccccccccccccccccccc',
+        cryptoComBridge: '0x015850307f6aab4ac6631923ceefe71b57492c9b',
     },
     [ChainId.Kovan]: {
         ...EMPTY_BRIDGE_ADDRESSES,
