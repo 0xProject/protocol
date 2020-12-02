502a5944dd15d607a776ed3b6bbd0240e06286f7
Alex Kroeger <alexrkroeger@gmail.com>
2020-12-02 10:15:43 -0800

    Upgraded to solidity 0.6, simplified contract, added tests

--- packages/asset-swapper/contracts/src/BalanceChecker.sol
+++ packages/asset-swapper/contracts/src/BalanceChecker.sol
@@ -16,51 +16,25 @@
 
 */
 
-pragma solidity ^0.5.9;
+pragma solidity ^0.6;
 
 // ERC20 contract interface
-contract IToken {
+abstract contract IToken {
     /// @dev Query the balance of owner
     /// @param _owner The address from which the balance will be retrieved
     /// @return Balance of owner
-    function balanceOf(address _owner) public view returns (uint);
+    function balanceOf(address _owner) public virtual view returns (uint256);
 
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) public view returns (uint);
+    function allowance(address _owner, address _spender) public virtual view returns (uint256);
 }
 
 contract BalanceChecker {
-    /* Fallback function, don't accept any ETH */
-    function() external payable {
-      revert("BalanceChecker does not accept payments");
-    }
-
-    /*
-      Check the token balance of a wallet in a token contract
-
-      Returns the balance of the token for user. Avoids possible errors:
-        - return 0 on non-contract address 
-        - returns 0 if the contract doesn't implement balanceOf
-    */
-    function tokenBalance(address user, address token) public view returns (uint) {
-        // check if token is actually a contract
-        uint256 tokenCode;
-        assembly { tokenCode := extcodesize(token) } // contract code size
-
-        // checks that it's a contract
-        // TODO: check function exists on contract
-        if (tokenCode > 0) {  
-            return IToken(token).balanceOf(user);
-        } else {
-            return 0;
-        }
-    }
-
     /*
       Check the token balances of wallet-token pairs.
-      Pass 0x0 as a "token" address to get ETH balance.
+      Pass 0xeee... as a "token" address to get ETH balance.
       Possible error throws:
         - extremely large arrays for user and or tokens (gas cost too high)
 
@@ -70,11 +44,11 @@ contract BalanceChecker {
         // make sure the users array and tokens array are of equal length
         require(users.length == tokens.length, "users array is a different length than the tokens array");
 
-        uint[] memory addrBalances = new uint[](users.length);
+        uint256[] memory addrBalances = new uint256[](users.length);
 
         for(uint i = 0; i < users.length; i++) {
-            if (tokens[i] != address(0x0)) {
-                addrBalances[i] = tokenBalance(users[i], tokens[i]);
+            if (tokens[i] != address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE)) {
+                addrBalances[i] = IToken(tokens[i]).balanceOf(users[i]);
             } else {
                 addrBalances[i] = users[i].balance; // ETH balance
             }
@@ -84,45 +58,24 @@ contract BalanceChecker {
     }
 
     /*
-      Check the allowance for a given owner, spender, and token
-
-      Returns the balance of the token for user. Avoids possible errors:
-        - return 0 on non-contract address 
-        - returns 0 if the contract doesn't implement allowance function
-    */
-    function allowance(address owner, address spender, address token) public view returns (uint) {
-        // check if token is actually a contract
-        uint256 tokenCode;
-        assembly { tokenCode := extcodesize(token) } // contract code size
-
-        // checks that it's a contract
-        // TODO: check function exists on contract
-        if (tokenCode > 0) {  
-            return IToken(token).allowance(owner, spender);
-        } else {
-            return 0;
-        }
-    }
-
-    /*
       Check the allowances of an array of owner-spender-tokens
       
-      Returns 0 for 0x0
+      Returns 0 for 0xeee... (ETH)
       Possible error throws:
         - extremely large arrays for user and or tokens (gas cost too high)
 
       Returns a one-dimensional array that's owners.length long.
     */
-    function allowances(address[] calldata owners, address[] calldata spenders, address[] calldata tokens) external view returns (uint[] memory) {
+    function allowances(address[] calldata owners, address[] calldata spenders, address[] calldata tokens) external view returns (uint256[] memory) {
         // make sure the arrays are all of equal length
         require(owners.length == spenders.length, "all arrays must be of equal length");
         require(owners.length == tokens.length, "all arrays must be of equal length");
 
-        uint[] memory addrAllowances = new uint[](owners.length);
+        uint256[] memory addrAllowances = new uint256[](owners.length);
 
         for(uint i = 0; i < owners.length; i++) {
-            if (tokens[i] != address(0x0)) {
-                addrAllowances[i] = allowance(owners[i], spenders[i], tokens[i]);
+            if (tokens[i] != address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE)) {
+                addrAllowances[i] = IToken(tokens[i]).allowance(owners[i], spenders[i]);
             } else {
                 // ETH
                 addrAllowances[i] = 0;
---
+++ packages/asset-swapper/test/contracts/balance_checker_test.ts
@@ -0,0 +1,78 @@
+import { artifacts as erc20Artifacts, DummyERC20TokenContract } from '@0x/contracts-erc20';
+import {
+    blockchainTests,
+    constants,
+    expect,
+    web3Wrapper,
+} from '@0x/contracts-test-utils';
+import { BigNumber } from '@0x/utils';
+import * as _ from 'lodash';
+
+import { artifacts } from '../artifacts';
+import { BalanceCheckerContract, TestNativeOrderSamplerContract } from '../wrappers';
+
+// tslint:disable: custom-no-magic-numbers
+
+const ETH_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+
+blockchainTests.resets('BalanceChecker contract', env => {
+    let contract: BalanceCheckerContract;
+    let testContract: TestNativeOrderSamplerContract;
+    let makerToken: string;
+
+    before(async () => {
+        testContract = await TestNativeOrderSamplerContract.deployFrom0xArtifactAsync(
+            artifacts.TestNativeOrderSampler,
+            env.provider,
+            env.txDefaults,
+            {},
+        );
+        contract = await BalanceCheckerContract.deployFrom0xArtifactAsync(
+            artifacts.BalanceChecker,
+            env.provider,
+            env.txDefaults,
+            {},
+        );
+        const NUM_TOKENS = new BigNumber(1);
+        [makerToken] = await testContract.createTokens(NUM_TOKENS).callAsync();
+        await testContract.createTokens(NUM_TOKENS).awaitTransactionSuccessAsync();
+    });
+
+    describe('getBalances', () => {
+        it('returns the correct array for a successful call', async () => {
+            const newMakerToken = await DummyERC20TokenContract.deployFrom0xArtifactAsync(
+                erc20Artifacts.DummyERC20Token,
+                env.provider,
+                env.txDefaults,
+                artifacts,
+                constants.DUMMY_TOKEN_NAME,
+                constants.DUMMY_TOKEN_SYMBOL,
+                new BigNumber(18),
+                constants.DUMMY_TOKEN_TOTAL_SUPPLY,
+            );
+
+            const accounts = await web3Wrapper.getAvailableAddressesAsync();
+            const owner = accounts[0];
+            const owner2 = accounts[1];
+
+            await newMakerToken.mint(new BigNumber(100)).awaitTransactionSuccessAsync({ from: owner });
+
+            const testResults = await contract.balances([owner, owner2], [newMakerToken.address, ETH_ADDRESS]).callAsync();
+
+            expect(testResults).to.eql([new BigNumber(100), new BigNumber(100000000000000000000)]);
+
+        });
+        it('it throws an error if the input arrays of different lengths', async () => {
+            const accounts = await web3Wrapper.getAvailableAddressesAsync();
+            const owner = accounts[0];
+
+            try {
+                await contract.balances([owner], [ETH_ADDRESS, ETH_ADDRESS]).callAsync();
+                expect.fail();
+            } catch (error) {
+                expect(error.message).to.eql('users array is a different length than the tokens array');
+            }
+        });
+    });
+
+ });
