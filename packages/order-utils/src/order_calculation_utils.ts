1e7b767dab8ca7002375ceb41db1d0d9446a62cc
xianny <xianny@gmail.com>
2021-01-19 11:01:08 -0800

    wip: new order type

--- packages/order-utils/src/order_calculation_utils.ts
+++ packages/order-utils/src/order_calculation_utils.ts
@@ -1,4 +1,4 @@
-import { Order } from '@0x/types';
+import { CommonOrderFields as Order, LimitOrderFields } from '@0x/protocol-utils';
 import { BigNumber } from '@0x/utils';
 
 import { constants } from './constants';
@@ -19,14 +19,14 @@ export const orderCalculationUtils = {
     willOrderExpire(order: Order, secondsFromNow: number): boolean {
         const millisecondsInSecond = 1000;
         const currentUnixTimestampSec = new BigNumber(Date.now() / millisecondsInSecond).integerValue();
-        return order.expirationTimeSeconds.isLessThan(currentUnixTimestampSec.plus(secondsFromNow));
+        return order.expiry.isLessThan(currentUnixTimestampSec.plus(secondsFromNow));
     },
     /**
      * Determines if the order is open and fillable by any taker.
      * @param order The order
      */
     isOpenOrder(order: Order): boolean {
-        return order.takerAddress === constants.NULL_ADDRESS;
+        return order.taker === constants.NULL_ADDRESS;
     },
     /**
      * Given an amount of taker asset, calculate the the amount of maker asset
@@ -36,8 +36,8 @@ export const orderCalculationUtils = {
     getMakerFillAmount(order: Order, takerFillAmount: BigNumber): BigNumber {
         // Round down because exchange rate favors Maker
         const makerFillAmount = takerFillAmount
-            .multipliedBy(order.makerAssetAmount)
-            .div(order.takerAssetAmount)
+            .multipliedBy(order.makerAmount)
+            .div(order.takerAmount)
             .integerValue(BigNumber.ROUND_FLOOR);
         return makerFillAmount;
     },
@@ -49,8 +49,8 @@ export const orderCalculationUtils = {
     getTakerFillAmount(order: Order, makerFillAmount: BigNumber): BigNumber {
         // Round up because exchange rate favors Maker
         const takerFillAmount = makerFillAmount
-            .multipliedBy(order.takerAssetAmount)
-            .div(order.makerAssetAmount)
+            .multipliedBy(order.takerAmount)
+            .div(order.makerAmount)
             .integerValue(BigNumber.ROUND_CEIL);
         return takerFillAmount;
     },
@@ -59,26 +59,13 @@ export const orderCalculationUtils = {
      * @param order The order
      * @param takerFillAmount the amount of taker asset
      */
-    getTakerFeeAmount(order: Order, takerFillAmount: BigNumber): BigNumber {
+    getTakerFeeAmount(order: LimitOrderFields, takerFillAmount: BigNumber): BigNumber {
         // Round down because Taker fee rate favors Taker
         const takerFeeAmount = takerFillAmount
-            .multipliedBy(order.takerFee)
-            .div(order.takerAssetAmount)
+            .multipliedBy(order.takerTokenFeeAmount)
+            .div(order.takerAmount)
             .integerValue(BigNumber.ROUND_FLOOR);
         return takerFeeAmount;
-    },
-    /**
-     * Given an amount of maker asset, calculate the fee amount required for the maker
-     * @param order The order
-     * @param makerFillAmount the amount of maker asset
-     */
-    getMakerFeeAmount(order: Order, makerFillAmount: BigNumber): BigNumber {
-        // Round down because Maker fee rate favors Maker
-        const makerFeeAmount = makerFillAmount
-            .multipliedBy(order.makerFee)
-            .div(order.makerAssetAmount)
-            .integerValue(BigNumber.ROUND_FLOOR);
-        return makerFeeAmount;
     },
     /**
      * Given a desired amount of ZRX from a fee order, calculate the amount of taker asset required to fill.
@@ -86,11 +73,11 @@ export const orderCalculationUtils = {
      * @param order The order
      * @param makerFillAmount the amount of maker asset
      */
-    getTakerFillAmountForFeeOrder(order: Order, makerFillAmount: BigNumber): [BigNumber, BigNumber] {
+    getTakerFillAmountForFeeOrder(order: LimitOrderFields, makerFillAmount: BigNumber): [BigNumber, BigNumber] {
         // For each unit of TakerAsset we buy (MakerAsset - TakerFee)
         const adjustedTakerFillAmount = makerFillAmount
-            .multipliedBy(order.takerAssetAmount)
-            .div(order.makerAssetAmount.minus(order.takerFee))
+            .multipliedBy(order.takerAmount)
+            .div(order.makerAmount.minus(order.takerTokenFeeAmount))
             .integerValue(BigNumber.ROUND_CEIL);
         // The amount that we buy will be greater than makerFillAmount, since we buy some amount for fees.
         const adjustedMakerFillAmount = orderCalculationUtils.getMakerFillAmount(order, adjustedTakerFillAmount);
