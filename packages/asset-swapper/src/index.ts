9bf2960d9aef99a1bc038669b8a7d855c5061c5e
Romain Butteaud <romain.butteaud@gmail.com>
2021-02-08 16:40:34 -0800

    fix: export AffiliateFeeType

--- packages/asset-swapper/src/index.ts
+++ packages/asset-swapper/src/index.ts
@@ -78,6 +78,7 @@ export {
     AltOffering,
     AltRfqtMakerAssetOfferings,
     AffiliateFeeAmount,
+    AffiliateFeeType,
     AssetSwapperContractAddresses,
     CalldataInfo,
     ExchangeProxyContractOpts,
