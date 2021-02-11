5c441e690e012f71f9b2e1e59a9c560dad9fb784
Lawrence Forman <me@merklejerk.com>
2021-02-11 12:40:01 -0500

    `@0x/asset-swapper`: Clean up curve VIP integration

--- packages/asset-swapper/src/quote_consumers/exchange_proxy_swap_quote_consumer.ts
+++ packages/asset-swapper/src/quote_consumers/exchange_proxy_swap_quote_consumer.ts
@@ -442,12 +442,3 @@ function getFQTTransformerDataFromOptimizedOrders(
     }
     return fqtData;
 }
-
-function encodeCurveLiquidityProviderData(fillData: CurveFillData): string {
-    return encodeCurveLiquidityProviderData({
-        curveAddress: fillData.pool.poolAddress,
-        exchangeFunctionSelector: fillData.pool.exchangeFunctionSelector,
-        fromCoinIdx: fillData.fromTokenIdx,
-        toCoinIdx: fillData.toTokenIdx,
-    });
-}
