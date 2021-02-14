5fde9320302f6c5fc1d50080bca1e9332881743a
ReggieNoble <noblereggie0622@gmail.com>
2021-02-14 06:56:39 -0500

    Changes to chore

--- packages/asset-swapper/src/utils/market_operation_utils/shell_utils.ts
+++
@@ -1,8 +0,0 @@
-import { MAINNET_SHELL_POOLS } from './constants';
-
-// tslint:disable completed-docs
-export function getShellsForPair(takerToken: string, makerToken: string): string[] {
-    return Object.values(MAINNET_SHELL_POOLS)
-        .filter(c => [makerToken, takerToken].every(t => c.tokens.includes(t)))
-        .map(i => i.poolAddress);
-}
