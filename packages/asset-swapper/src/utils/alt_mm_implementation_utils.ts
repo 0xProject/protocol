29510e72a75bb9e84b2e69eada385a0595614f22
Alex Kroeger <alexrkroeger@gmail.com>
2021-03-24 10:44:03 -0700

    appease prettier

--- packages/asset-swapper/src/utils/alt_mm_implementation_utils.ts
+++ packages/asset-swapper/src/utils/alt_mm_implementation_utils.ts
@@ -244,7 +244,10 @@ export async function returnQuoteFromAltMMAsync<ResponseT>(
     }
     // successful handling but no quote is indicated by status = 'rejected'
     if (response.data.status === 'rejected') {
-        warningLogger(response.data.id, `Alt RFQ MM handled the request successfully but did not return a quote (status = 'rejected')`);
+        warningLogger(
+            response.data.id,
+            `Alt RFQ MM handled the request successfully but did not return a quote (status = 'rejected')`,
+        );
         return {
             data: (emptyResponse as unknown) as ResponseT,
             // hack: set the http status to 204 no content so we can more
