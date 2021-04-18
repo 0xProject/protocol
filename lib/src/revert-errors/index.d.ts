6a0fb4cdc02adc231666f6c4d648f533b0ed9c5e
Alex Kroeger <alexrkroeger@gmail.com>
2021-04-18 11:41:50 -0700

    gitpkg

---
+++ lib/src/revert-errors/index.d.ts
@@ -0,0 +1,6 @@
+export { RevertError } from '@0x/utils';
+import { Common, LiquidityProvider, MetaTransactions, Ownable, Proxy, SignatureValidator, SimpleFunctionRegistry, Spender, TransformERC20, Wallet } from './inherited';
+import * as NativeOrders from './native_orders';
+import * as Signatures from './signatures';
+export { Common, Proxy, SimpleFunctionRegistry, Ownable, Spender, TransformERC20, Wallet, MetaTransactions, SignatureValidator, LiquidityProvider, NativeOrders, Signatures, };
+//# sourceMappingURL=index.d.ts.map
