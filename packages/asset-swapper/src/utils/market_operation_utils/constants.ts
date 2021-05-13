7d08d11684a3e6ae93d31399059f956d0af38418
Kim Persson <kimpersson88@gmail.com>
2021-05-13 12:58:34 +0200

    refactor: import valueByChainId from token-metadata to consolidate impl

--- packages/asset-swapper/src/utils/market_operation_utils/constants.ts
+++ packages/asset-swapper/src/utils/market_operation_utils/constants.ts
@@ -1,5 +1,7 @@
 import { ChainId, getContractAddressesForChainOrThrow } from '@0x/contract-addresses';
 import { FillQuoteTransformerOrderType } from '@0x/protocol-utils';
+// TODO(kimpers): does this belong here?
+import { valueByChainId } from '@0x/token-metadata';
 import { BigNumber } from '@0x/utils';
 import { formatBytes32String } from '@ethersproject/strings';
 
@@ -42,21 +44,6 @@ export const NULL_BYTES = '0x';
 export const NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const SAMPLER_ADDRESS = '0x5555555555555555555555555555555555555555';
 export const COMPARISON_PRICE_DECIMALS = 10;
-
-function valueByChainId<T>(rest: Partial<{ [key in ChainId]: T }>, defaultValue: T): { [key in ChainId]: T } {
-    // TODO I don't like this but iterating through enums is weird
-    return {
-        [ChainId.Mainnet]: defaultValue,
-        [ChainId.Ropsten]: defaultValue,
-        [ChainId.Rinkeby]: defaultValue,
-        [ChainId.Kovan]: defaultValue,
-        [ChainId.Ganache]: defaultValue,
-        [ChainId.BSC]: defaultValue,
-        [ChainId.Polygon]: defaultValue,
-        [ChainId.PolygonMumbai]: defaultValue,
-        ...(rest || {}),
-    };
-}
 
 /**
  * Valid sources for market sell.
