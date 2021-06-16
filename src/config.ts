c5ce4d280b18fa6648f37795e8b5b512a670e833
Kim Persson <kimpers@users.noreply.github.com>
2021-06-16 13:03:49 +0200

    fix: re-enable Maker PSM (#645)

--- src/config.ts
+++ src/config.ts
@@ -431,7 +431,7 @@ const EXCLUDED_SOURCES = (() => {
     const allERC20BridgeSources = Object.values(ERC20BridgeSource);
     switch (CHAIN_ID) {
         case ChainId.Mainnet:
-            return [ERC20BridgeSource.MultiBridge, ERC20BridgeSource.MakerPsm];
+            return [ERC20BridgeSource.MultiBridge];
         case ChainId.Kovan:
             return allERC20BridgeSources.filter(
                 (s) => s !== ERC20BridgeSource.Native && s !== ERC20BridgeSource.UniswapV2,
