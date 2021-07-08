2bab06ba77bca3643fb4307fc1df207cc2de9528
Kim Persson <kimpersson88@gmail.com>
2021-07-08 15:37:42 +0200

    feat: integrate Rust router with asset-swapper WIP

--- packages/asset-swapper/src/utils/market_operation_utils/path_optimizer.ts
+++ packages/asset-swapper/src/utils/market_operation_utils/path_optimizer.ts
@@ -1,5 +1,7 @@
 import { BigNumber } from '@0x/utils';
 import * as _ from 'lodash';
+import { OptimizerCapture, route } from 'neon-router';
+import { performance } from 'perf_hooks';
 
 import { MarketOperation } from '../../types';
 
@@ -9,17 +11,140 @@ import { ERC20BridgeSource, Fill } from './types';
 // tslint:disable: prefer-for-of custom-no-magic-numbers completed-docs no-bitwise
 
 const RUN_LIMIT_DECAY_FACTOR = 0.5;
+const RUST_ROUTER_NUM_SAMPLES = 200;
+const SHOULD_USE_RUST_ROUTER = process.env.RUST_ROUTER === 'true';
+
+function findOptimalRustPath(input: BigNumber, allFills: Fill[][]): Path {
+    // Track sample id's to integers (required by rust router)
+    const sampleIdLookup: { [key: string]: number } = {};
+    let sampleIdCounter = 0;
+    const fillToSampleId = (s: { source: string; sourcePathId: string; index: number }): number => {
+        const key = `${s.source}-${s.sourcePathId}-${s.index}`;
+        if (sampleIdLookup[key]) {
+            return sampleIdLookup[key];
+        } else {
+            sampleIdLookup[key] = ++sampleIdCounter;
+            return sampleIdLookup[key];
+        }
+    };
+
+    const adjustedParsedFills = allFills.map(fills => {
+        const adjustedFills: Fill[] = [];
+        // Samples are turned into Fills
+        // Fills are dependent on their parent and have their parents information "subtracted" from them
+        // e.g a samples for [1,10,100] => [5,50,500] look like [1, 9, 91] => [5, 40, 400]
+        for (let i = 0; i < fills.length; i++) {
+            const parsedFill: Fill = { ...fills[i] };
+            if (parsedFill.index !== 0) {
+                const parent = adjustedFills[i - 1];
+                parsedFill.parent = parent;
+                parsedFill.input = parsedFill.input.plus(parent.input);
+                parsedFill.output = parsedFill.output.plus(parent.output);
+                // Adjusted output is only modified for the first fill
+                const feePerc = parent.adjustedOutput.dividedBy(parent.output);
+                parsedFill.adjustedOutput = feePerc.times(parsedFill.output);
+            }
+            adjustedFills.push(parsedFill);
+        }
+        return adjustedFills;
+    });
+
+    // TODO(kimpers): Optimize: to one reduce
+    const pathsIn = adjustedParsedFills.map((fills: any) => ({
+        ids: fills.map((f: Fill) => fillToSampleId(f)),
+        inputs: fills.map((f: Fill) => f.input.toNumber()),
+        outputs: fills.map((f: Fill) => f.output.toNumber()),
+        outputFees: fills.map((f: Fill) => f.output.minus(f.adjustedOutput).toNumber()),
+    }));
+
+    // TODO(kimpers): replace all numbers with BigNumber or BigInt?
+    const rustArgs: OptimizerCapture = {
+        side: 'Sell',
+        // HACK: There can be off by 1 errors, somewhere...
+        // TODO(kimpers): use big num or something.
+        targetInput: input.plus(1).toNumber(),
+        pathsIn,
+    };
+
+    const before = performance.now();
+    const rustRoute: number[] = route(rustArgs, RUST_ROUTER_NUM_SAMPLES);
+    console.log('Rust perf (real):', performance.now() - before);
+
+    const fillsByPathId = _.groupBy(_.flatten(adjustedParsedFills), o => o.sourcePathId);
+
+    // TODO: is this safe with regard to ordering??
+    const sourcePathKeys = Object.keys(fillsByPathId);
+    const fakeFills: Fill[] = [];
+    const totalInputs = BigNumber.sum(...rustRoute);
+    for (let i = 0; i < rustRoute.length; i++) {
+        if (rustRoute[i] === 0) {
+            continue;
+        }
+        const rustInput = new BigNumber(rustRoute[i]);
+        // HACK: Handle the case where the router can under quote the input
+        // Set the first fill just a tad higher
+        const adjInput =
+            totalInputs.lt(input) && fakeFills.length === 0 ? rustInput.plus(input.minus(totalInputs)) : rustInput;
+        // Rust router has chosen this source;
+        const sourcePathKey = sourcePathKeys[i];
+        const fills = fillsByPathId[sourcePathKey];
+        let fill = fills[fills.length - 1];
+        // Descend to approach a closer fill for fillData which may not be consistent
+        // throughout the path (UniswapV3) and for a closer guesstimate at
+        // gas used
+        for (let k = fills.length - 1; k >= 0; k--) {
+            if (k === 0) {
+                fill = fills[0];
+            }
+            if (rustInput.isGreaterThan(fills[k].input)) {
+                // Between here and the previous fill
+                // HACK: Use the midpoint between the two
+                const left = fills[k];
+                const right = fills[k + 1];
+                if (left && right) {
+                    const midPrice = left.output
+                        .dividedBy(left.input)
+                        .plus(right.output.dividedBy(right.input))
+                        .dividedBy(2);
+                    fill = {
+                        ...right, // default to the greater (for gas used)
+                        input: rustInput,
+                        output: midPrice.times(rustInput).decimalPlaces(0),
+                    };
+                } else {
+                    fill = left || right;
+                }
+                break;
+            }
+        }
+        const adjustedOutput = fill.output
+            .dividedBy(fill.input)
+            .times(adjInput)
+            .decimalPlaces(0);
+        fakeFills.push({
+            ...fill,
+            input: adjInput,
+            output: adjustedOutput,
+            index: 0,
+            parent: undefined,
+        });
+    }
+
+    const fakePath = Path.create(MarketOperation.Sell, fakeFills, input);
+
+    return fakePath;
+}
 
 /**
  * Find the optimal mixture of fills that maximizes (for sells) or minimizes
  * (for buys) output, while meeting the input requirement.
  */
-export async function findOptimalPathAsync(
+async function findOptimalPathJSAsync(
     side: MarketOperation,
     fills: Fill[][],
     targetInput: BigNumber,
-    runLimit: number = 2 ** 8,
-    opts: PathPenaltyOpts = DEFAULT_PATH_PENALTY_OPTS,
+    runLimit: number,
+    opts: PathPenaltyOpts,
 ): Promise<Path | undefined> {
     // Sort fill arrays by descending adjusted completed rate.
     // Remove any paths which cannot impact the optimal path
@@ -35,6 +160,26 @@ export async function findOptimalPathAsync(
         await Promise.resolve();
     }
     return optimalPath.isComplete() ? optimalPath : undefined;
+}
+
+/**
+ * Finds the optimal path using either the Rust or JS based routers
+ *
+ */
+export async function findOptimalPathAsync(
+    side: MarketOperation,
+    fills: Fill[][],
+    targetInput: BigNumber,
+    runLimit: number = 2 ** 8,
+    opts: PathPenaltyOpts = DEFAULT_PATH_PENALTY_OPTS,
+): Promise<Path | undefined> {
+    if (SHOULD_USE_RUST_ROUTER) {
+        console.log('-----USING RUST ROUTER!-------');
+        return findOptimalRustPath(targetInput, fills);
+    }
+
+    console.log('-----USING JS ROUTER!-------');
+    return findOptimalPathJSAsync(side, fills, targetInput, runLimit, opts);
 }
 
 // Sort fill arrays by descending adjusted completed rate.
