b7aa2d6fe088284f95e4990d2f7bd1a5bc3a5fc6
Kim Persson <kimpersson88@gmail.com>
2021-07-20 21:35:52 +0200

    feat: AaveV2 deposit/withdrawal integration WIP

--- packages/asset-swapper/test/artifacts.ts
+++ packages/asset-swapper/test/artifacts.ts
@@ -5,6 +5,7 @@
  */
 import { ContractArtifact } from 'ethereum-types';
 
+import * as AaveV2Sampler from '../test/generated-artifacts/AaveV2Sampler.json';
 import * as BalanceChecker from '../test/generated-artifacts/BalanceChecker.json';
 import * as BalancerSampler from '../test/generated-artifacts/BalancerSampler.json';
 import * as BalancerV2Sampler from '../test/generated-artifacts/BalancerV2Sampler.json';
@@ -42,6 +43,7 @@ import * as UniswapV2Sampler from '../test/generated-artifacts/UniswapV2Sampler.
 import * as UniswapV3Sampler from '../test/generated-artifacts/UniswapV3Sampler.json';
 import * as UtilitySampler from '../test/generated-artifacts/UtilitySampler.json';
 export const artifacts = {
+    AaveV2Sampler: AaveV2Sampler as ContractArtifact,
     BalanceChecker: BalanceChecker as ContractArtifact,
     BalancerSampler: BalancerSampler as ContractArtifact,
     BalancerV2Sampler: BalancerV2Sampler as ContractArtifact,
