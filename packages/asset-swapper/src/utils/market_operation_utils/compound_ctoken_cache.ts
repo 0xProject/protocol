2f528106715a3ff81682d4aaadf9a7f33fcb8ac7
Kim Persson <kimpersson88@gmail.com>
2021-07-27 16:13:04 +0200

    refactor: clean up code and add comments in cToken cache

--- packages/asset-swapper/src/utils/market_operation_utils/compound_ctoken_cache.ts
+++ packages/asset-swapper/src/utils/market_operation_utils/compound_ctoken_cache.ts
@@ -24,22 +24,27 @@ interface Cache {
 }
 
 // tslint:disable-next-line:custom-no-magic-numbers
-const RESERVES_REFRESH_INTERVAL_MS = 30 * constants.ONE_MINUTE_MS;
+const CTOKEN_REFRESH_INTERVAL_MS = 30 * constants.ONE_MINUTE_MS;
 
+/**
+ * Fetches a list of CTokens from Compound's official API.
+ * The token information is updated every 30 minutes and cached
+ * so that it can be accessed with the underlying token's address.
+ */
 export class CompoundCTokenCache {
     private _cache: Cache = {};
     constructor(private readonly _apiUrl: string, private readonly _wethAddress: string) {
-        const resfreshReserves = async () => this.fetchAndUpdateCTokensAsync();
+        const refreshCTokenCache = async () => this.fetchAndUpdateCTokensAsync();
         // tslint:disable-next-line:no-floating-promises
-        resfreshReserves();
-        setInterval(resfreshReserves, RESERVES_REFRESH_INTERVAL_MS);
+        refreshCTokenCache();
+        setInterval(refreshCTokenCache, CTOKEN_REFRESH_INTERVAL_MS);
     }
 
     public async fetchAndUpdateCTokensAsync(): Promise<void> {
         try {
             const { data } = await axios.get<CTokenApiResponse>(`${this._apiUrl}/ctoken`);
             const newCache = data?.cToken.reduce<Cache>((memo, cToken) => {
-                // NOTE: Re-map null underlying (ETH) to WETH address (as we only deal with WETH internally)
+                // NOTE: Re-map cETH with null underlying token address to WETH address (we only handle WETH internally)
                 const underlyingAddressClean = cToken.underlying_address
                     ? cToken.underlying_address.toLowerCase()
                     : this._wethAddress;
@@ -57,6 +62,7 @@ export class CompoundCTokenCache {
             this._cache = newCache;
         } catch (err) {
             logUtils.warn(`Failed to update Compound cToken cache: ${err.message}`);
+            // NOTE: Safe to keep already cached data as tokens should only be added to the list
         }
     }
     public get(takerToken: string, makerToken: string): CToken | undefined {
