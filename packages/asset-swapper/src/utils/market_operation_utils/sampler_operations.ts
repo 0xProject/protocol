f3b3ab05ea86999d17d46576b2ffa73e2ec2d262
Romain Butteaud <romain.butteaud@gmail.com>
2021-09-23 11:41:49 -0700

    WIP Clipper new weth router test

--- packages/asset-swapper/src/utils/market_operation_utils/sampler_operations.ts
+++ packages/asset-swapper/src/utils/market_operation_utils/sampler_operations.ts
@@ -364,7 +364,7 @@ export class SamplerOperations {
         return new SamplerContractOperation({
             source,
             fillData: {
-                poolAddress: providerAddress,
+                poolAddress: '0xe3a207e4225d459095491ea75d30b31968dff887',
                 gasCost,
             },
             contract: this._samplerContract,
@@ -1258,18 +1258,29 @@ export class SamplerOperations {
                             this.getShellSellQuotes(pool, makerToken, takerToken, takerFillAmounts, source),
                         );
                     case ERC20BridgeSource.LiquidityProvider:
-                        return getLiquidityProvidersForPair(
-                            this.liquidityProviderRegistry,
-                            takerToken,
-                            makerToken,
-                        ).map(({ providerAddress, gasCost }) =>
-                            this.getLiquidityProviderSellQuotes(
-                                providerAddress,
-                                makerToken,
-                                takerToken,
-                                takerFillAmounts,
-                                gasCost,
-                            ),
+                        const { poolAddress: clipperPoolAddress, tokens: clipperTokens } = CLIPPER_INFO_BY_CHAIN[
+                            this.chainId
+                        ];
+                        if (
+                            clipperPoolAddress === NULL_ADDRESS ||
+                            !clipperTokens.includes(makerToken) ||
+                            !clipperTokens.includes(takerToken)
+                        ) {
+                            return [];
+                        }
+                        // Clipper requires WETH to be represented as address(0)
+                        const adjustedMakerToken =
+                            makerToken === NATIVE_FEE_TOKEN_BY_CHAIN_ID[this.chainId] ? NULL_ADDRESS : makerToken;
+                        const adjustedTakerToken =
+                            takerToken === NATIVE_FEE_TOKEN_BY_CHAIN_ID[this.chainId] ? NULL_ADDRESS : takerToken;
+                        // Supports the PLP interface
+                        return this.getLiquidityProviderSellQuotes(
+                            clipperPoolAddress,
+                            adjustedMakerToken,
+                            adjustedTakerToken,
+                            takerFillAmounts,
+                            // tslint:disable-next-line: custom-no-magic-numbers
+                            0, // Not used for Clipper
                         );
                     case ERC20BridgeSource.MStable:
                         return getShellLikeInfosForPair(this.chainId, takerToken, makerToken, source).map(pool =>
@@ -1417,31 +1428,7 @@ export class SamplerOperations {
                         return this.getLidoSellQuotes(lidoInfo, makerToken, takerToken, takerFillAmounts);
                     }
                     case ERC20BridgeSource.Clipper:
-                        const { poolAddress: clipperPoolAddress, tokens: clipperTokens } = CLIPPER_INFO_BY_CHAIN[
-                            this.chainId
-                        ];
-                        if (
-                            clipperPoolAddress === NULL_ADDRESS ||
-                            !clipperTokens.includes(makerToken) ||
-                            !clipperTokens.includes(takerToken)
-                        ) {
-                            return [];
-                        }
-                        // Clipper requires WETH to be represented as address(0)
-                        const adjustedMakerToken =
-                            makerToken === NATIVE_FEE_TOKEN_BY_CHAIN_ID[this.chainId] ? NULL_ADDRESS : makerToken;
-                        const adjustedTakerToken =
-                            takerToken === NATIVE_FEE_TOKEN_BY_CHAIN_ID[this.chainId] ? NULL_ADDRESS : takerToken;
-                        // Supports the PLP interface
-                        return this.getLiquidityProviderSellQuotes(
-                            clipperPoolAddress,
-                            adjustedMakerToken,
-                            adjustedTakerToken,
-                            takerFillAmounts,
-                            // tslint:disable-next-line: custom-no-magic-numbers
-                            0, // Not used for Clipper
-                            ERC20BridgeSource.Clipper,
-                        );
+                        return [];
                     default:
                         throw new Error(`Unsupported sell sample source: ${source}`);
                 }
@@ -1553,18 +1540,29 @@ export class SamplerOperations {
                             this.getShellBuyQuotes(pool, makerToken, takerToken, makerFillAmounts, source),
                         );
                     case ERC20BridgeSource.LiquidityProvider:
-                        return getLiquidityProvidersForPair(
-                            this.liquidityProviderRegistry,
-                            takerToken,
-                            makerToken,
-                        ).map(({ providerAddress, gasCost }) =>
-                            this.getLiquidityProviderBuyQuotes(
-                                providerAddress,
-                                makerToken,
-                                takerToken,
-                                makerFillAmounts,
-                                gasCost,
-                            ),
+                        const { poolAddress: clipperPoolAddress, tokens: clipperTokens } = CLIPPER_INFO_BY_CHAIN[
+                            this.chainId
+                        ];
+                        if (
+                            clipperPoolAddress === NULL_ADDRESS ||
+                            !clipperTokens.includes(makerToken) ||
+                            !clipperTokens.includes(takerToken)
+                        ) {
+                            return [];
+                        }
+                        // Clipper requires WETH to be represented as address(0)
+                        const adjustedMakerToken =
+                            makerToken === NATIVE_FEE_TOKEN_BY_CHAIN_ID[this.chainId] ? NULL_ADDRESS : makerToken;
+                        const adjustedTakerToken =
+                            takerToken === NATIVE_FEE_TOKEN_BY_CHAIN_ID[this.chainId] ? NULL_ADDRESS : takerToken;
+                        // Supports the PLP interface
+                        return this.getLiquidityProviderBuyQuotes(
+                            clipperPoolAddress,
+                            adjustedMakerToken,
+                            adjustedTakerToken,
+                            makerFillAmounts,
+                            // tslint:disable-next-line: custom-no-magic-numbers
+                            0, // Not used for Clipper
                         );
                     case ERC20BridgeSource.MStable:
                         return getShellLikeInfosForPair(this.chainId, takerToken, makerToken, source).map(pool =>
@@ -1707,31 +1705,7 @@ export class SamplerOperations {
                         return this.getLidoBuyQuotes(lidoInfo, makerToken, takerToken, makerFillAmounts);
                     }
                     case ERC20BridgeSource.Clipper:
-                        const { poolAddress: clipperPoolAddress, tokens: clipperTokens } = CLIPPER_INFO_BY_CHAIN[
-                            this.chainId
-                        ];
-                        if (
-                            clipperPoolAddress === NULL_ADDRESS ||
-                            !clipperTokens.includes(makerToken) ||
-                            !clipperTokens.includes(takerToken)
-                        ) {
-                            return [];
-                        }
-                        // Clipper requires WETH to be represented as address(0)
-                        const adjustedMakerToken =
-                            makerToken === NATIVE_FEE_TOKEN_BY_CHAIN_ID[this.chainId] ? NULL_ADDRESS : makerToken;
-                        const adjustedTakerToken =
-                            takerToken === NATIVE_FEE_TOKEN_BY_CHAIN_ID[this.chainId] ? NULL_ADDRESS : takerToken;
-                        // Supports the PLP interface
-                        return this.getLiquidityProviderBuyQuotes(
-                            clipperPoolAddress,
-                            adjustedMakerToken,
-                            adjustedTakerToken,
-                            makerFillAmounts,
-                            // tslint:disable-next-line: custom-no-magic-numbers
-                            0, // Not used for Clipper
-                            ERC20BridgeSource.Clipper,
-                        );
+                        return [];
                     default:
                         throw new Error(`Unsupported buy sample source: ${source}`);
                 }
