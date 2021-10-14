c4b00f59c9fc19af9dd3a276d0eb5f1e4fd42be4
shawnxin89 <shawn@0xproject.com>
2021-10-14 14:59:17 -0700

    add more Fantom liquidity sources

--- packages/asset-swapper/src/utils/market_operation_utils/bridge_source_utils.ts
+++ packages/asset-swapper/src/utils/market_operation_utils/bridge_source_utils.ts
@@ -42,6 +42,8 @@ import {
     SMOOTHY_BSC_INFOS,
     SMOOTHY_MAINNET_INFOS,
     SNOWSWAP_MAINNET_INFOS,
+    SPIRITSWAP_ROUTER_BY_CHAIN_ID,
+    SPOOKYSWAP_ROUTER_BY_CHAIN_ID,
     SUSHISWAP_ROUTER_BY_CHAIN_ID,
     SWERVE_MAINNET_INFOS,
     TRADER_JOE_ROUTER_BY_CHAIN_ID,
@@ -468,7 +470,9 @@ export function uniswapV2LikeRouterAddress(
         | ERC20BridgeSource.ShibaSwap
         | ERC20BridgeSource.JetSwap
         | ERC20BridgeSource.TraderJoe
-        | ERC20BridgeSource.Pangolin,
+        | ERC20BridgeSource.Pangolin
+        | ERC20BridgeSource.SpookySwap
+        | ERC20BridgeSource.SpiritSwap,
 ): string {
     switch (source) {
         case ERC20BridgeSource.UniswapV2:
@@ -509,6 +513,10 @@ export function uniswapV2LikeRouterAddress(
             return PANGOLIN_ROUTER_BY_CHAIN_ID[chainId];
         case ERC20BridgeSource.TraderJoe:
             return TRADER_JOE_ROUTER_BY_CHAIN_ID[chainId];
+        case ERC20BridgeSource.SpookySwap:
+            return SPOOKYSWAP_ROUTER_BY_CHAIN_ID[chainId];
+        case ERC20BridgeSource.SpiritSwap:
+            return SPIRITSWAP_ROUTER_BY_CHAIN_ID[chainId];
         default:
             throw new Error(`Unknown UniswapV2 like source ${source}`);
     }
