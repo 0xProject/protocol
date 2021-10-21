be3542cfa636fe29bea079aac18ab70f57daff09
cece-z <cece@0xproject.com>
2021-10-21 02:06:41 -0400

    init sol files and dependencies

---
+++ contracts/zero-ex/contracts/src/features/interfaces/IUniswapV2Feature.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+
+  Copyright 2021 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+pragma solidity ^0.6.5;
+pragma experimental ABIEncoderV2;
+
+import "@0x/contracts-erc20/contracts/src/v06/IERC20TokenV06.sol";
+
+/// @dev VIP UniswapV2 (and forks) fill functions.
+interface IUniswapV2Feature {
+
+    enum ProtocolFork {
+        PancakeSwap,
+        PancakeSwapV2,
+        BakerySwap,
+        SushiSwap,
+        ApeSwap,
+        CafeSwap,
+        CheeseSwap,
+        JulSwap
+    }
+
+    /// @dev Efficiently sell directly to UniswapV2 (and forks).
+    /// @param tokens Sell path.
+    /// @param sellAmount of `tokens[0]` Amount to sell.
+    /// @param minBuyAmount Minimum amount of `tokens[-1]` to buy.
+    /// @param fork The protocol fork to use.
+    /// @return buyAmount Amount of `tokens[-1]` bought.
+    function sellToUniswapV2(
+        IERC20TokenV06[] calldata tokens,
+        uint256 sellAmount,
+        uint256 minBuyAmount,
+        ProtocolFork fork
+    )
+        external
+        payable
+        returns (uint256 buyAmount);
+}
