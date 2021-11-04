07bedbd71402a0bf52c2f5bebf986b35e15a299c
Noah Khamliche <noah@0x.org>
2021-11-04 12:44:15 -0400

    celo api

--- src/config.ts
+++ src/config.ts
@@ -421,6 +421,8 @@ const EXCLUDED_SOURCES = (() => {
             return [ERC20BridgeSource.MultiBridge, ERC20BridgeSource.Native];
         case ChainId.Avalanche:
             return [ERC20BridgeSource.MultiBridge, ERC20BridgeSource.Native];
+        case ChainId.Celo:
+            return [ERC20BridgeSource.MultiBridge, ERC20BridgeSource.Native];
         default:
             return allERC20BridgeSources.filter((s) => s !== ERC20BridgeSource.Native);
     }
@@ -437,6 +439,8 @@ const EXCLUDED_FEE_SOURCES = (() => {
         case ChainId.BSC:
             return [ERC20BridgeSource.Uniswap];
         case ChainId.Polygon:
+            return [];
+        case ChainId.Celo:
             return [];
         default:
             return [ERC20BridgeSource.Uniswap, ERC20BridgeSource.UniswapV2];
