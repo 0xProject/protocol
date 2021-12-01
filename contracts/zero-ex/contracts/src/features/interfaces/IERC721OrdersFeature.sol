ac6f272dc1881c4ca6ec56202dc0f65616d348b2
Michael Zhu <mchl.zhu.96@gmail.com>
2021-12-01 10:32:14 -0800

    Add missing @return comments

--- contracts/zero-ex/contracts/src/features/interfaces/IERC721OrdersFeature.sol
+++ contracts/zero-ex/contracts/src/features/interfaces/IERC721OrdersFeature.sol
@@ -99,6 +99,8 @@ interface IERC721OrdersFeature {
     /// @param signatures The order signatures.
     /// @param revertIfIncomplete If true, reverts if this
     ///        function fails to fill any individual order.
+    /// @return successes An array of booleans corresponding to whether
+    ///         each order in `orders` was successfully filled.
     function batchBuyERC721s(
         LibERC721Order.ERC721Order[] calldata orders,
         LibSignature.Signature[] calldata signatures,
@@ -116,6 +118,9 @@ interface IERC721OrdersFeature {
     /// @param buyOrder Order buying an ERC721 asset.
     /// @param sellOrderSignature Signature for the sell order.
     /// @param buyOrderSignature Signature for the buy order.
+    /// @return profit The amount of profit earned by the caller
+    ///         of this function (denominated in the ERC20 token
+    ///         of the matched orders).
     function matchERC721Orders(
         LibERC721Order.ERC721Order calldata sellOrder,
         LibERC721Order.ERC721Order calldata buyOrder,
@@ -133,6 +138,11 @@ interface IERC721OrdersFeature {
     /// @param buyOrders Orders buying ERC721 assets.
     /// @param sellOrderSignatures Signatures for the sell orders.
     /// @param buyOrderSignatures Signatures for the buy orders.
+    /// @return profits The amount of profit earned by the caller
+    ///         of this function for each pair of matched orders
+    ///         (denominated in the ERC20 token of the order pair).
+    /// @return successes An array of booleans corresponding to
+    ///         whether each pair of orders was successfully matched.
     function batchMatchERC721Orders(
         LibERC721Order.ERC721Order[] calldata sellOrders,
         LibERC721Order.ERC721Order[] calldata buyOrders,
