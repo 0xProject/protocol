476988c64a13dd2ad64b2ab30806af0acc046c5d
Noah Khamliche <71143770+dextracker@users.noreply.github.com>
2021-12-06 13:31:45 -0800

    Sushi router celo fix (#376)
    * Updated Sushiswap router on CELO
    
    Sushi address in the docs was incorrect, this is the correct address `0x1421bDe4B10e8dd459b3BCb598810B1337D56842`.
    
    * address to lowercase
    
    * Update CHANGELOG.json
    
    * fixed sushi router address, replaced celo ref with wcelo, fixed mcusd address
    
    * celo->wcelo
    
    * Update packages/asset-swapper/CHANGELOG.json
    
    Co-authored-by: Jacob Evans <jacob@dekz.net>
    
    * changelog
    
    * version bump
    
    Co-authored-by: Jacob Evans <jacob@dekz.net>

--- packages/asset-swapper/src/utils/market_operation_utils/constants.ts
+++ packages/asset-swapper/src/utils/market_operation_utils/constants.ts
@@ -506,6 +506,7 @@ export const AVALANCHE_TOKENS = {
 
 export const CELO_TOKENS = {
     WETH: '0xe919f65739c26a42616b7b8eedc6b5524d1e3ac4',
+<<<<<<< HEAD
     CELO: '0x471ece3750da237f93b8e339c536989b8978a438',
     mCUSD: '0x64defa3544c695db8c535d289d843a189aa26b98',
     wBTC: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
@@ -528,6 +529,10 @@ export const CELO_TOKENS = {
     oDAI: '0x90Ca507a5D4458a4C6C6249d186b6dCb02a5BCCd',
     pCELO: '0x301a61D01A63c8D670c2B8a43f37d12eF181F997',
     pUSD: '0xEadf4A7168A82D30Ba0619e64d5BCf5B30B45226',
+=======
+    WCELO: '0x471ece3750da237f93b8e339c536989b8978a438',
+    mCUSD: '0x918146359264c492bd6934071c6bd31c854edbc3',
+>>>>>>> 4170f970d (Sushi router celo fix (#376))
 };
 
 export const FANTOM_TOKENS = {
@@ -738,7 +743,7 @@ export const DEFAULT_INTERMEDIATE_TOKENS_BY_CHAIN_ID = valueByChainId<string[]>(
             AVALANCHE_TOKENS.USDC,
         ],
         [ChainId.Fantom]: [FANTOM_TOKENS.WFTM, FANTOM_TOKENS.WETH, FANTOM_TOKENS.DAI, FANTOM_TOKENS.USDC],
-        [ChainId.Celo]: [CELO_TOKENS.mCUSD, CELO_TOKENS.WETH, CELO_TOKENS.CELO],
+        [ChainId.Celo]: [CELO_TOKENS.mCUSD, CELO_TOKENS.WETH, CELO_TOKENS.WCELO],
     },
     [],
 );
