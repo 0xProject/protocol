455ee52366c5d8d06ae5dac678e882e3bcfd0a8f
Kim Persson <kimpers@users.noreply.github.com>
2022-01-04 23:39:21 +0100

    feat: push router performance metrics to prometheus (#800)
    * chore: bump asset-swapper to push router performance metrics
    
    * feat: send router metrics to prometheus
    
    * fix: sampler metrics naming

--- src/utils/sampler_metrics.ts
+++ src/utils/sampler_metrics.ts
@@ -12,9 +12,15 @@ const SAMPLER_GAS_LIMIT_SUMMARY = new Summary({
     help: 'Provides information about the gas limit detected during a sampler call',
 });
 
-const SAMPLER_BLOCK_NUMBER_GUAGE = new Gauge({
+const SAMPLER_BLOCK_NUMBER_GAUGE = new Gauge({
     name: 'sampler_blocknumber',
     help: 'Provides information about the gas limit detected during a sampler call',
+});
+
+const ROUTER_EXECUTION_TIME_SUMMARY = new Summary({
+    name: 'router_execution_time',
+    help: 'Provides information about the execution time for routing related logic',
+    labelNames: ['router', 'type'],
 });
 
 export const SAMPLER_METRICS: SamplerMetrics = {
@@ -27,6 +33,15 @@ export const SAMPLER_METRICS: SamplerMetrics = {
     },
 
     logBlockNumber: (blockNumber: BigNumber): void => {
-        SAMPLER_BLOCK_NUMBER_GUAGE.set(blockNumber.toNumber());
+        SAMPLER_BLOCK_NUMBER_GAUGE.set(blockNumber.toNumber());
+    },
+
+    logRouterDetails: (data: {
+        router: 'neon-router' | 'js';
+        type: 'all' | 'vip' | 'total';
+        timingMs: number;
+    }): void => {
+        const { router, type, timingMs } = data;
+        ROUTER_EXECUTION_TIME_SUMMARY.observe({ router, type }, timingMs);
     },
 };
