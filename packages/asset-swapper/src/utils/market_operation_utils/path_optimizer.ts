59d3fd2ef0543a1166d87118ea12429da7c41e6a
Kim Persson <kimpers@users.noreply.github.com>
2022-02-22 13:31:47 +0100

    fix: start native orders "samples" at 0 & don't skip neg adj orders (#425)
    * fix: start native orders "samples" at 0 & don't skip neg adj orders
    
    * fix: bail on routes with invalid output estimation (no valid route)
    
    * chore: bump neon-router version
    
    * chore: add asset-swapper changelog entry

--- packages/asset-swapper/src/utils/market_operation_utils/path_optimizer.ts
+++ packages/asset-swapper/src/utils/market_operation_utils/path_optimizer.ts
@@ -5,9 +5,10 @@ import { BigNumber, hexUtils } from '@0x/utils';
 import * as _ from 'lodash';
 import { performance } from 'perf_hooks';
 
+import { DEFAULT_WARNING_LOGGER } from '../../constants';
 import { MarketOperation, NativeOrderWithFillableAmounts } from '../../types';
-import { VIP_ERC20_BRIDGE_SOURCES_BY_CHAIN_ID, ZERO_AMOUNT } from '../market_operation_utils/constants';
 
+import { VIP_ERC20_BRIDGE_SOURCES_BY_CHAIN_ID, ZERO_AMOUNT } from './constants';
 import { dexSamplesToFills, ethToOutputAmount, nativeOrdersToFills } from './fills';
 import { DEFAULT_PATH_PENALTY_OPTS, Path, PathPenaltyOpts } from './path';
 import { DexSample, ERC20BridgeSource, FeeSchedule, Fill, FillData, SamplerMetrics } from './types';
@@ -154,8 +155,10 @@ function findRoutesAndCreateOptimalPath(
         const inputs = [];
         const outputs = [];
         const outputFees = [];
-        for (let i = 1; i <= 13; i++) {
-            const fraction = i / 13;
+        // NOTE: We start at 0 here because the native order might be much larger than the amount
+        // By starting at 0 we make sure we can always use a portion of the native order to fill/partial fill
+        for (let i = 0; i <= 12; i++) {
+            const fraction = i / 12;
             const currentInput = BigNumber.min(normalizedOrderInput.times(fraction), normalizedOrderInput);
             const currentOutput = BigNumber.min(normalizedOrderOutput.times(fraction), normalizedOrderOutput);
             const id = `${ERC20BridgeSource.Native}-${serializedPaths.length}-${idx}-${i}`;
@@ -210,7 +213,11 @@ function findRoutesAndCreateOptimalPath(
 
     const scale = input.dividedBy(totalRoutedAmount);
     for (const [routeInput, routeSamplesAndNativeOrders, outputAmount, sourcePathId] of routesAndSamplesAndOutputs) {
-        if (!routeInput || !routeSamplesAndNativeOrders || !outputAmount || !Number.isFinite(outputAmount)) {
+        if (!Number.isFinite(outputAmount)) {
+            DEFAULT_WARNING_LOGGER(rustArgs, `neon-router: invalid route outputAmount ${outputAmount}`);
+            return undefined;
+        }
+        if (!routeInput || !routeSamplesAndNativeOrders || !outputAmount) {
             continue;
         }
         // TODO(kimpers): [TKR-241] amounts are sometimes clipped in the router due to precision loss for number/f64
@@ -229,6 +236,7 @@ function findRoutesAndCreateOptimalPath(
                 opts.outputAmountPerEth,
                 opts.inputAmountPerEth,
                 fees,
+                false,
             )[0] as Fill | undefined;
             // Note: If the order has an adjusted rate of less than or equal to 0 it will be skipped
             // and nativeFill will be `undefined`
