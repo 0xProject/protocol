4f391c6ab1e5b4d6515b7ccd4afff9dae2c10cfc
Jacob Evans <jacob@dekz.net>
2022-02-24 18:47:05 +1000

    lint

--- packages/asset-swapper/src/scripts/curve_pool_factory_reader.ts
+++ packages/asset-swapper/src/scripts/curve_pool_factory_reader.ts
@@ -2,10 +2,11 @@ import { RPCSubprovider, Web3ProviderEngine } from '@0x/subproviders';
 import { providerUtils as ZeroExProviderUtils } from '@0x/utils';
 import { BlockParamLiteral } from '@0x/web3-wrapper';
 import _ = require('lodash');
+import { inspect } from 'util';
+
 import { artifacts } from '../artifacts';
 import { CURVE_MAINNET_INFOS } from '../utils/market_operation_utils/constants';
 import { CurvePoolFactoryReaderContract } from '../wrappers';
-import { inspect } from 'util';
 
 const PROVIDER = new Web3ProviderEngine();
 PROVIDER.addProvider(new RPCSubprovider(process.env.ETHEREUM_RPC_URL!));
@@ -19,13 +20,13 @@ const FACTORY_READER = new CurvePoolFactoryReaderContract(QUERY_ADDRESS, PROVIDE
 
 const getCurveCryptoFactoryPools = async (
     address: string,
-): Promise<{
+): Promise<Array<{
     coins: string[];
     symbols: string[];
     pool: string;
     hasBalance: boolean;
     codeHash: string;
-}[]> => {
+}>> => {
     const pools = await FACTORY_READER.getCryptoFactoryPools(address).callAsync(
         { overrides },
         BlockParamLiteral.Latest,
@@ -36,6 +37,7 @@ const getCurveCryptoFactoryPools = async (
 const CURVE_CRYPTO_FACTORY = '0xf18056bbd320e96a48e3fbf8bc061322531aac99';
 const CURVE_META_FACTORY = '0xb9fc157394af804a3578134a6585c0dc9cc990d4';
 
+// tslint:disable no-floating-promises
 (async () => {
     try {
         // Appears to be 2 different factories, cryptoFactory pools looks like the latest and greatest
@@ -49,6 +51,7 @@ const CURVE_META_FACTORY = '0xb9fc157394af804a3578134a6585c0dc9cc990d4';
             return !currentlyMappedAddresses.includes(pool.pool) && pool.hasBalance;
         });
 
+        // tslint:disable no-console
         console.log(inspect(_.groupBy(missingPools, 'codeHash'), { showHidden: false, depth: null, colors: true }));
         console.log(`Found ${missingPools.length} missing pools`);
     } catch (e) {
