754e6ebd4796efb66aa86bc57e9efa6e3484634a
Jacob Evans <jacob@dekz.net>
2022-02-25 20:42:26 +1000

    map automatically

--- packages/asset-swapper/contracts/src/curve/CurvePoolFactoryReader.sol
+++ packages/asset-swapper/contracts/src/curve/CurvePoolFactoryReader.sol
@@ -37,7 +37,28 @@ interface ICurvePoolFactory {
 
     function pool_list(uint256) external view returns (address);
 
-    function pool_implementation() external view returns (address);
+    function get_coins() external view returns (address[2] memory);
+}
+
+interface ICurveMetaPoolFactory {
+    function pool_count() external view returns (uint256);
+
+    function pool_list(uint256) external view returns (address);
+
+    function get_coins() external view returns (address[4] memory);
+    function get_underlying_coins(address) external view returns (address[8] memory);
+    function is_meta(address) external view returns (bool);
+}
+
+interface ICurvePoolRegistry {
+    function pool_count() external view returns (uint256);
+
+    function pool_list(uint256) external view returns (address);
+
+    function get_coins(address) external view returns (address[8] memory);
+    function get_underlying_coins(address) external view returns (address[8] memory);
+    function is_meta(address) external view returns (bool);
+    function get_pool_name(address) external view returns (string memory);
 }
 
 contract CurvePoolFactoryReader {
@@ -50,7 +71,18 @@ contract CurvePoolFactoryReader {
         bytes32 codeHash;
     }
 
-    function getCryptoFactoryPools(ICurvePoolFactory factory)
+    struct CurveRegistryPoolInfo {
+        address[] coins;
+        address[] underlyingCoins;
+        string[] symbols;
+        string[] underlyingSymbols;
+        address pool;
+        bool hasBalance;
+        bytes32 codeHash;
+        bool isMeta;
+    }
+
+    function getFactoryPools(ICurvePoolFactory factory)
         external
         view
         returns (CurveFactoryPoolInfo[] memory poolInfos)
@@ -60,7 +92,7 @@ contract CurvePoolFactoryReader {
         for (uint256 i = 0; i < poolCount; i++) {
             ICurveFactoryPool pool = ICurveFactoryPool(factory.pool_list(i));
             try
-                CurvePoolFactoryReader(address(this)).getCryptoFactoryPoolInfo
+                CurvePoolFactoryReader(address(this)).getFactoryPoolInfo
                     (pool)
                     returns (CurveFactoryPoolInfo memory poolInfo)
             {
@@ -68,13 +100,51 @@ contract CurvePoolFactoryReader {
             } catch { }
             // Set regardless, failed pools will have an address but no data
             poolInfos[i].pool = address(pool);
-            bytes32 codeHash;    
-            assembly { codeHash := extcodehash(pool) }
-            poolInfos[i].codeHash = codeHash;
         }
     }
 
-    function getCryptoFactoryPoolInfo(ICurveFactoryPool pool)
+    function getRegistryPools(ICurvePoolRegistry registry)
+        external
+        view
+        returns (CurveRegistryPoolInfo[] memory poolInfos)
+    {
+        uint256 poolCount = registry.pool_count();
+        poolInfos = new CurveRegistryPoolInfo[](poolCount);
+        for (uint256 i = 0; i < poolCount; i++) {
+            ICurveFactoryPool pool = ICurveFactoryPool(registry.pool_list(i));
+            try
+                CurvePoolFactoryReader(address(this)).getRegistryPoolInfo
+                    (registry, pool)
+                    returns (CurveRegistryPoolInfo memory poolInfo)
+            {
+                poolInfos[i] = poolInfo;
+            } catch { }
+            // Set regardless, failed pools will have an address but no data
+            poolInfos[i].pool = address(pool);
+        }
+    }
+
+    function getRegistryPoolInfo(ICurvePoolRegistry registry, ICurveFactoryPool pool)
+        external
+        view
+        returns (CurveRegistryPoolInfo memory poolInfo)
+    {
+        // CurveFactoryPoolInfo memory factoryPoolInfo = this.getFactoryPoolInfo(pool);
+        poolInfo.pool = address(pool);
+        poolInfo.codeHash = _codeHash(address(pool));
+        // poolInfo.hasBalance = factoryPoolInfo.hasBalance;
+
+        poolInfo.isMeta = registry.is_meta(address(pool));
+        // Pulled iteratively in getCryptoFactoryPoolInfo
+        // poolInfo.coins = factoryPoolInfo.coins;
+        // Convert from address[8]
+        poolInfo.coins = _trimCoins(registry.get_coins(address(pool)));
+        poolInfo.symbols = _coinSymbols(poolInfo.coins);
+        poolInfo.underlyingCoins = _trimCoins(registry.get_underlying_coins(address(pool)));
+        poolInfo.underlyingSymbols = _coinSymbols(poolInfo.underlyingCoins);
+    }
+
+    function getFactoryPoolInfo(ICurveFactoryPool pool)
         external
         view
         returns (CurveFactoryPoolInfo memory poolInfo)
@@ -99,19 +169,16 @@ contract CurvePoolFactoryReader {
         } while (lastAddress != address(0));
 
         // Fetch all of the symbols in the pool, some may fail
-        poolInfo.symbols = new string[](poolInfo.coins.length);
+        poolInfo.symbols = _coinSymbols(poolInfo.coins);
         // Also fetch the accumulated balances, so we can reject anything that's
         // basically uninitialized (balance 0)
         uint256 totalBalance = 0;
         for (uint256 i = 0; i < poolInfo.coins.length; i++) {
+            // 0x0000 or 0xeeeee
+            if (_codeHash(poolInfo.coins[i]) == bytes32(0)) {
+                continue;
+            }
             IERC20Reader coin = IERC20Reader(poolInfo.coins[i]);
-            try
-                coin.symbol()
-                    returns (string memory symbol)
-            {
-                poolInfo.symbols[i] = symbol;
-            } catch { }
-
             try
                 coin.balanceOf(address(pool))
                     returns (uint256 balance)
@@ -122,6 +189,26 @@ contract CurvePoolFactoryReader {
 
         // If theres some tokens in there
         poolInfo.hasBalance = totalBalance > 0;
+
+        poolInfo.codeHash = _codeHash(address(pool));
+    }
+
+    function _coinSymbols(address[] memory coins)
+        internal
+        view
+        returns (string[] memory symbols)
+    {
+        symbols = new string[](coins.length);
+        for (uint256 i = 0; i < coins.length; i++) {
+            (bool didSucceed, bytes memory result) = coins[i].staticcall(
+                abi.encodeWithSelector(
+                    IERC20Reader(coins[i]).symbol.selector
+                )
+            );
+            if (didSucceed && result.length > 0) {
+                symbols[i] = abi.decode(result, (string));
+            }
+        }
     }
 
     function _append(address[] memory addressArray, address addr)
@@ -135,4 +222,30 @@ contract CurvePoolFactoryReader {
         }
         newArray[newArray.length-1] = addr;
     }
+
+    function _trimCoins(address[8] memory coins)
+        internal
+        view
+        returns (address[] memory)
+    {
+        address[] memory trimmed;
+        for (uint256 i = 0; i < coins.length; i++) {
+            // Sometimes first item is address(0), ETH?
+            if (coins[i] != address(0)) {
+                trimmed = _append(trimmed, coins[i]);
+            } 
+        }
+        return trimmed;
+    }
+
+    function _codeHash(address addr)
+        internal
+        view
+        returns (bytes32)
+    {
+        bytes32 codeHash;    
+        assembly { codeHash := extcodehash(addr) }
+        return codeHash;
+    }
+
 }
--- packages/asset-swapper/src/scripts/curve_pool_factory_reader.ts
+++ packages/asset-swapper/src/scripts/curve_pool_factory_reader.ts
@@ -6,6 +6,7 @@ import { inspect } from 'util';
 
 import { artifacts } from '../artifacts';
 import { CURVE_MAINNET_INFOS } from '../utils/market_operation_utils/constants';
+import { CurveFunctionSelectors, CurveInfo } from '../utils/market_operation_utils/types';
 import { CurvePoolFactoryReaderContract } from '../wrappers';
 
 const PROVIDER = new Web3ProviderEngine();
@@ -18,42 +19,118 @@ const overrides = {
 };
 const FACTORY_READER = new CurvePoolFactoryReaderContract(QUERY_ADDRESS, PROVIDER);
 
-const getCurveCryptoFactoryPools = async (
-    address: string,
-): Promise<Array<{
+interface PoolBase {
     coins: string[];
     symbols: string[];
     pool: string;
     hasBalance: boolean;
     codeHash: string;
-}>> => {
-    const pools = await FACTORY_READER.getCryptoFactoryPools(address).callAsync(
-        { overrides },
-        BlockParamLiteral.Latest,
-    );
+}
+
+interface RegistryPool extends PoolBase {
+    underlyingCoins: string[];
+    underlyingSymbols: string[];
+    isMeta: boolean;
+}
+
+const getCurveFactoryPools = async (address: string): Promise<Array<PoolBase>> => {
+    const pools = await FACTORY_READER.getFactoryPools(address).callAsync({ overrides }, BlockParamLiteral.Latest);
     return pools;
 };
 
-const CURVE_CRYPTO_FACTORY = '0xf18056bbd320e96a48e3fbf8bc061322531aac99';
+const getCurveRegistryPools = async (address: string): Promise<Array<RegistryPool>> => {
+    const pools = await FACTORY_READER.getRegistryPools(address).callAsync({ overrides }, BlockParamLiteral.Latest);
+    return pools;
+};
+
+// Provides the address to the registry
+// https://curve.readthedocs.io/registry-address-provider.html
+const REGISTRY_ADDRESS_PROVIDER = '0x0000000022D53366457F9d5E68Ec105046FC4383';
+// get_registry on the registryAddressProvider or also the 0th address in get_address
+// 0: The main registry contract. Used to locate pools and query information about them.
+const CURVE_REGISTRY = '0x90e00ace148ca3b23ac1bc8c240c2a7dd9c2d7f5'; // 42 pools
+// 1: Aggregate getter methods for querying large data sets about a single pool. Designed for off-chain use.
+// 2: Generalized swap contract. Used for finding rates and performing exchanges.
+// 3: The metapool factory.
 const CURVE_META_FACTORY = '0xb9fc157394af804a3578134a6585c0dc9cc990d4';
+// 4: The fee distributor. Used to distribute collected fees to veCRV holders.
+// 5: ? Some other registry 0x8F942C20D02bEfc377D41445793068908E2250D0, Curve V2?
+const CURVE_V2_FACTORY = '0x8F942C20D02bEfc377D41445793068908E2250D0';
+// 6: ? Crypto Factory, 20 pools
+const CURVE_CRYPTO_FACTORY = '0xf18056bbd320e96a48e3fbf8bc061322531aac99';
+
+const ETH = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+
+/* TODO
+ * 
+FEI-3Crv $109m
+0x06cb22615BA53E60D67Bf6C341a0fD5E718E1655 is in META_FACTORY
+but not in Registry 
+ */
+
+const normalizeCoins = (coins: string[]): string[] => coins.map(c => (c === ETH ? WETH : c));
+
+const determineRegistryCurveInfos = (pool: Array<RegistryPool>): CurveInfo[] => {
+    return pool.map(p => {
+        const isUnderlying = p.underlyingCoins.length > 1 && !p.underlyingCoins.every(c => p.coins.includes(c));
+        if (p.isMeta || isUnderlying) {
+            // Meta indicates Stable + 3Crv
+            // Typically that would be
+            // coins [Stable, 3Crv]
+            // underlyingCoins [Stable, DAI, USDC, USDT]
+            const info: CurveInfo = {
+                exchangeFunctionSelector: CurveFunctionSelectors.exchange_underlying,
+                sellQuoteFunctionSelector: CurveFunctionSelectors.get_dy_underlying,
+                buyQuoteFunctionSelector: CurveFunctionSelectors.None,
+                tokens: normalizeCoins(p.underlyingCoins),
+                metaTokens: undefined,
+                poolAddress: p.pool,
+                gasSchedule: p.isMeta ? 300e3 : 600e3,
+            };
+            return info;
+        } else {
+            const info: CurveInfo = {
+                exchangeFunctionSelector: CurveFunctionSelectors.exchange,
+                sellQuoteFunctionSelector: CurveFunctionSelectors.get_dy,
+                buyQuoteFunctionSelector: CurveFunctionSelectors.None,
+                tokens: normalizeCoins(p.coins),
+                metaTokens: undefined,
+                poolAddress: p.pool,
+                gasSchedule: 200e3,
+            };
+            return info;
+        }
+    });
+};
+
+const determineFactoryCurveInfos = (pool: Array<RegistryPool>): CurveInfo[] => {
+    return [];
+};
 
 // tslint:disable no-floating-promises
 (async () => {
     try {
         // Appears to be 2 different factories, cryptoFactory pools looks like the latest and greatest
-        const cryptoFactoryPools = await getCurveCryptoFactoryPools(CURVE_CRYPTO_FACTORY);
-        const otherFactoryPools = await getCurveCryptoFactoryPools(CURVE_META_FACTORY);
-        const factoryPools = [...cryptoFactoryPools, ...otherFactoryPools];
+        const cryptoFactoryPools = await getCurveFactoryPools(CURVE_CRYPTO_FACTORY);
+        const metaFactoryPools = await getCurveRegistryPools(CURVE_META_FACTORY);
+        const v2FactoryPools = await getCurveFactoryPools(CURVE_V2_FACTORY);
+        const registryPools = await getCurveRegistryPools(CURVE_REGISTRY);
+        const factoryPools = [...cryptoFactoryPools, ...metaFactoryPools, ...v2FactoryPools, ...registryPools];
+        // console.log(Object.values(CURVE_MAINNET_INFOS));
+        // console.log('\n');
+        console.log(determineRegistryCurveInfos(registryPools));
 
         const currentlyMappedAddresses = Object.values(CURVE_MAINNET_INFOS).map(info => info.poolAddress);
 
-        const missingPools = factoryPools.filter(pool => {
-            return !currentlyMappedAddresses.includes(pool.pool) && pool.hasBalance;
-        });
+        const missingPools = _.chain(factoryPools)
+            .filter(({ pool, hasBalance }) => !currentlyMappedAddresses.includes(pool) && hasBalance)
+            .uniqBy('pool')
+            .value();
 
         // tslint:disable no-console
-        console.log(inspect(_.groupBy(missingPools, 'codeHash'), { showHidden: false, depth: null, colors: true }));
-        console.log(`Found ${missingPools.length} missing pools`);
+        // console.log(inspect(_.groupBy(missingPools, 'codeHash'), { showHidden: false, depth: null, colors: true }));
+        // console.log(`Found ${missingPools.length} missing pools`);
     } catch (e) {
         console.log(e);
         throw e;
