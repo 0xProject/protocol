f87412adb95023ba85f73b416ccf3fc8570e46ca
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2022-03-07 16:32:25 -0700

    Update v2 Job with LL rejection price difference (#140)

---
+++ migrations/1646689728252-AddLLPriceRejectionBpsToV2Job.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+const llRejectPriceDifferenceBps = new TableColumn({
+    name: 'll_reject_price_difference_bps',
+    type: 'integer',
+    isNullable: true,
+    default: null,
+});
+
+export class AddRfqMakerUriColumns1646096840712 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('rfqm_v2_jobs', llRejectPriceDifferenceBps);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('rfqm_v2_jobs', llRejectPriceDifferenceBps);
+    }
+}
