021e3911a68dd575389734a811a9cb9ce10d042e
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2022-03-16 18:03:22 -0600

    Create migration to add chain id to heartbeat table (#160)

---
+++ migrations/1647474241801-HeartbeatChainId.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+const chainIdColumn = new TableColumn({
+    name: 'chain_id',
+    type: 'integer',
+    isNullable: true,
+    default: null,
+});
+
+export class HeartbeatChainId1647474241801 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('rfqm_worker_heartbeats', chainIdColumn);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('rfqm_worker_heartbeats', chainIdColumn);
+    }
+}
