bca2d35acb6d401a4884f302a5b6c950124b0732
Byeong Min Park <42824048+byeongminP@users.noreply.github.com>
2022-03-28 11:08:51 -0700

    feat: make dummy mm bot support chainId (#172)
    * feat: add chainId support to MM
    
    * fix: apply changes
    
    * chore: lint

--- src/handlers/dummy_mm_handler.ts
+++ src/handlers/dummy_mm_handler.ts
@@ -1,29 +1,38 @@
 // tslint:disable:max-file-line-count
 // tslint:disable: prefer-function-over-method
 import { ethSignHashWithKey, OtcOrder, RfqOrder, Signature } from '@0x/protocol-utils';
-import { SubmitRequest, TakerRequestQueryParamsUnnested } from '@0x/quote-server';
+import { SubmitRequest } from '@0x/quote-server';
 import { Fee } from '@0x/quote-server/lib/src/types';
 import { BigNumber } from '@0x/utils';
 import * as express from 'express';
 import * as HttpStatus from 'http-status-codes';
 
 import { ONE_MINUTE_MS, ONE_SECOND_MS } from '../constants';
+import { QuoteServerPriceParams } from '../types';
 import { stringsToOtcOrderFields, stringsToSignature } from '../utils/rfqm_request_utils';
 
 const WETH_ROPSTEN = '0xc778417e063141139fce010982780140aa0cd5ab';
-const TOKEN_A = '0x374a16f5e686c09b0cc9e8bc3466b3b645c74aa7';
-const TOKEN_B = '0xF84830B73b2ED3C7267E7638f500110eA47FDf30';
+const TTA_ROPSTEN = '0x374a16f5e686c09b0cc9e8bc3466b3b645c74aa7';
+const TTB_ROPSTEN = '0xF84830B73b2ED3C7267E7638f500110eA47FDf30';
+
+const WMATIC_POLYGON = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270';
+const DAI_POLYGON = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063';
+
 const MM_PRIVATE_KEY = '0xf0d8f376ca991256ddb256fb7cd28d68d971b07f5c0cf62cf0294c1ff8078a90';
 const MM_ADDRESS = '0x06754422cf9f54ae0e67D42FD788B33D8eb4c5D5';
+
 const ROPSTEN_CHAIN_ID = 3;
+const POLYGON_CHAIN_ID = 137;
+
 const ONE_TOKEN = 1e18;
-const TOKEN_SET = new Set([WETH_ROPSTEN.toLocaleLowerCase(), TOKEN_A.toLowerCase(), TOKEN_B.toLowerCase()]);
 
 /**
  * This class implements handlers that are used to satisfy the MM Quote Server spec
  */
 export class DummyMMHandlers {
-    private static _parseQuoteRequest(req: express.Request): TakerRequestQueryParamsUnnested {
+    private readonly _tokenSetByChainId: Map<number | undefined, Set<string>>;
+
+    private static _parseQuoteRequest(req: express.Request): QuoteServerPriceParams {
         const {
             sellTokenAddress,
             buyTokenAddress,
@@ -35,6 +44,7 @@ export class DummyMMHandlers {
             feeToken,
             feeAmount,
             feeType,
+            chainId,
         } = req.query;
 
         const isSelling = sellAmountBaseUnits !== undefined;
@@ -48,6 +58,7 @@ export class DummyMMHandlers {
             feeToken: feeToken as string,
             feeAmount: feeAmount as string,
             feeType: feeType as string,
+            chainId: chainId as string,
         };
 
         if (isSelling) {
@@ -80,6 +91,7 @@ export class DummyMMHandlers {
             takerAmount: new BigNumber(rawOrder.takerAmount),
             maker: rawOrder.maker,
             taker: rawOrder.taker,
+            chainId: rawOrder.chainId,
         });
 
         return {
@@ -121,15 +133,32 @@ export class DummyMMHandlers {
         };
     }
 
+    constructor() {
+        this._tokenSetByChainId = new Map<number | undefined, Set<string>>();
+        this._tokenSetByChainId.set(
+            ROPSTEN_CHAIN_ID,
+            new Set([WETH_ROPSTEN.toLocaleLowerCase(), TTA_ROPSTEN.toLowerCase(), TTB_ROPSTEN.toLowerCase()]),
+        );
+        this._tokenSetByChainId.set(
+            POLYGON_CHAIN_ID,
+            new Set([WMATIC_POLYGON.toLocaleLowerCase(), DAI_POLYGON.toLocaleLowerCase()]),
+        );
+    }
+
     /**
      * Simple pricing that always returns 1:1 v1 prices
      */
     public async getPriceV1Async(req: express.Request, res: express.Response): Promise<void> {
-        const requestParams = DummyMMHandlers._parseQuoteRequest(req);
-        const { sellTokenAddress, buyTokenAddress, sellAmountBaseUnits, buyAmountBaseUnits } = requestParams;
+        const params = DummyMMHandlers._parseQuoteRequest(req);
+        const tokenSet = this._tokenSetByChainId.get(Number(params.chainId));
+        const { sellTokenAddress, buyTokenAddress, sellAmountBaseUnits, buyAmountBaseUnits } = params;
 
         // Check tokens
-        if (!TOKEN_SET.has(sellTokenAddress.toLowerCase()) || !TOKEN_SET.has(buyTokenAddress.toLowerCase())) {
+        if (
+            !tokenSet ||
+            !tokenSet.has(sellTokenAddress.toLowerCase()) ||
+            !tokenSet.has(buyTokenAddress.toLowerCase())
+        ) {
             res.status(HttpStatus.NO_CONTENT).send({});
             return;
         }
@@ -162,11 +191,16 @@ export class DummyMMHandlers {
      * Simple pricing that always returns 1:1 v2 prices
      */
     public async getPriceV2Async(req: express.Request, res: express.Response): Promise<void> {
-        const requestParams = DummyMMHandlers._parseQuoteRequest(req);
-        const { sellTokenAddress, buyTokenAddress, sellAmountBaseUnits, buyAmountBaseUnits } = requestParams;
+        const params = DummyMMHandlers._parseQuoteRequest(req);
+        const tokenSet = this._tokenSetByChainId.get(Number(params.chainId));
+        const { sellTokenAddress, buyTokenAddress, sellAmountBaseUnits, buyAmountBaseUnits } = params;
 
         // Check tokens
-        if (!TOKEN_SET.has(sellTokenAddress.toLowerCase()) || !TOKEN_SET.has(buyTokenAddress.toLowerCase())) {
+        if (
+            !tokenSet ||
+            !tokenSet.has(sellTokenAddress.toLowerCase()) ||
+            !tokenSet.has(buyTokenAddress.toLowerCase())
+        ) {
             res.status(HttpStatus.NO_CONTENT).send({});
             return;
         }
@@ -200,12 +234,17 @@ export class DummyMMHandlers {
      * Simple quoting that always signs an order 1:1 for RFQM v1
      */
     public async getQuoteV1Async(req: express.Request, res: express.Response): Promise<void> {
-        const requestParams = DummyMMHandlers._parseQuoteRequest(req);
+        const params = DummyMMHandlers._parseQuoteRequest(req);
+        const tokenSet = this._tokenSetByChainId.get(Number(params.chainId));
         const { sellTokenAddress, buyTokenAddress, sellAmountBaseUnits, buyAmountBaseUnits, txOrigin, takerAddress } =
-            requestParams;
+            params;
 
         // Check tokens
-        if (!TOKEN_SET.has(sellTokenAddress.toLowerCase()) || !TOKEN_SET.has(buyTokenAddress.toLowerCase())) {
+        if (
+            !tokenSet ||
+            !tokenSet.has(sellTokenAddress.toLowerCase()) ||
+            !tokenSet.has(buyTokenAddress.toLowerCase())
+        ) {
             res.status(HttpStatus.NO_CONTENT).send({});
             return;
         }
@@ -232,7 +271,7 @@ export class DummyMMHandlers {
             makerAmount: new BigNumber(amount.toString()),
             takerAmount: new BigNumber(amount.toString()),
             expiry: fiveMinLater,
-            chainId: ROPSTEN_CHAIN_ID,
+            chainId: Number(params.chainId),
             salt: now,
         });
 
