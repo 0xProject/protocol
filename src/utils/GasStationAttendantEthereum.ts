2d9292376eee7fb719a1ef512e1651113773d268
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2022-04-07 11:05:01 -0600

    feat: Create gas station attendant (#190)

---
+++ src/utils/GasStationAttendantEthereum.ts
@@ -0,0 +1,110 @@
+import { BigNumber } from '@0x/utils';
+
+import { GWEI_DECIMALS } from '../constants';
+
+import { GasOracle } from './GasOracle';
+import { GasStationAttendant, Wei, WeiPerGas } from './GasStationAttendant';
+import { calculateGasEstimate } from './rfqm_gas_estimate_utils';
+import { SubmissionContext } from './SubmissionContext';
+
+const INITIAL_MAX_PRIORITY_FEE_PER_GAS_GWEI = 2;
+const MAX_PRIORITY_FEE_PER_GAS_CAP = new BigNumber(128e9); // The maximum tip we're willing to pay
+// Retrying an EIP 1559 transaction: https://docs.alchemy.com/alchemy/guides/eip-1559/retry-eip-1559-tx
+const MAX_PRIORITY_FEE_PER_GAS_MULTIPLIER = 1.5; // Increase multiplier for tip with each resubmission cycle
+
+/**
+ * An implementation of `GasStationAttendant` designed for Ethereum Mainnet.
+ */
+export class GasStationAttendantEthereum implements GasStationAttendant {
+    private readonly _gasOracle: GasOracle;
+
+    constructor(gasOracle: GasOracle) {
+        this._gasOracle = gasOracle;
+    }
+
+    /**
+     * The Safe Balance For Trade is based on historical data as outlined here:
+     * https://0xproject.quip.com/qZdFAHLpT7JI/RFQm-healthz-System-Health-Endpoint#temp:C:cXH5851e0f15e8c4828bffc1339d
+     */
+    // tslint:disable-next-line: prefer-function-over-method
+    public async getSafeBalanceForTradeAsync(): Promise<Wei> {
+        return new BigNumber(82500000000000000);
+    }
+
+    /**
+     * Uses an estimate of the current base fee with 6
+     * 10% increases plus the "instant" maxPriorityFeePerGas
+     * as reported by the oracle.
+     *
+     * Gas amount is estimated for an unwrap of the AAVE-USDC pair.
+     */
+    public async getWorkerBalanceForTradeAsync(): Promise<WeiPerGas> {
+        const baseFee = await this._gasOracle.getBaseFeePerGasWeiAsync();
+        const instantTip = await this._gasOracle.getMaxPriorityFeePerGasWeiAsync('instant');
+
+        // Pad the baseFee for 6 10% increases
+        const baseFeePad = Math.pow(1.1, 6); // tslint:disable-line: custom-no-magic-numbers
+        const paddedBaseFee = baseFee.times(baseFeePad);
+        const gasRate = paddedBaseFee.plus(instantTip);
+
+        // Use a gas estimate of a pretty high-cost pair
+        const gasEstimate = calculateGasEstimate(
+            '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9', // AAVE
+            '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
+            'otc',
+            true,
+        );
+
+        return gasRate.times(gasEstimate);
+    }
+
+    /**
+     * Calculated by looking at historical data and seeing we average 1.5 transactions
+     * per job. This means we expect to pay 2.75 GWEI priority fee plus the base fee.
+     */
+    public async getExpectedTransactionGasRateAsync(): Promise<WeiPerGas> {
+        const baseFee = await this._gasOracle.getBaseFeePerGasWeiAsync();
+        // Currently we submit a 2 GWEI tip then multiply it by 1.5 per submission
+        // Trades take ~1.5 submissions on average, so that's 2.75 GWEI
+        const avgMaxPriorityFeePerGasRate = 2750000000;
+        return baseFee.plus(avgMaxPriorityFeePerGasRate);
+    }
+
+    /**
+     * The submission strategy starts with a maxPriorityFee of 2 GWEI and adds
+     * 2 x the base fee to get the initial maxFeePerGas.
+     */
+    public async getNextBidAsync(
+        submissionContext: SubmissionContext<any> | null,
+    ): Promise<{ maxFeePerGas: BigNumber; maxPriorityFeePerGas: BigNumber } | null> {
+        const baseFee = await this._gasOracle.getBaseFeePerGasWeiAsync();
+        if (!submissionContext) {
+            // Our first bid is 2x the base fee + 2 GWEI tip
+            const initialMaxPriorityFeePerGas = new BigNumber(INITIAL_MAX_PRIORITY_FEE_PER_GAS_GWEI).times(
+                Math.pow(10, GWEI_DECIMALS),
+            );
+            return {
+                maxPriorityFeePerGas: initialMaxPriorityFeePerGas,
+                maxFeePerGas: baseFee.times(2).plus(initialMaxPriorityFeePerGas),
+            };
+        }
+        const { maxFeePerGas: oldMaxFeePerGas, maxPriorityFeePerGas: oldMaxPriorityFeePerGas } =
+            submissionContext.maxGasFees;
+        const newMaxPriorityFeePerGas = oldMaxPriorityFeePerGas.times(MAX_PRIORITY_FEE_PER_GAS_MULTIPLIER);
+        if (newMaxPriorityFeePerGas.isGreaterThanOrEqualTo(MAX_PRIORITY_FEE_PER_GAS_CAP)) {
+            // We've reached our max; don't put in any new transactions
+            return null;
+        }
+        // The RPC nodes still need at least a 0.1 increase in both values to accept the new transaction.
+        // For the new max fee per gas, we'll take the maximum of a 0.1 increase from the last value
+        // or the value from an increase in the base fee.
+        const newMaxFeePerGas = BigNumber.max(
+            oldMaxFeePerGas.multipliedBy(1.1), // tslint:disable-line: custom-no-magic-numbers
+            baseFee.multipliedBy(2).plus(newMaxPriorityFeePerGas),
+        );
+        return {
+            maxPriorityFeePerGas: newMaxPriorityFeePerGas,
+            maxFeePerGas: newMaxFeePerGas,
+        };
+    }
+}
---
+++ test/utils/GasStationAttendantEthereumTest.ts
@@ -0,0 +1,133 @@
+import { expect } from '@0x/contracts-test-utils';
+import { BigNumber } from '@0x/utils';
+import 'mocha';
+import { instance, mock, when } from 'ts-mockito';
+
+import { RfqmV2TransactionSubmissionEntity } from '../../src/entities/RfqmV2TransactionSubmissionEntity';
+import { GasOracle } from '../../src/utils/GasOracle';
+import { GasStationAttendantEthereum } from '../../src/utils/GasStationAttendantEthereum';
+import { calculateGasEstimate } from '../../src/utils/rfqm_gas_estimate_utils';
+import { RfqBlockchainUtils } from '../../src/utils/rfq_blockchain_utils';
+import { SubmissionContext } from '../../src/utils/SubmissionContext';
+
+let gasOracleMock: GasOracle;
+
+describe('GasStationAttendantEthereum', () => {
+    before(() => {
+        gasOracleMock = mock(GasOracle);
+    });
+
+    describe('getWorkerBalanceForTradeAsync', () => {
+        it('gets the balance to trade', async () => {
+            when(gasOracleMock.getBaseFeePerGasWeiAsync()).thenResolve(new BigNumber(1000));
+            when(gasOracleMock.getMaxPriorityFeePerGasWeiAsync('instant')).thenResolve(new BigNumber(666));
+
+            const attendant = new GasStationAttendantEthereum(instance(gasOracleMock));
+
+            const workerGasToTrade = await attendant.getWorkerBalanceForTradeAsync();
+
+            // Base fee is 1000. With 6 10% increases = 1000 * (1.1)^ 6 = 1771.561
+            // Instant tip is 666
+            // Gas estimate matches the one used in the algorithm
+            const gasEstimate = calculateGasEstimate(
+                '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
+                '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+                'otc',
+                true,
+            );
+
+            expect(workerGasToTrade.toPrecision(2).toString()).to.equal(
+                // tslint:disable-next-line: custom-no-magic-numbers
+                new BigNumber(1771.561).plus(666).times(gasEstimate).toPrecision(2).toString(),
+            );
+        });
+    });
+
+    describe('getExpectedTransactionGasRateAsync', () => {
+        it('estimates the transaction gas rate', async () => {
+            when(gasOracleMock.getBaseFeePerGasWeiAsync()).thenResolve(new BigNumber(1000));
+
+            const attendant = new GasStationAttendantEthereum(instance(gasOracleMock));
+
+            const gasRate = await attendant.getExpectedTransactionGasRateAsync();
+
+            // Base fee is 1000
+            // Tip estimate is
+            const tipEstimate = new BigNumber(2750000000);
+
+            expect(gasRate.toString()).to.equal(
+                // tslint:disable-next-line: custom-no-magic-numbers
+                new BigNumber(1000).plus(tipEstimate).toString(),
+            );
+        });
+    });
+    describe('getNextBidAsync', () => {
+        it('gets an initial bid when there are no existing transactions', async () => {
+            when(gasOracleMock.getBaseFeePerGasWeiAsync()).thenResolve(new BigNumber(1000));
+
+            const attendant = new GasStationAttendantEthereum(instance(gasOracleMock));
+            const gasRate = await attendant.getNextBidAsync(null);
+
+            const initialMaxPriorityFeePerGasWei = new BigNumber(2000000000);
+            const initialMaxFeePerGasWei = new BigNumber(/* base fee */ 1000)
+                .times(2)
+                .plus(initialMaxPriorityFeePerGasWei);
+
+            expect(gasRate?.maxPriorityFeePerGas?.toString()).to.equal(initialMaxPriorityFeePerGasWei.toString());
+            expect(gasRate?.maxFeePerGas?.toString()).to.equal(initialMaxFeePerGasWei.toString());
+        });
+
+        it('calculates a resubmit bid when the base fee rises', async () => {
+            when(gasOracleMock.getBaseFeePerGasWeiAsync()).thenResolve(new BigNumber(1000));
+            when(gasOracleMock.getMaxPriorityFeePerGasWeiAsync('instant')).thenResolve(new BigNumber(666));
+            const transaction1 = new RfqmV2TransactionSubmissionEntity({
+                transactionHash: '0x1',
+                from: '0xfrom',
+                to: '0xto',
+                orderHash: '0xOrderhash',
+                nonce: 0,
+                maxFeePerGas: new BigNumber(3),
+                maxPriorityFeePerGas: new BigNumber(2),
+            });
+
+            const submissionContext = new SubmissionContext(instance(mock(RfqBlockchainUtils)), [transaction1]);
+            const attendant = new GasStationAttendantEthereum(instance(gasOracleMock));
+            const gasRate = await attendant.getNextBidAsync(submissionContext);
+
+            // Previous submission gas prices were 2 tip, 3 max
+            // new tip = 2 * 1.5 = 3
+            // Base fee is now 1000
+            // total = 2 x base fee + tip
+            // total = 2 * 1000 + 3
+            expect(gasRate?.maxPriorityFeePerGas?.toString()).to.equal(new BigNumber(3).toString());
+            expect(gasRate?.maxFeePerGas?.toString()).to.equal(new BigNumber(2003).toString());
+        });
+
+        it('calculates a resubmit bid with the minimum max fee per gas increase', async () => {
+            when(gasOracleMock.getBaseFeePerGasWeiAsync()).thenResolve(new BigNumber(0));
+            when(gasOracleMock.getMaxPriorityFeePerGasWeiAsync('instant')).thenResolve(new BigNumber(666));
+            const transaction1 = new RfqmV2TransactionSubmissionEntity({
+                transactionHash: '0x1',
+                from: '0xfrom',
+                to: '0xto',
+                orderHash: '0xOrderhash',
+                nonce: 0,
+                maxFeePerGas: new BigNumber(2002),
+                maxPriorityFeePerGas: new BigNumber(2),
+            });
+
+            const submissionContext = new SubmissionContext(instance(mock(RfqBlockchainUtils)), [transaction1]);
+            const attendant = new GasStationAttendantEthereum(instance(gasOracleMock));
+            const gasRate = await attendant.getNextBidAsync(submissionContext);
+
+            // Previous submission gas prices were 2 tip, 3 max
+            // new tip = 2 * 1.5 = 3
+
+            // Base fee is now 0, so max fee must go up by 10% so the node
+            // accepts the transacion
+            // old max fee per gas * 110% = 2002 * 1.1 = 2202.2
+            expect(gasRate?.maxPriorityFeePerGas?.toString()).to.equal(new BigNumber(3).toString());
+            expect(gasRate?.maxFeePerGas?.toString()).to.equal(new BigNumber(2202.2).toString());
+        });
+    });
+});
