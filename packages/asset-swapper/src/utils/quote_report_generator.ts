26edf14859136e25d9b3561e4c7f9c54a288c8b2
mzhu25 <mchl.zhu.96@gmail.com>
2022-04-21 13:52:13 -0700

    Add estimatedGas to ExtendedQuoteReport (#463)

--- packages/asset-swapper/src/utils/quote_report_generator.ts
+++ packages/asset-swapper/src/utils/quote_report_generator.ts
@@ -102,6 +102,7 @@ export interface ExtendedQuoteReport {
     sourcesConsidered: ExtendedQuoteReportIndexedEntryOutbound[];
     sourcesDelivered: ExtendedQuoteReportIndexedEntryOutbound[] | undefined;
     blockNumber: number | undefined;
+    estimatedGas: string;
 }
 
 export interface PriceComparisonsReport {
