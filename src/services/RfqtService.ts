356c770ca45570b6b3212c36c653d0d116cf13a1
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2022-05-04 11:34:04 -0600

    Create RfqtService with price function (#221)

---
+++ src/services/RfqtService.ts
@@ -0,0 +1,66 @@
+import { AltRfqMakerAssetOfferings } from '@0x/asset-swapper/lib/src/types';
+import { MarketOperation } from '@0x/types';
+import { BigNumber } from '@0x/utils';
+
+import { Integrator } from '../config';
+import { NULL_ADDRESS } from '../constants';
+import { QuoteRequestor, V4RFQIndicativeQuoteMM } from '../quoteRequestor/quoteRequestor';
+
+/**
+ * Central class to contain the logic to handle RFQT Trades.
+ *
+ * `"v1"` functions support `MetaTransaction` trades while
+ * `"v2"` functions (will) support `OtcOrder` trades.
+ *
+ * `v1` relies heavily on `QuoteRequestor` which has been copied over
+ * from `0x/asset-swapper`.
+ */
+export class RfqtService {
+    constructor(private readonly _quoteRequestor: QuoteRequestor) {}
+
+    /**
+     * Pass through to `QuoteRequestor::requestRfqtIndicativeQuotesAsync` to fetch
+     * indicative quotes from market makers.
+     */
+    public async getV1PricesAsync(parameters: {
+        altRfqAssetOfferings: AltRfqMakerAssetOfferings;
+        assetFillAmount: BigNumber;
+        comparisonPrice: BigNumber | undefined;
+        makerToken: string;
+        marketOperation: MarketOperation;
+        takerToken: string;
+        takerAddress: string;
+        intentOnFilling: boolean;
+        integrator: Integrator;
+    }): Promise<V4RFQIndicativeQuoteMM[]> {
+        const {
+            altRfqAssetOfferings,
+            assetFillAmount,
+            comparisonPrice,
+            integrator,
+            intentOnFilling, // tslint:disable-line boolean-naming
+            makerToken,
+            marketOperation,
+            takerAddress,
+            takerToken,
+        } = parameters;
+
+        return this._quoteRequestor.requestRfqtIndicativeQuotesAsync(
+            makerToken,
+            takerToken,
+            assetFillAmount,
+            marketOperation,
+            comparisonPrice,
+            {
+                altRfqAssetOfferings,
+                integrator,
+                intentOnFilling,
+                isIndicative: true,
+                isLastLook: false,
+                makerEndpointMaxResponseTimeMs: 600,
+                takerAddress: NULL_ADDRESS,
+                txOrigin: takerAddress,
+            },
+        );
+    }
+}
---
+++ test/services/RfqtServiceTest.ts
@@ -0,0 +1,94 @@
+import { RfqMakerAssetOfferings } from '@0x/asset-swapper/lib/src/types';
+import { MarketOperation } from '@0x/types';
+import { BigNumber } from '@0x/utils';
+import { AxiosInstance } from 'axios';
+
+import { QuoteRequestor } from '../../src/quoteRequestor/quoteRequestor';
+import { RfqtService } from '../../src/services/RfqtService';
+
+jest.mock('../../src/quoteRequestor/quoteRequestor', () => ({
+    QuoteRequestor: jest.fn().mockImplementation(() => {
+        return {
+            requestRfqtIndicativeQuotesAsync: jest.fn().mockResolvedValue([]),
+        };
+    }),
+}));
+
+// TODO (rhinodavid): Find a better way to initialize mocked classes
+// tslint:disable-next-line: no-object-literal-type-assertion
+const mockQuoteRequestor = jest.mocked(new QuoteRequestor({} as RfqMakerAssetOfferings, {} as AxiosInstance));
+
+describe('Rfqt Service', () => {
+    describe('v1', () => {
+        it('passes through calls to QuoteRequestor::requestRfqtIndicativeQuotesAsync', async () => {
+            const rfqtService = new RfqtService(mockQuoteRequestor);
+
+            await rfqtService.getV1PricesAsync({
+                altRfqAssetOfferings: {
+                    'alt-mm': [
+                        {
+                            id: 'id',
+                            baseAsset: '0xbaseasset',
+                            quoteAsset: '0xquoteasset',
+                            baseAssetDecimals: 420,
+                            quoteAssetDecimals: 69,
+                        },
+                    ],
+                },
+                assetFillAmount: new BigNumber(111),
+                comparisonPrice: new BigNumber(666),
+                makerToken: '0xmakertoken',
+                marketOperation: MarketOperation.Buy,
+                takerAddress: '0xtakeraddress',
+                takerToken: '0xtakertoken',
+                intentOnFilling: false,
+                integrator: {
+                    apiKeys: [],
+                    integratorId: 'uuid-integrator',
+                    plp: false,
+                    rfqm: false,
+                    rfqt: true,
+                    label: 'Scam Integrator 1',
+                },
+            });
+
+            const args = mockQuoteRequestor.requestRfqtIndicativeQuotesAsync.mock.calls[0];
+            expect(args).toMatchInlineSnapshot(`
+                Array [
+                  "0xmakertoken",
+                  "0xtakertoken",
+                  "111",
+                  "Buy",
+                  "666",
+                  Object {
+                    "altRfqAssetOfferings": Object {
+                      "alt-mm": Array [
+                        Object {
+                          "baseAsset": "0xbaseasset",
+                          "baseAssetDecimals": 420,
+                          "id": "id",
+                          "quoteAsset": "0xquoteasset",
+                          "quoteAssetDecimals": 69,
+                        },
+                      ],
+                    },
+                    "integrator": Object {
+                      "apiKeys": Array [],
+                      "integratorId": "uuid-integrator",
+                      "label": "Scam Integrator 1",
+                      "plp": false,
+                      "rfqm": false,
+                      "rfqt": true,
+                    },
+                    "intentOnFilling": false,
+                    "isIndicative": true,
+                    "isLastLook": false,
+                    "makerEndpointMaxResponseTimeMs": 600,
+                    "takerAddress": "0x0000000000000000000000000000000000000000",
+                    "txOrigin": "0xtakeraddress",
+                  },
+                ]
+            `);
+        });
+    });
+});
