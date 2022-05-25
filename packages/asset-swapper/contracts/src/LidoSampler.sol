5a16a209e5d8090dbc3c8944bf111928d323bec9
Kyu <kyuhyun217@gmail.com>
2022-05-25 10:00:06 -0700

    Refactor LidoSampler to reduce its bytecode size

--- packages/asset-swapper/contracts/src/LidoSampler.sol
+++ packages/asset-swapper/contracts/src/LidoSampler.sol
@@ -22,10 +22,12 @@ pragma experimental ABIEncoderV2;
 
 import "./SamplerUtils.sol";
 
+
 interface IWstETH {
     function getWstETHByStETH(uint256 _stETHAmount) external view returns (uint256);
     function getStETHByWstETH(uint256 _wstETHAmount) external view returns (uint256);
 }
+
 
 contract LidoSampler is SamplerUtils {
     struct LidoInfo {
@@ -58,6 +60,41 @@ contract LidoSampler is SamplerUtils {
             return takerTokenAmounts;
         }
 
+        return _sampleSellsForWrapped(lidoInfo, takerToken, makerToken, takerTokenAmounts);
+    }
+
+    /// @dev Sample buy quotes from Lido.
+    /// @param lidoInfo Info regarding a specific Lido deployment
+    /// @param takerToken Address of the taker token (what to sell).
+    /// @param makerToken Address of the maker token (what to buy).
+    /// @param makerTokenAmounts Maker token buy amount for each sample.
+    /// @return takerTokenAmounts Taker amounts sold at each maker token
+    ///         amount.
+    function sampleBuysFromLido(
+        LidoInfo memory lidoInfo,
+        address takerToken,
+        address makerToken,
+        uint256[] memory makerTokenAmounts
+    )
+        public
+        view
+        returns (uint256[] memory)
+    {
+        if (takerToken == lidoInfo.wethToken && makerToken == address(lidoInfo.stEthToken)) {
+            // Minting stETH is always 1:1 therefore we can just return the same amounts back.
+            return makerTokenAmounts;
+        }
+
+        // Swap out `makerToken` and `takerToken` and re-use `_sampleSellsForWrapped`.
+        return _sampleSellsForWrapped(lidoInfo, makerToken, takerToken, makerTokenAmounts);
+    }
+
+    function _sampleSellsForWrapped(
+        LidoInfo memory lidoInfo,
+        address takerToken,
+        address makerToken,
+        uint256[] memory takerTokenAmounts
+    ) private view  returns (uint256[] memory) {
         IWstETH wstETH = IWstETH(lidoInfo.wstEthToken);
         uint256 numSamples = takerTokenAmounts.length;
         uint256[] memory makerTokenAmounts = new uint256[](numSamples);
@@ -78,51 +115,5 @@ contract LidoSampler is SamplerUtils {
 
         // Returns 0 values.
         return makerTokenAmounts;
-    }
-
-    /// @dev Sample buy quotes from Lido.
-    /// @param lidoInfo Info regarding a specific Lido deployment
-    /// @param takerToken Address of the taker token (what to sell).
-    /// @param makerToken Address of the maker token (what to buy).
-    /// @param makerTokenAmounts Maker token buy amount for each sample.
-    /// @return takerTokenAmounts Taker amounts sold at each maker token
-    ///         amount.
-    function sampleBuysFromLido(
-        LidoInfo memory lidoInfo,
-        address takerToken,
-        address makerToken,
-        uint256[] memory makerTokenAmounts
-    )
-        public
-        view
-        returns (uint256[] memory)
-    {
-        _assertValidPair(makerToken, takerToken);
-
-        if (takerToken == lidoInfo.wethToken && makerToken != address(lidoInfo.stEthToken)) {
-            // Minting stETH is always 1:1 therefore we can just return the same amounts back.
-            return makerTokenAmounts;
-        }
-
-        IWstETH wstETH = IWstETH(lidoInfo.wstEthToken);
-        uint256 numSamples = makerTokenAmounts.length;
-        uint256[] memory takerTokenAmounts = new uint256[](numSamples);
-
-        if (takerToken == lidoInfo.stEthToken && makerToken == lidoInfo.wstEthToken) {
-            for (uint256 i = 0; i < numSamples; i++) {
-                takerTokenAmounts[i] = wstETH.getStETHByWstETH(makerTokenAmounts[i]);
-            }
-            return takerTokenAmounts;
-        }
-
-        if (takerToken == lidoInfo.wstEthToken && makerToken == lidoInfo.stEthToken) {
-            for (uint256 i = 0; i < numSamples; i++) {
-                takerTokenAmounts[i] = wstETH.getWstETHByStETH(makerTokenAmounts[i]);
-            }
-            return takerTokenAmounts;
-        }
-
-        // Returns 0 values.
-        return takerTokenAmounts;
     }
 }
