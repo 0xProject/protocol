2e824e6a3f120b3f24b73ea152292908ca2328fc
Kyu <kyuhyun217@gmail.com>
2022-06-02 13:49:24 -0700

    Enable KyberDmm on Polygon (#870)

--- src/config.ts
+++ src/config.ts
@@ -524,7 +524,7 @@ const EXCLUDED_SOURCES = (() => {
         case ChainId.BSC:
             return [ERC20BridgeSource.MultiBridge, ERC20BridgeSource.Native];
         case ChainId.Polygon:
-            return [ERC20BridgeSource.MultiBridge, ERC20BridgeSource.Native, ERC20BridgeSource.KyberDmm];
+            return [ERC20BridgeSource.MultiBridge, ERC20BridgeSource.Native];
         case ChainId.Avalanche:
             return [ERC20BridgeSource.MultiBridge, ERC20BridgeSource.Native];
         case ChainId.Celo:
