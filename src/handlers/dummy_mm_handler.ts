68fe64592a4022b52ab9cbd12ff0edef6c945535
phil-ociraptor <phil@0x.org>
2022-06-02 20:29:30 -0500

    feat: allow Dummy MM to provide liquidity on USDT/USDC for Polygon (#253)

--- src/handlers/dummy_mm_handler.ts
+++ src/handlers/dummy_mm_handler.ts
@@ -1,4 +1,5 @@
 // tslint:disable:max-file-line-count
+// tslint:disable: custom-no-magic-numbers
 // tslint:disable: prefer-function-over-method
 import { ethSignHashWithKey, OtcOrder, RfqOrder, Signature } from '@0x/protocol-utils';
 import { SubmitRequest } from '@0x/quote-server';
@@ -13,18 +14,34 @@ import { stringsToOtcOrderFields, stringsToSignature } from '../utils/rfqm_reque
 
 const WETH_ROPSTEN = '0xc778417e063141139fce010982780140aa0cd5ab';
 const TTA_ROPSTEN = '0x374a16f5e686c09b0cc9e8bc3466b3b645c74aa7';
-const TTB_ROPSTEN = '0xF84830B73b2ED3C7267E7638f500110eA47FDf30';
+const TTB_ROPSTEN = '0xf84830b73b2ed3c7267e7638f500110ea47fdf30';
 
-const WMATIC_POLYGON = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270';
-const DAI_POLYGON = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063';
+const WMATIC_POLYGON = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270';
+const DAI_POLYGON = '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063';
+const USDC_POLYGON = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174';
+const USDT_POLYGON = '0xc2132d05d31c914a87c6611c10748aeb04b58e8f';
 
 const MM_PRIVATE_KEY = '0xf0d8f376ca991256ddb256fb7cd28d68d971b07f5c0cf62cf0294c1ff8078a90';
-const MM_ADDRESS = '0x06754422cf9f54ae0e67D42FD788B33D8eb4c5D5';
+const MM_ADDRESS = '0x06754422cf9f54ae0e67d42fd788b33d8eb4c5d5';
 
 const ROPSTEN_CHAIN_ID = 3;
 const POLYGON_CHAIN_ID = 137;
 
-const ONE_TOKEN = 1e18;
+const tokenToDecimals: Record<string, number> = {
+    [WETH_ROPSTEN]: 18,
+    [TTA_ROPSTEN]: 18,
+    [TTB_ROPSTEN]: 18,
+    [WMATIC_POLYGON]: 18,
+    [DAI_POLYGON]: 18,
+    [USDC_POLYGON]: 6,
+    [USDT_POLYGON]: 6,
+};
+
+const whitelistedIntegrators = new Set([
+    '74188355-c85b-4f18-9de4-6dec3ec61b8d',
+    '301e83b5-61f4-409b-bc61-8886dd56189d',
+    '1c016c87-3128-4f78-b0f5-e90038d165ef',
+]);
 
 /**
  * This class implements handlers that are used to satisfy the MM Quote Server spec
@@ -32,7 +49,7 @@ const ONE_TOKEN = 1e18;
 export class DummyMMHandlers {
     private readonly _tokenSetByChainId: Map<number | undefined, Set<string>>;
 
-    private static _parseQuoteRequest(req: express.Request): QuoteServerPriceParams {
+    private static _parseQuoteRequest(req: express.Request): QuoteServerPriceParams & { integratorId?: string } {
         const {
             sellTokenAddress,
             buyTokenAddress,
@@ -47,6 +64,8 @@ export class DummyMMHandlers {
             chainId,
         } = req.query;
 
+        const integratorId = req.headers['0x-integrator-id'];
+
         const isSelling = sellAmountBaseUnits !== undefined;
 
         const BASE_REQUEST = {
@@ -59,6 +78,7 @@ export class DummyMMHandlers {
             feeAmount: feeAmount as string,
             feeType: feeType as string,
             chainId: chainId as string,
+            integratorId: integratorId as string,
         };
 
         if (isSelling) {
@@ -141,7 +161,7 @@ export class DummyMMHandlers {
         );
         this._tokenSetByChainId.set(
             POLYGON_CHAIN_ID,
-            new Set([WMATIC_POLYGON.toLocaleLowerCase(), DAI_POLYGON.toLocaleLowerCase()]),
+            new Set([USDC_POLYGON.toLowerCase(), USDT_POLYGON.toLowerCase()]),
         );
     }
 
@@ -151,7 +171,12 @@ export class DummyMMHandlers {
     public async getPriceV1Async(req: express.Request, res: express.Response): Promise<void> {
         const params = DummyMMHandlers._parseQuoteRequest(req);
         const tokenSet = this._tokenSetByChainId.get(Number(params.chainId));
-        const { sellTokenAddress, buyTokenAddress, sellAmountBaseUnits, buyAmountBaseUnits } = params;
+        const { sellTokenAddress, buyTokenAddress, sellAmountBaseUnits, buyAmountBaseUnits, integratorId } = params;
+
+        // Check integrator
+        if (!integratorId || !whitelistedIntegrators.has(integratorId)) {
+            return;
+        }
 
         // Check tokens
         if (
@@ -193,7 +218,12 @@ export class DummyMMHandlers {
     public async getPriceV2Async(req: express.Request, res: express.Response): Promise<void> {
         const params = DummyMMHandlers._parseQuoteRequest(req);
         const tokenSet = this._tokenSetByChainId.get(Number(params.chainId));
-        const { sellTokenAddress, buyTokenAddress, sellAmountBaseUnits, buyAmountBaseUnits } = params;
+        const { sellTokenAddress, buyTokenAddress, sellAmountBaseUnits, buyAmountBaseUnits, integratorId } = params;
+
+        // Check integrator
+        if (!integratorId || !whitelistedIntegrators.has(integratorId)) {
+            return;
+        }
 
         // Check tokens
         if (
@@ -236,8 +266,20 @@ export class DummyMMHandlers {
     public async getQuoteV1Async(req: express.Request, res: express.Response): Promise<void> {
         const params = DummyMMHandlers._parseQuoteRequest(req);
         const tokenSet = this._tokenSetByChainId.get(Number(params.chainId));
-        const { sellTokenAddress, buyTokenAddress, sellAmountBaseUnits, buyAmountBaseUnits, txOrigin, takerAddress } =
-            params;
+        const {
+            sellTokenAddress,
+            buyTokenAddress,
+            sellAmountBaseUnits,
+            buyAmountBaseUnits,
+            txOrigin,
+            takerAddress,
+            integratorId,
+        } = params;
+
+        // Check integrator
+        if (!integratorId || !whitelistedIntegrators.has(integratorId)) {
+            return;
+        }
 
         // Check tokens
         if (
@@ -257,6 +299,14 @@ export class DummyMMHandlers {
         // Tokens
         const takerToken = sellTokenAddress as string;
         const makerToken = buyTokenAddress as string;
+
+        // Enforce a 5 unit maximum
+        const oneUnit = 10 ** tokenToDecimals[takerToken];
+        const fiveUnits = oneUnit * 5;
+        if (amount.gt(fiveUnits)) {
+            res.status(HttpStatus.NO_CONTENT).send({});
+            return;
+        }
 
         // Expiry
         const now = new BigNumber(Date.now());
@@ -291,15 +341,20 @@ export class DummyMMHandlers {
     /**
      * Approves even amounts and rejects odd amounts, ignoring decimals
      *
-     * Examples:
+     * Example for WETH:
      * - 1.000_000_000_000_000_000 is considered odd!
      * - 2.000_000_000_000_000_001 is considered even!
      */
     public async submitAsync(req: express.Request, res: express.Response): Promise<void> {
         const requestParams = DummyMMHandlers._parseSubmitRequest(req);
         const { order, fee, orderHash, takerTokenFillAmount } = requestParams;
+        const decimals = tokenToDecimals[order.takerToken];
 
-        const isEven = order.takerAmount.div(ONE_TOKEN).integerValue().mod(2).eq(0);
+        const isEven = order.takerAmount
+            .div(10 ** decimals)
+            .integerValue()
+            .mod(2)
+            .eq(0);
         const response = {
             fee,
             proceedWithFill: isEven,
@@ -313,15 +368,20 @@ export class DummyMMHandlers {
     /**
      * Approves even amounts and rejects odd amounts, ignoring decimals
      *
-     * Examples:
+     * Example for WETH:
      * - 1.000_000_000_000_000_000 is considered odd!
      * - 2.000_000_000_000_000_001 is considered even!
      */
     public async signAsync(req: express.Request, res: express.Response): Promise<void> {
         const requestParams = DummyMMHandlers._parseSignRequest(req);
         const { order, feeAmount, orderHash } = requestParams;
+        const decimals = tokenToDecimals[order.takerToken];
 
-        const isEven = order.takerAmount.div(ONE_TOKEN).integerValue().mod(2).eq(0);
+        const isEven = order.takerAmount
+            .div(10 ** decimals)
+            .integerValue()
+            .mod(2)
+            .eq(0);
 
         // Reject
         if (!isEven) {
