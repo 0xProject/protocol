9b131199ade048d3909da4ad29f69e8f5b35f21e
Noah Khamliche <noah@0xproject.com>
2022-06-02 20:14:02 -0400

    add weth/eth wrap/unwrap support for bancorv3

--- packages/asset-swapper/src/utils/market_operation_utils/orders.ts
+++ packages/asset-swapper/src/utils/market_operation_utils/orders.ts
@@ -208,6 +208,8 @@ export function getErc20BridgeSourceToBridgeSource(source: ERC20BridgeSource): s
             return encodeBridgeSourceId(BridgeProtocol.Platypus, 'Platypus');
         case ERC20BridgeSource.MeshSwap:
             return encodeBridgeSourceId(BridgeProtocol.UniswapV2, 'MeshSwap');
+        case ERC20BridgeSource.BancorV3:
+            return encodeBridgeSourceId(BridgeProtocol.BancorV3, 'BancorV3');
         default:
             throw new Error(AggregationError.NoBridgeForSource);
     }
@@ -387,7 +389,7 @@ export function createBridgeDataForBridgeOrder(order: OptimizedMarketBridgeOrder
             break;
         case ERC20BridgeSource.BancorV3:
             const bancorV3FillData = (order as OptimizedMarketBridgeOrder<BancorFillData>).fillData;
-            bridgeData = encoder.encode([bancorV3FillData.networkAddress, bancorFillData.path, bancor]);
+            bridgeData = encoder.encode([bancorV3FillData.networkAddress, bancorV3FillData.path]);
             break;
         default:
             throw new Error(AggregationError.NoBridgeForSource);
