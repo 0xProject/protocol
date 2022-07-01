6fc2704827fc19d667edfff4dca82866c9fd0a1b
phil-ociraptor <phil@0x.org>
2022-07-01 18:11:00 -0400

    chore: add ABI for ChildERC20 (#278)

---
+++ src/abis/index.ts
@@ -0,0 +1,6 @@
+// PolygonBridgedERC20.json is from https://github.com/maticnetwork/pos-portal/blob/master/artifacts/ChildERC20.json
+import * as polygonBridgedERC20 from './PolygonBridgedERC20.json';
+
+export const abis = {
+    polygonBridgedERC20: polygonBridgedERC20.abi,
+};
