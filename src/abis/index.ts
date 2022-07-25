4ebb1fabd871aa573ab9839449d21f921291a062
Byeong Min Park <42824048+byeongminP@users.noreply.github.com>
2022-07-25 15:08:36 -0700

    chore: add EIP-2612 permit abi (#317)
    * chore: add permit abi
    
    * chore: rename file

--- src/abis/index.ts
+++ src/abis/index.ts
@@ -1,6 +1,9 @@
+// PermitERC20.json is extracted from https://github.com/Uniswap/v2-core/blob/master/contracts/UniswapV2ERC20.sol
+import * as permitERC20 from './PermitERC20.json';
 // PolygonBridgedERC20.json is from https://github.com/maticnetwork/pos-portal/blob/master/artifacts/ChildERC20.json
 import * as polygonBridgedERC20 from './PolygonBridgedERC20.json';
 
 export const abis = {
     polygonBridgedERC20: polygonBridgedERC20.abi,
+    permitERC20: permitERC20.abi,
 };
