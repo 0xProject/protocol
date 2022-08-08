4dae8de1b60a24e5a0fd76c7d4ee2809694d166c
phil-ociraptor <phil@0x.org>
2022-08-08 11:46:50 -0500

    feat: add Foundry support to contracts/zero-ex (#534)
    Co-authored-by: Michael Zhu <mchl.zhu.96@gmail.com>

---
+++ contracts/zero-ex/contracts/test/foundry/ContractTest.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+
+  Copyright 2022 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+pragma solidity ^0.6;
+
+import "forge-std/Test.sol";
+
+contract ContractTest is Test {
+    function setUp() public {}
+
+    function testExample() public {
+      assertTrue(true);
+    }
+}
