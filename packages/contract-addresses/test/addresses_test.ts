02b296e59c8ff0d12a115d68f01d9cc398d82870
Kyu <kyuhyun217@gmail.com>
2022-08-10 16:22:37 +0900

    Install test dev dependencies and configure circleci

---
+++ packages/contract-addresses/test/addresses_test.ts
@@ -0,0 +1,14 @@
+import * as chai from 'chai';
+import 'mocha';
+
+const expect = chai.expect;
+
+describe('addresses.json sanity test', () => {
+    it('all addresses are lowercased', async () => {
+        expect(true).to.be.false;
+    });
+
+    it('all transformer addresses are valid', async () => {
+        expect(true).to.be.true;
+    });
+});
