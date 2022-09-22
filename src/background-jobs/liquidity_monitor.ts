0ac80159caff5e9e54f30973a2e2964b1960cf8d
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2022-09-22 17:55:02 -0600

    Refactor liquidity monitor job to be extendable (#443)

--- src/background-jobs/liquidity_monitor.ts
+++ src/background-jobs/liquidity_monitor.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { Job, Queue } from 'bullmq';
 import { Gauge } from 'prom-client';
 
@@ -15,7 +15,7 @@ import { BackgroundJobBlueprint } from './blueprint';
 
 /**
  * The liquidity monitor runs periodically to create a history
- * of liquity being served by 0x products.
+ * of liquidity being served by 0x products.
  */
 
 const QUEUE_NAME = 'liquidity-monitor';
@@ -70,11 +70,78 @@ const LIQUIDITY_MONITOR_GAUGE = new Gauge({
         'source', // can be specific market maker or product, i.e. zero/g, rfqm, etc.
         'chain_id',
     ],
-    help: 'Gauge indicating whether liquidity is available for the pair/source/chain combination',
+    help: 'Gauge indicating whether liquidity is available for the pair/source/chain combination. Value is a Status type.',
 });
 
 /**
- * Pushes a liquidity monitor job to the work queue.
+ * The interface which defines all the necessary information
+ * needed to create a Grafana state timeline panel for a sepecific
+ * pair on a specific chain:
+ *         _____________________________________
+ *         |        WMATIC-USDC (polygon)       |
+ *         |------------------------------------|
+ *         | zero/g   | xxxxxxx   xxxxxxxxxxxx  |
+ *         | altonomy | xxxxxxxxxxxxxxxxxxxxxx  |
+ *         | jump     |         xxxxxxxxxx      |
+ *         |------------------------------------|
+ *
+ * Each item in `checks` will result in a new row in the panel.
+ */
+interface PairCheck {
+    buyAmount: string;
+    buyToken: string; // contract address
+    chainId: number;
+    checks: { source: string; checkerFunction: CheckerFunction }[];
+    feeAmount: string;
+    pair: string; // buy token - sell token, e.g. WETH-USDT
+    sellToken: string; // contract address
+    takerAddress: string;
+}
+
+/**
+ * A function containing the information needed to execute a liquidity
+ * check against a 0x API endpoint or a market maker quote server endpoint
+ */
+type CheckerFunction = (parameters: {
+    axiosInstance: AxiosInstance;
+    buyAmount: string;
+    buyToken: string;
+    chainId: number;
+    feeAmount?: string;
+    sellToken: string;
+    takerAddress: string;
+}) => Promise<{ liquidityAvailable: boolean }>;
+
+/**
+ * Creates a checker function to check top-level 0x API endpoints
+ * like rfqm or zero/g
+ */
+function createCheckProduct(url: string): CheckerFunction {
+    return async (params: {
+        axiosInstance: AxiosInstance;
+        buyAmount: string;
+        buyToken: string;
+        chainId: number;
+        sellToken: string;
+        takerAddress: string;
+    }): Promise<{ liquidityAvailable: boolean }> => {
+        const response = await params.axiosInstance.get<{ liquidityAvailable: boolean }>(url, {
+            params: {
+                buyToken: params.buyToken,
+                sellToken: params.sellToken,
+                buyAmount: params.buyAmount,
+                takerAddress: params.takerAddress,
+            },
+            headers: {
+                '0x-chain-id': params.chainId.toString(),
+            },
+        });
+        return response.data;
+    };
+}
+
+/**
+ * Pushes a liquidity monitor job to the work queue
  */
 async function createAsync(
     queue: Queue,
@@ -88,7 +155,7 @@ async function createAsync(
 }
 
 /**
- * The logic that runs to check liquidity and update the gauge.
+ * The logic that runs to check liquidity and update the gauge
  */
 async function processAsync(
     job: Job<BackgroundJobLiquidityMonitorData, BackgroundJobLiquidityMonitorResult>,
@@ -100,45 +167,72 @@ async function processAsync(
 
     const axiosInstance = axios.create({ headers: { '0x-api-key': devApiKey } });
 
-    await job.updateProgress(0);
     logger.info(
         { jobName: job.name, queue: job.queueName, data: job.data, timestamp: Date.now() },
         'Processing liquidity monitor job',
     );
 
-    // Starting off simple here with a handcoded check of RFQm and zero/g polygon WMATIC->USDC
-    // Later, we can add more pairs and more sources.
-    const wmaticPolygon = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270';
-    const usdcPowPolygon = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174';
+    /////////////////////////////////////////////////////
+    // ADD NEW CHECKS HERE
+    /////////////////////////////////////////////////////
 
-    [
-        { source: 'rfqm', url: 'https://polygon.api.0x.org/rfqm/v1/quote' },
-        { source: 'zero/g', url: 'https://polygon.api.0x.org/zero-gas/swap/v1/quote' },
-    ].forEach(async ({ source, url }) => {
-        try {
-            const { data: response } = await axiosInstance.get<{ liquidityAvailable: boolean }>(url, {
-                params: {
-                    buyToken: wmaticPolygon,
-                    sellToken: usdcPowPolygon,
-                    buyAmount: '1000000',
-                    takerAddress: '0x4Ea754349AcE5303c82f0d1D491041e042f2ad22',
-                },
-                headers: {
-                    '0x-chain-id': '137',
-                },
-            });
-            if (!response.hasOwnProperty('liquidityAvailable')) {
-                throw new Error('Malformed response');
+    const wmaticUsdcPolygonCheck = {
+        buyAmount: '1000000',
+        buyToken: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+        chainId: 137,
+        checks: [
+            {
+                checkerFunction: createCheckProduct('https://polygon.api.0x.org/rfqm/v1/quote'),
+                source: 'rfqm',
+            },
+            {
+                checkerFunction: createCheckProduct('https://polygon.api.0x.org/zero-gas/swap/v1/quote'),
+                source: 'zero/g',
+            },
+        ],
+        feeAmount: '0',
+        pair: 'WMATIC-USDC',
+        sellToken: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+        takerAddress: '0x4Ea754349AcE5303c82f0d1D491041e042f2ad22',
+    };
+
+    const pairChecks: PairCheck[] = [wmaticUsdcPolygonCheck];
+
+    /////////////////////////////////////////////////////
+
+    let progress = 0;
+    await job.updateProgress(0);
+    const totalCheckCount = pairChecks.flatMap((pairCheck) => pairCheck.checks).length;
+
+    pairChecks.forEach((pairCheck) => {
+        pairCheck.checks.forEach(async (check) => {
+            try {
+                const result = await check.checkerFunction({
+                    axiosInstance,
+                    buyAmount: pairCheck.buyAmount,
+                    buyToken: pairCheck.buyToken,
+                    chainId: pairCheck.chainId,
+                    sellToken: pairCheck.sellToken,
+                    takerAddress: pairCheck.takerAddress,
+                });
+                if (!result.hasOwnProperty('liquidityAvailable')) {
+                    throw new Error('Malformed response');
+                }
+                const { liquidityAvailable: isLiquidityAvailable } = result;
+                LIQUIDITY_MONITOR_GAUGE.labels(pairCheck.pair, check.source, pairCheck.chainId.toString()).set(
+                    isLiquidityAvailable ? Status.LiquidityAvailable : Status.NoLiquidityAvailable,
+                );
+            } catch (e) {
+                const errorJson = axios.isAxiosError(e) ? e.toJSON() : null;
+                logger.error({ message: e.message, axiosErrorJson: errorJson }, 'Liquidity check failed');
+                LIQUIDITY_MONITOR_GAUGE.labels(pairCheck.pair, check.source, pairCheck.chainId.toString()).set(
+                    Status.Fail,
+                );
+            } finally {
+                progress += 100 / totalCheckCount; // tslint:disable-line: custom-no-magic-numbers
+                await job.updateProgress(progress);
             }
-            const { liquidityAvailable: isLiquidityAvailable } = response;
-            LIQUIDITY_MONITOR_GAUGE.labels('WMATIC-USDC', source, '137').set(
-                isLiquidityAvailable ? Status.LiquidityAvailable : Status.NoLiquidityAvailable,
-            );
-        } catch (e) {
-            const errorJson = axios.isAxiosError(e) ? e.toJSON() : null;
-            logger.error({ message: e.message, axiosErrorJson: errorJson }, 'Liquidity check failed');
-            LIQUIDITY_MONITOR_GAUGE.labels('WMATIC-USDC', source, '137').set(Status.Fail);
-        }
+        });
     });
 
     // tslint:disable-next-line: custom-no-magic-numbers
