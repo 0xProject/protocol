cfa084c7d1813d23832ff14334baf1bd375c32d9
Vic <vic@0x.org>
2022-10-11 14:49:12 -0700

    feat: Add circular dependency check on lint (#475)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     root: true,
     ignorePatterns: ['node_modules/**/*', 'lib/**/*'],
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'eslint-plugin-jest'],
+    plugins: ['@typescript-eslint', 'eslint-plugin-jest', 'eslint-plugin-import'],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
@@ -11,5 +11,17 @@ module.exports = {
     ],
     rules: {
         "jest/no-focused-tests": "error",
+        "import/no-cycle": "error",
+    },
+    // Instructions from https://github.com/import-js/eslint-plugin-import and https://github.com/import-js/eslint-import-resolver-typescript#configuration
+    "settings": {
+        "import/parsers": {
+          "@typescript-eslint/parser": [".ts"]
+        },
+        "import/resolver": {
+          "typescript": {
+            "alwaysTryTypes": true,
+          }
+        }
     }
 };
