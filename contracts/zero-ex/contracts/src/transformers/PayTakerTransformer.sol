4ace7c8c43341956ea7c1207cd53749749d74b51
Kyu <kyuhyun217@gmail.com>
2022-10-28 11:39:22 -0700

    fix: Migrate transformers to smart contract compatible `unsafeTransformerTransfer` [TKR-587] (#594)
    * Use `call` instead of `transfer` in `LibERC20Transformer`
    
    * Since `transfer` only forwards 2300 gas it can cause an `out of gas`
    revert when the receipient is a smart contract.
    
    * Add `unsafeTransformerTransfer` and migrate `transformers` to use
    `unsafeTransformerTransfer` instead of `transformerTransfer`
    
    * Update .prettierignore

--- contracts/zero-ex/contracts/src/transformers/PayTakerTransformer.sol
+++ contracts/zero-ex/contracts/src/transformers/PayTakerTransformer.sol
@@ -65,7 +65,7 @@ contract PayTakerTransformer is Transformer {
                 amount = data.tokens[i].getTokenBalanceOf(address(this));
             }
             if (amount != 0) {
-                data.tokens[i].transformerTransfer(context.recipient, amount);
+                data.tokens[i].unsafeTransformerTransfer(context.recipient, amount);
             }
         }
         return LibERC20Transformer.TRANSFORMER_SUCCESS;
