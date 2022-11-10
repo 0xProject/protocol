6faf447d76fb0c46a1ad6d30a4c651b440e9faee
phil-ociraptor <phil@0x.org>
2022-11-09 16:48:39 -0800

    chore: update EIP-712 registry for polygon and ethereum (#500)

--- src/eip712registry/index.ts
+++ src/eip712registry/index.ts
@@ -1,10 +1,12 @@
 import { Eip712Domain } from '../types';
 
+import * as ethereum from './ethereum.json';
 import * as polygon from './polygon.json';
 
 export const EIP_712_REGISTRY: Record<
     number,
     Record<string, { kind: string; domain: Eip712Domain; domainSeparator: string }>
 > = {
+    1: ethereum,
     137: polygon,
 };
