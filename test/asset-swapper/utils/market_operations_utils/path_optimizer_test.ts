d307ddda2087f2a3f76e1df4f360f629c25e7a2d
Kyu <kyuhyun217@gmail.com>
2022-11-21 13:05:04 -0800

    Add more PathOptimizer tests (#1077)
    * Single native source routing
    * DEX multiplex routing

--- test/asset-swapper/utils/market_operations_utils/path_optimizer_test.ts
+++ test/asset-swapper/utils/market_operations_utils/path_optimizer_test.ts
@@ -1,13 +1,26 @@
 import * as chai from 'chai';
 import * as _ from 'lodash';
 import 'mocha';
-import { ChainId, IdentityFillAdjustor, MarketOperation } from '../../../../src/asset-swapper';
+import {
+    ChainId,
+    FillQuoteTransformerOrderType,
+    IdentityFillAdjustor,
+    MarketOperation,
+    SignatureType,
+} from '../../../../src/asset-swapper';
 import { PathOptimizer } from '../../../../src/asset-swapper/utils/market_operation_utils/path_optimizer';
-import { ERC20BridgeSource, FeeSchedule } from '../../../../src/asset-swapper/utils/market_operation_utils/types';
+import {
+    DexSample,
+    ERC20BridgeSource,
+    FeeSchedule,
+    FillData,
+    MultiHopFillData,
+} from '../../../../src/asset-swapper/utils/market_operation_utils/types';
 import { BigNumber } from '@0x/utils';
 import { chaiSetup } from '../chai_setup';
 import { PathPenaltyOpts } from '../../../../src/asset-swapper/utils/market_operation_utils/path';
 import { ONE_ETHER } from '../../../../src/asset-swapper/utils/market_operation_utils/constants';
+import { NativeOrderWithFillableAmounts } from '../../../../src/asset-swapper/types';
 
 chaiSetup.configure();
 const expect = chai.expect;
@@ -26,149 +39,268 @@ const NO_OP_PATH_PENALTY_OPTS: PathPenaltyOpts = {
 };
 
 describe('PathOptimizer', () => {
-    // TODO: add more test (e.g. multiplex, native order).
+    describe('Single source type (DEX, MultiHop, Native) routing', () => {
+        it('Returns the best single DEX source path', async () => {
+            const pathOptimizer = new PathOptimizer({
+                side: MarketOperation.Sell,
+                chainId: ChainId.Mainnet,
+                feeSchedule: NO_OP_FEE_SCHEDULE,
+                neonRouterNumSamples: 5,
+                fillAdjustor: new IdentityFillAdjustor(),
+                pathPenaltyOpts: NO_OP_PATH_PENALTY_OPTS,
+                inputAmount: ONE_ETHER.times(4),
+            });
 
-    it('Returns the best single source path', async () => {
-        const pathOptimizer = new PathOptimizer({
-            side: MarketOperation.Sell,
-            chainId: ChainId.Mainnet,
-            feeSchedule: NO_OP_FEE_SCHEDULE,
-            neonRouterNumSamples: 4,
-            fillAdjustor: new IdentityFillAdjustor(),
-            pathPenaltyOpts: NO_OP_PATH_PENALTY_OPTS,
-            inputAmount: ONE_ETHER.times(4),
+            // UniswapV3 is the best path.
+            const path = pathOptimizer.findOptimalPathFromSamples(
+                [
+                    createDexSamples({
+                        source: ERC20BridgeSource.UniswapV3,
+                        inputsInEther: [1, 2, 3, 4],
+                        outputsInEther: [2, 4, 6, 8],
+                    }),
+                    createDexSamples({
+                        source: ERC20BridgeSource.SushiSwap,
+                        inputsInEther: [1, 2, 3, 4],
+                        outputsInEther: [1, 2, 3, 4],
+                    }),
+                ],
+                [],
+                [],
+            );
+
+            if (path === undefined) {
+                expect(path).to.be.not.undefined();
+                return;
+            }
+
+            expect(path.fills).lengthOf(1);
+
+            const fill = path?.fills[0];
+            expect(fill.source).eq(ERC20BridgeSource.UniswapV3);
+            expect(fill.input).bignumber.eq(ONE_ETHER.times(4));
+            expect(fill.output).bignumber.eq(ONE_ETHER.times(8));
         });
 
-        // UniswapV3 is the best path.
-        // UniswapV3: 4 -> 8
-        // SushiSwap: 4 -> 4
-        const path = pathOptimizer.findOptimalPathFromSamples(
-            [
+        it('Returns the multihop path', async () => {
+            const pathOptimizer = new PathOptimizer({
+                side: MarketOperation.Sell,
+                chainId: ChainId.Mainnet,
+                feeSchedule: NO_OP_FEE_SCHEDULE,
+                neonRouterNumSamples: 5,
+                fillAdjustor: new IdentityFillAdjustor(),
+                pathPenaltyOpts: NO_OP_PATH_PENALTY_OPTS,
+                inputAmount: ONE_ETHER.times(4),
+            });
+
+            // Multihop is the best path.
+            const path = pathOptimizer.findOptimalPathFromSamples(
                 [
-                    { source: ERC20BridgeSource.UniswapV3, fillData: {}, input: ONE_ETHER, output: ONE_ETHER.times(2) },
-                    {
+                    createDexSamples({
                         source: ERC20BridgeSource.UniswapV3,
-                        fillData: {},
-                        input: ONE_ETHER.times(2),
-                        output: ONE_ETHER.times(4),
-                    },
-                    {
-                        source: ERC20BridgeSource.UniswapV3,
-                        fillData: {},
-                        input: ONE_ETHER.times(3),
-                        output: ONE_ETHER.times(6),
-                    },
-                    {
-                        source: ERC20BridgeSource.UniswapV3,
-                        fillData: {},
-                        input: ONE_ETHER.times(4),
-                        output: ONE_ETHER.times(8),
-                    },
+                        inputsInEther: [1, 2, 3, 4],
+                        outputsInEther: [2, 4, 6, 8],
+                    }),
                 ],
                 [
-                    { source: ERC20BridgeSource.SushiSwap, fillData: {}, input: ONE_ETHER, output: ONE_ETHER },
-                    {
-                        source: ERC20BridgeSource.SushiSwap,
-                        fillData: {},
-                        input: ONE_ETHER.times(2),
-                        output: ONE_ETHER.times(2),
-                    },
-                    {
-                        source: ERC20BridgeSource.SushiSwap,
-                        fillData: {},
-                        input: ONE_ETHER.times(3),
-                        output: ONE_ETHER.times(3),
-                    },
-                    {
-                        source: ERC20BridgeSource.SushiSwap,
-                        fillData: {},
-                        input: ONE_ETHER.times(4),
-                        output: ONE_ETHER.times(4),
-                    },
+                    createMultihopSample({
+                        firstHopSource: ERC20BridgeSource.Curve,
+                        secondHopSource: ERC20BridgeSource.CurveV2,
+                        inputInEther: 4,
+                        outputInEther: 9,
+                    }),
                 ],
-            ],
-            [],
-            [],
-        );
+                [],
+            );
 
-        expect(path?.fills).lengthOf(1);
+            if (path === undefined) {
+                expect(path).to.be.not.undefined();
+                return;
+            }
 
-        const fill = path?.fills[0];
-        expect(fill?.source).eq(ERC20BridgeSource.UniswapV3);
-        expect(fill?.input).bignumber.eq(ONE_ETHER.times(4));
-        expect(fill?.output).bignumber.eq(ONE_ETHER.times(8));
-    });
+            expect(path.fills).lengthOf(1);
 
-    it('Returns the multihop path when it is the best path', async () => {
-        const pathOptimizer = new PathOptimizer({
-            side: MarketOperation.Sell,
-            chainId: ChainId.Mainnet,
-            feeSchedule: NO_OP_FEE_SCHEDULE,
-            neonRouterNumSamples: 4,
-            fillAdjustor: new IdentityFillAdjustor(),
-            pathPenaltyOpts: NO_OP_PATH_PENALTY_OPTS,
-            inputAmount: ONE_ETHER.times(4),
+            const fill = path?.fills[0];
+            expect(fill.source).eq(ERC20BridgeSource.MultiHop);
+            expect(fill.input).bignumber.eq(ONE_ETHER.times(4));
+            expect(fill.output).bignumber.eq(ONE_ETHER.times(9));
         });
 
-        // Multihop is the best path.
-        // UniswapV3: 4 -> 8
-        // Multihop: 4 -> 9
-        const path = pathOptimizer.findOptimalPathFromSamples(
-            [
+        it('Returns the native source path', async () => {
+            const pathOptimizer = new PathOptimizer({
+                side: MarketOperation.Sell,
+                chainId: ChainId.Mainnet,
+                feeSchedule: NO_OP_FEE_SCHEDULE,
+                neonRouterNumSamples: 5,
+                fillAdjustor: new IdentityFillAdjustor(),
+                pathPenaltyOpts: NO_OP_PATH_PENALTY_OPTS,
+                inputAmount: ONE_ETHER.times(4),
+            });
+
+            // Native order is the best route.
+            const path = pathOptimizer.findOptimalPathFromSamples(
                 [
-                    { source: ERC20BridgeSource.UniswapV3, fillData: {}, input: ONE_ETHER, output: ONE_ETHER.times(2) },
-                    {
+                    createDexSamples({
                         source: ERC20BridgeSource.UniswapV3,
-                        fillData: {},
-                        input: ONE_ETHER.times(2),
-                        output: ONE_ETHER.times(4),
-                    },
-                    {
-                        source: ERC20BridgeSource.UniswapV3,
-                        fillData: {},
-                        input: ONE_ETHER.times(3),
-                        output: ONE_ETHER.times(6),
-                    },
-                    {
-                        source: ERC20BridgeSource.UniswapV3,
-                        fillData: {},
-                        input: ONE_ETHER.times(4),
-                        output: ONE_ETHER.times(8),
-                    },
+                        inputsInEther: [1, 2, 3, 4],
+                        outputsInEther: [1, 2, 3, 4],
+                    }),
                 ],
-            ],
-            [
-                {
-                    source: ERC20BridgeSource.MultiHop,
-                    fillData: {
-                        firstHopSource: {
-                            source: ERC20BridgeSource.Curve,
-                            fillData: {},
-                            encodeCall: () => 'fake-encodedCall',
-                            handleCallResults: () => [],
-                            handleRevert: () => [],
-                        },
-                        secondHopSource: {
-                            source: ERC20BridgeSource.CurveV2,
-                            fillData: {},
-                            encodeCall: () => 'fake-encodedCall',
-                            handleCallResults: () => [],
-                            handleRevert: () => [],
-                        },
-                        intermediateToken: 'fake-intermediate-token',
-                    },
-                    input: ONE_ETHER.times(4),
-                    output: ONE_ETHER.times(9),
-                },
-            ],
-            [],
-        );
+                [
+                    createMultihopSample({
+                        firstHopSource: ERC20BridgeSource.Curve,
+                        secondHopSource: ERC20BridgeSource.CurveV2,
+                        inputInEther: 4,
+                        outputInEther: 4,
+                    }),
+                ],
+                [
+                    createOtcOrder({
+                        inputInEther: 4,
+                        outputInEther: 5,
+                    }),
+                ],
+            );
 
-        expect(path?.fills).lengthOf(1);
+            if (path === undefined) {
+                expect(path).to.be.not.undefined();
+                return;
+            }
 
-        const fill = path?.fills[0];
-        expect(fill?.source).eq(ERC20BridgeSource.MultiHop);
-        expect(fill?.input).bignumber.eq(ONE_ETHER.times(4));
-        expect(fill?.output).bignumber.eq(ONE_ETHER.times(9));
+            expect(path.fills).lengthOf(1);
+
+            const fill = path?.fills[0];
+            expect(fill.source).eq(ERC20BridgeSource.Native);
+            expect(fill.input).bignumber.eq(ONE_ETHER.times(4));
+            expect(fill.output).bignumber.eq(ONE_ETHER.times(5));
+        });
     });
+
+    describe('Multiplex routing', () => {
+        it('Returns the multiplex path from dex samples', async () => {
+            const pathOptimizer = new PathOptimizer({
+                side: MarketOperation.Sell,
+                chainId: ChainId.Mainnet,
+                feeSchedule: NO_OP_FEE_SCHEDULE,
+                neonRouterNumSamples: 5,
+                fillAdjustor: new IdentityFillAdjustor(),
+                pathPenaltyOpts: NO_OP_PATH_PENALTY_OPTS,
+                inputAmount: ONE_ETHER.times(4),
+            });
+
+            // The best route involves utilizing both.
+            const path = pathOptimizer.findOptimalPathFromSamples(
+                [
+                    createDexSamples({
+                        source: ERC20BridgeSource.UniswapV3,
+                        inputsInEther: [1, 2, 3, 4],
+                        outputsInEther: [2, 4, 6, 7],
+                    }),
+                    createDexSamples({
+                        source: ERC20BridgeSource.SushiSwap,
+                        inputsInEther: [1, 2, 3, 4],
+                        outputsInEther: [2, 3, 4, 5],
+                    }),
+                ],
+                [],
+                [],
+            );
+
+            if (path === undefined) {
+                expect(path).to.be.not.undefined();
+                return;
+            }
+
+            expect(path.fills).lengthOf(2);
+            const fill0 = path.fills[0];
+            const fill1 = path.fills[1];
+            expect(fill0.source).eq(ERC20BridgeSource.UniswapV3);
+            expect(fill1.source).eq(ERC20BridgeSource.SushiSwap);
+
+            const inputTotal = fill0.input.plus(fill1.input);
+            const outputTotal = fill0.output.plus(fill1.output);
+
+            expect(inputTotal).bignumber.eq(ONE_ETHER.times(4));
+            expect(outputTotal).bignumber.eq(ONE_ETHER.times(8));
+        });
+    });
+
+    // TODO: DEX + native
+    // TODO: native + native
 });
+
+function createDexSample(source: ERC20BridgeSource, inputInEther: number, outputInEther: number): DexSample<FillData> {
+    return {
+        source,
+        input: ONE_ETHER.times(inputInEther),
+        output: ONE_ETHER.times(outputInEther),
+        fillData: {},
+    };
+}
+
+function createDexSamples(params: {
+    source: ERC20BridgeSource;
+    inputsInEther: number[];
+    outputsInEther: number[];
+}): DexSample<FillData>[] {
+    const { source, inputsInEther, outputsInEther } = params;
+    return inputsInEther.map((inputInEther, i) => createDexSample(source, inputInEther, outputsInEther[i]));
+}
+
+function createMultihopSample(params: {
+    firstHopSource: ERC20BridgeSource;
+    secondHopSource: ERC20BridgeSource;
+    inputInEther: number;
+    outputInEther: number;
+}): DexSample<MultiHopFillData> {
+    return {
+        source: ERC20BridgeSource.MultiHop,
+        fillData: {
+            firstHopSource: {
+                source: params.firstHopSource,
+                fillData: {},
+                encodeCall: () => 'fake-encodedCall',
+                handleCallResults: () => [],
+                handleRevert: () => [],
+            },
+            secondHopSource: {
+                source: params.secondHopSource,
+                fillData: {},
+                encodeCall: () => 'fake-encodedCall',
+                handleCallResults: () => [],
+                handleRevert: () => [],
+            },
+            intermediateToken: 'fake-intermediate-token',
+        },
+        input: ONE_ETHER.times(params.inputInEther),
+        output: ONE_ETHER.times(params.outputInEther),
+    };
+}
+
+function createOtcOrder(params: { inputInEther: number; outputInEther: number }): NativeOrderWithFillableAmounts {
+    const { inputInEther, outputInEther } = params;
+    return {
+        type: FillQuoteTransformerOrderType.Otc,
+        order: {
+            txOrigin: '',
+            expiryAndNonce: new BigNumber(42),
+            makerToken: 'fake-maker-token',
+            takerToken: 'fake-taker-token',
+            makerAmount: ONE_ETHER.times(outputInEther),
+            takerAmount: ONE_ETHER.times(inputInEther),
+            maker: 'maker',
+            taker: 'taker',
+            chainId: 42,
+            verifyingContract: 'fake-verifying contract',
+        },
+        signature: {
+            signatureType: SignatureType.EIP712,
+            r: 'r',
+            s: 's',
+            v: 42,
+        },
+        fillableMakerAmount: ONE_ETHER.times(outputInEther),
+        fillableTakerAmount: ONE_ETHER.times(inputInEther),
+        fillableTakerFeeAmount: new BigNumber(0),
+    };
+}
