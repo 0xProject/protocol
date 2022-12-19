b8d40fda5bdf26f1a9df1776664182a010fe7799
dextracker <noah@0xproject.com>
2022-12-19 18:23:09 -0500

    prettier and lint

--- contracts/zero-ex/contracts/test/foundry/utils/TestUtils.sol
+++ contracts/zero-ex/contracts/test/foundry/utils/TestUtils.sol
@@ -53,7 +53,6 @@ contract TestUtils is Test {
         return nstr;
     }
 
-
     function _findTransformerNonce(address transformer, address deployer) internal pure returns (uint32) {
         address current;
         for (uint32 i = 0; i < 1024; i++) {
