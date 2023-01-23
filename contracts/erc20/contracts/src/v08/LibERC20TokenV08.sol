891d173705e67d8e58e7d51e9f6afa8fc1950afc
Savarn Dontamsetti (Sav) <SavDont@users.noreply.github.com>
2023-01-23 18:00:43 -0500

    chore: adding V08 contracts [LIT-783] (#646)
    * Adding V08 contracts
    
    * Removing LibSafeMathV08
    
    * Apply suggestions from code review
    
    Co-authored-by: duncancmt <1207590+duncancmt@users.noreply.github.com>

---
+++ contracts/erc20/contracts/src/v08/LibERC20TokenV08.sol
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+
+  Copyright 2020 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+pragma solidity ^0.8.0;
+
+import "@0x/contracts-utils/contracts/src/v08/errors/LibRichErrorsV08.sol";
+import "@0x/contracts-utils/contracts/src/v08/LibBytesV08.sol";
+import "./IERC20TokenV08.sol";
+
+library LibERC20TokenV08 {
+    bytes private constant DECIMALS_CALL_DATA = hex"313ce567";
+
+    /// @dev Calls `IERC20TokenV08(token).approve()`.
+    ///      Reverts if the return data is invalid or the call reverts.
+    /// @param token The address of the token contract.
+    /// @param spender The address that receives an allowance.
+    /// @param allowance The allowance to set.
+    function compatApprove(IERC20TokenV08 token, address spender, uint256 allowance) internal {
+        bytes memory callData = abi.encodeCall(token.approve, (spender, allowance));
+        _callWithOptionalBooleanResult(address(token), callData);
+    }
+
+    /// @dev Calls `IERC20TokenV08(token).approve()` and sets the allowance to the
+    ///      maximum if the current approval is not already >= an amount.
+    ///      Reverts if the return data is invalid or the call reverts.
+    /// @param token The address of the token contract.
+    /// @param spender The address that receives an allowance.
+    /// @param amount The minimum allowance needed.
+    function approveIfBelow(IERC20TokenV08 token, address spender, uint256 amount) internal {
+        if (token.allowance(address(this), spender) < amount) {
+            compatApprove(token, spender, type(uint256).max);
+        }
+    }
+
+    /// @dev Calls `IERC20TokenV08(token).transfer()`.
+    ///      Reverts if the return data is invalid or the call reverts.
+    /// @param token The address of the token contract.
+    /// @param to The address that receives the tokens
+    /// @param amount Number of tokens to transfer.
+    function compatTransfer(IERC20TokenV08 token, address to, uint256 amount) internal {
+        bytes memory callData = abi.encodeCall(token.transfer, (to, amount));
+        _callWithOptionalBooleanResult(address(token), callData);
+    }
+
+    /// @dev Calls `IERC20TokenV08(token).transferFrom()`.
+    ///      Reverts if the return data is invalid or the call reverts.
+    /// @param token The address of the token contract.
+    /// @param from The owner of the tokens.
+    /// @param to The address that receives the tokens
+    /// @param amount Number of tokens to transfer.
+    function compatTransferFrom(IERC20TokenV08 token, address from, address to, uint256 amount) internal {
+        bytes memory callData = abi.encodeCall(token.transferFrom, (from, to, amount));
+        _callWithOptionalBooleanResult(address(token), callData);
+    }
+
+    /// @dev Retrieves the number of decimals for a token.
+    ///      Returns `18` if the call reverts.
+    /// @param token The address of the token contract.
+    /// @return tokenDecimals The number of decimals places for the token.
+    function compatDecimals(IERC20TokenV08 token) internal view returns (uint8 tokenDecimals) {
+        tokenDecimals = 18;
+        (bool didSucceed, bytes memory resultData) = address(token).staticcall(DECIMALS_CALL_DATA);
+        if (didSucceed && resultData.length >= 32) {
+            tokenDecimals = abi.decode(resultData, (uint8));
+        }
+    }
+
+    /// @dev Retrieves the allowance for a token, owner, and spender.
+    ///      Returns `0` if the call reverts.
+    /// @param token The address of the token contract.
+    /// @param owner The owner of the tokens.
+    /// @param spender The address the spender.
+    /// @return allowance_ The allowance for a token, owner, and spender.
+    function compatAllowance(
+        IERC20TokenV08 token,
+        address owner,
+        address spender
+    ) internal view returns (uint256 allowance_) {
+        (bool didSucceed, bytes memory resultData) = address(token).staticcall(
+            abi.encodeCall(token.allowance, (owner, spender))
+        );
+        if (didSucceed && resultData.length >= 32) {
+            allowance_ = abi.decode(resultData, (uint256));
+        }
+    }
+
+    /// @dev Retrieves the balance for a token owner.
+    ///      Returns `0` if the call reverts.
+    /// @param token The address of the token contract.
+    /// @param owner The owner of the tokens.
+    /// @return balance The token balance of an owner.
+    function compatBalanceOf(IERC20TokenV08 token, address owner) internal view returns (uint256 balance) {
+        (bool didSucceed, bytes memory resultData) = address(token).staticcall(
+            abi.encodeCall(token.balanceOf, (owner))
+        );
+        if (didSucceed && resultData.length >= 32) {
+            balance = abi.decode(resultData, (uint256));
+        }
+    }
+
+    /// @dev Executes a call on address `target` with calldata `callData`
+    ///      and asserts that either nothing was returned or a single boolean
+    ///      was returned equal to `true`.
+    /// @param target The call target.
+    /// @param callData The abi-encoded call data.
+    function _callWithOptionalBooleanResult(address target, bytes memory callData) private {
+        (bool didSucceed, bytes memory resultData) = target.call(callData);
+        // Revert if the call reverted.
+        if (!didSucceed) {
+            LibRichErrorsV08.rrevert(resultData);
+        }
+        // If we get back 0 returndata, this may be a non-standard ERC-20 that
+        // does not return a boolean. Check that it at least contains code.
+        if (resultData.length == 0) {
+            require(target.code.length > 0, "invalid token address, contains no code");
+            return;
+        }
+        // If we get back at least 32 bytes, we know the target address
+        // contains code, and we assume it is a token that returned a boolean
+        // success value, which must be true.
+        if (resultData.length >= 32) {
+            if (!abi.decode(resultData, (bool))) {
+                LibRichErrorsV08.rrevert(resultData);
+            }
+        }
+        // If 0 < returndatasize < 32, the target is a contract, but not a
+        // valid token.
+        LibRichErrorsV08.rrevert(resultData);
+    }
+}
