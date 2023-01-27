2f83346debe55b70500d1ebd89301f0cf75d9908
Jacob Evans <jacob@dekz.net>
2023-01-27 13:31:15 +1000

    feat: PositiveSlippageTransformer tests

---
+++ contracts/zero-ex/tests/transformers/PositiveSlippageFeeTransformerTest.t.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+  Copyright 2023 ZeroEx Intl.
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+pragma solidity ^0.6.5;
+pragma experimental ABIEncoderV2;
+
+import "@0x/contracts-erc20/contracts/src/v06/WETH9V06.sol";
+
+import "../BaseTest.sol";
+import "../../contracts/src/transformers/PositiveSlippageFeeTransformer.sol";
+import "../../contracts/src/transformers/IERC20Transformer.sol";
+
+contract PositiveSlippageFeeTransformerTest is BaseTest {
+    address public owner = account1;
+    address public feeRecipient = account2;
+    WETH9V06 weth = new WETH9V06();
+    IERC20TokenV06 token1 = IERC20TokenV06(address(weth));
+
+    PositiveSlippageFeeTransformer target = new PositiveSlippageFeeTransformer();
+
+    function setUp() public {
+        vm.deal(address(this), 1e19);
+        weth.deposit{value: 10}();
+    }
+
+    function test_positiveSlippage() public {
+        // Send positive slippage to contract which executes PositiveSlippageFeeTransformer
+        weth.transfer(address(target), 10);
+        uint256 takerAmount = 1;
+
+        bytes4 result = target.transform(
+            IERC20Transformer.TransformContext({
+                sender: payable(address(this)),
+                recipient: payable(address(this)),
+                data: abi.encode(
+                    PositiveSlippageFeeTransformer.TokenFee({
+                        token: IERC20TokenV06(token1),
+                        bestCaseAmount: takerAmount,
+                        recipient: payable(feeRecipient)
+                    })
+                )
+            })
+        );
+        assertEq(token1.balanceOf(feeRecipient), 9);
+        assertEq(result, LibERC20Transformer.TRANSFORMER_SUCCESS);
+    }
+
+    function test_positiveSlippage_zeroPositiveSlippage() public {
+        uint256 takerAmount = 10;
+        weth.transfer(address(target), 10);
+
+        bytes4 result = target.transform(
+            IERC20Transformer.TransformContext({
+                sender: payable(address(this)),
+                recipient: payable(address(this)),
+                data: abi.encode(
+                    PositiveSlippageFeeTransformer.TokenFee({
+                        token: IERC20TokenV06(token1),
+                        bestCaseAmount: takerAmount,
+                        recipient: payable(feeRecipient)
+                    })
+                )
+            })
+        );
+        assertEq(token1.balanceOf(feeRecipient), 0);
+        assertEq(result, LibERC20Transformer.TRANSFORMER_SUCCESS);
+    }
+}
