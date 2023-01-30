e0ef25404c5a53db1cad27244411bea4fe5ed6e1
Elyas <35933488+eobbad@users.noreply.github.com>
2023-01-30 17:08:33 -0500

    feat: woofiV2 w/ dependencies (#1234)
    * update dependencies
    
    * add few more deps
    
    * revert changes

--- contracts/src/WooPPSampler.sol
+++ contracts/src/WooPPSampler.sol
@@ -5,53 +5,24 @@ import "./SamplerUtils.sol";
 import "./ApproximateBuys.sol";
 
 interface IWooPP {
-    /// @dev get the quote token address (immutable)
-    /// @return address of quote token
-    function quoteToken() external view returns (address);
-
-    /// @dev Query the amount for selling the base token amount.
-    /// @param baseToken the base token to sell
-    /// @param baseAmount the amount to sell
-    /// @return quoteAmount the swapped quote amount
-    function querySellBase(address baseToken, uint256 baseAmount) external view returns (uint256 quoteAmount);
-
-    /// @dev Query the amount for selling the quote token.
-    /// @param baseToken the base token to receive (buy)
-    /// @param quoteAmount the amount to sell
-    /// @return baseAmount the swapped base token amount
-    function querySellQuote(address baseToken, uint256 quoteAmount) external view returns (uint256 baseAmount);
+    /// @dev query the amount to swap fromToken -> toToken
+    /// @param fromToken the from token
+    /// @param toToken the to token
+    /// @param fromAmount the amount of fromToken to swap
+    /// @return toAmount the predicted amount to receive
+    function querySwap(address fromToken, address toToken, uint256 fromAmount) external view returns (uint256 toAmount);
 }
 
 contract WooPPSampler is SamplerUtils, ApproximateBuys {
-    function query(
-        uint256 amountIn,
-        address tokenIn,
-        address tokenOut,
-        address pool
-    ) internal view returns (uint256 amountOut) {
-        if (amountIn == 0) {
-            return 0;
-        }
-        address quoteToken = IWooPP(pool).quoteToken();
-        if (tokenIn == quoteToken) {
-            amountOut = IWooPP(pool).querySellQuote(tokenOut, amountIn);
-        } else if (tokenOut == quoteToken) {
-            amountOut = IWooPP(pool).querySellBase(tokenIn, amountIn);
-        } else {
-            uint256 quoteAmount = IWooPP(pool).querySellBase(tokenIn, amountIn);
-            amountOut = IWooPP(pool).querySellQuote(tokenOut, quoteAmount);
-        }
-    }
-
     /// @dev Sample sell quotes from WooFI.
-    /// @param pool Address of the pool we are sampling from
+    /// @param router Address of the router we are sampling from
     /// @param takerToken Address of the taker token (what to sell).
     /// @param makerToken Address of the maker token (what to buy).
     /// @param takerTokenAmounts Taker token sell amount for each sample (sorted in ascending order).
     /// @return makerTokenAmounts Maker amounts bought at each taker token
     ///         amount.
     function sampleSellsFromWooPP(
-        address pool,
+        IWooPP router,
         address takerToken,
         address makerToken,
         uint256[] memory takerTokenAmounts
@@ -59,8 +30,7 @@ contract WooPPSampler is SamplerUtils, ApproximateBuys {
         uint256 numSamples = takerTokenAmounts.length;
         makerTokenAmounts = new uint256[](numSamples);
         for (uint256 i = 0; i < numSamples; i++) {
-            makerTokenAmounts[i] = query(takerTokenAmounts[i], takerToken, makerToken, pool);
-
+            makerTokenAmounts[i] = router.querySwap(takerToken, makerToken, takerTokenAmounts[i]);
             if (makerTokenAmounts[i] == 0) {
                 break;
             }
@@ -68,14 +38,14 @@ contract WooPPSampler is SamplerUtils, ApproximateBuys {
     }
 
     /// @dev Sample buy quotes from WooFI.
-    /// @param pool Address of the pool we are sampling from
+    /// @param router Address of the router we are sampling from
     /// @param takerToken Address of the taker token (what to sell).
     /// @param makerToken Address of the maker token (what to buy).
     /// @param makerTokenAmounts Maker token sell amount for each sample (sorted in ascending order).
     /// @return takerTokenAmounts Taker amounts bought at each taker token
     ///         amount.
     function sampleBuysFromWooPP(
-        address pool,
+        IWooPP router,
         address takerToken,
         address makerToken,
         uint256[] memory makerTokenAmounts
@@ -83,8 +53,8 @@ contract WooPPSampler is SamplerUtils, ApproximateBuys {
         uint256 numSamples = makerTokenAmounts.length;
         takerTokenAmounts = _sampleApproximateBuys(
             ApproximateBuyQuoteOpts({
-                takerTokenData: abi.encode(pool, takerToken, makerToken),
-                makerTokenData: abi.encode(pool, makerToken, takerToken),
+                takerTokenData: abi.encode(router, takerToken, makerToken),
+                makerTokenData: abi.encode(router, makerToken, takerToken),
                 getSellQuoteCallback: _sampleSellForApproximateBuyFromWoofi
             }),
             makerTokenAmounts
@@ -96,14 +66,14 @@ contract WooPPSampler is SamplerUtils, ApproximateBuys {
         bytes memory makerTokenData,
         uint256 sellAmount
     ) internal view returns (uint256) {
-        (address _pool, address _takerToken, address _makerToken) = abi.decode(
+        (IWooPP _router, address _takerToken, address _makerToken) = abi.decode(
             takerTokenData,
-            (address, address, address)
+            (IWooPP, address, address)
         );
         (bool success, bytes memory resultData) = address(this).staticcall(
             abi.encodeWithSelector(
                 this.sampleSellsFromWooPP.selector,
-                _pool,
+                _router,
                 _takerToken,
                 _makerToken,
                 _toSingleValueArray(sellAmount)
