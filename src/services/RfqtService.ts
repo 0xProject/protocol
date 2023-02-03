d6772adc27adb0cfec7e08a8be720c988c6502a0
dextracker <noah@0xproject.com>
2023-02-03 16:44:22 -0500

    remove redundant check

--- src/services/RfqtService.ts
+++ src/services/RfqtService.ts
@@ -320,7 +320,7 @@ export class RfqtService {
                     quoteContext.txOrigin,
                     baseNonce.plus(i),
                     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                    quoteContext.bucket !== undefined ? new BigNumber(quoteContext.bucket! + i) : new BigNumber(0), // bucket
+                    quoteContext.bucket !== undefined ? new BigNumber(quoteContext.bucket + i) : new BigNumber(0), // bucket
                 ),
                 price,
             }));
