57d9e191811a53b6928bbb873cad76cdd1657f8e
Elyas <35933488+eobbad@users.noreply.github.com>
2023-02-07 13:49:00 -0500

    remove smoothy selectors (#1251)

--- src/asset-swapper/utils/market_operation_utils/types.ts
+++ src/asset-swapper/utils/market_operation_utils/types.ts
@@ -42,9 +42,6 @@ export enum CurveFunctionSelectors {
     exchange_underlying_v2 = '0x65b2489b',
     get_dy_v2 = '0x556d6e9f',
     get_dy_underlying_v2 = '0x85f11d1e',
-    // Smoothy(deprecated)
-    swap_uint256 = '0x5673b02d', // swap(uint256,uint256,uint256,uint256)
-    get_swap_amount = '0x45cf2ef6', // getSwapAmount(uint256,uint256,uint256)
     // Nerve BSC, Saddle Mainnet, Synapse
     swap = '0x91695586', // swap(uint8,uint8,uint256,uint256,uint256)
     calculateSwap = '0xa95b089f', // calculateSwap(uint8,uint8,uint256)
