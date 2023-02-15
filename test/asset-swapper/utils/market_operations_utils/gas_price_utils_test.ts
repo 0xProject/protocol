ffd67802eea86c821631f2eab92c3cffd228cb14
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2023-02-15 09:46:20 -0700

    refactor: Remove msw from gas price utils test (#1268)

--- test/asset-swapper/utils/market_operations_utils/gas_price_utils_test.ts
+++ test/asset-swapper/utils/market_operations_utils/gas_price_utils_test.ts
@@ -1,7 +1,6 @@
 import * as chai from 'chai';
 import 'mocha';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
+import * as TypeMoq from 'typemoq';
 
 import { GasPriceUtils } from '../../../../src/asset-swapper';
 
@@ -9,37 +8,32 @@ import { chaiSetup } from '../chai_setup';
 chaiSetup.configure();
 const expect = chai.expect;
 
-const server = setupServer(
-    rest.get('https://mock-0x-gas-api.org/median', (_req, res, ctx) => {
-        return res(
-            ctx.json({
-                result: {
-                    source: 'MEDIAN',
-                    timestamp: 1659386474,
-                    instant: 22000000000,
-                    fast: 18848500000,
-                    standard: 14765010000,
-                    low: 13265000000,
-                },
-            }),
-        );
+const mockResponse = new Response(
+    JSON.stringify({
+        result: {
+            source: 'MEDIAN',
+            timestamp: 1659386474,
+            instant: 22000000000,
+            fast: 18848500000,
+            standard: 14765010000,
+            low: 13265000000,
+        },
     }),
 );
 
+const fetchMock = TypeMoq.GlobalMock.ofInstance(fetch, 'fetch', global);
+fetchMock
+    .setup((x) => x(TypeMoq.It.isValue('https://mock-0x-gas-api.org/median')))
+    .returns(() => Promise.resolve(mockResponse));
+
 describe('GasPriceUtils', () => {
     describe('getGasPriceEstimationOrThrowAsync', () => {
-        beforeEach(() => {
-            server.listen();
-        });
-
-        afterEach(() => {
-            server.close();
-        });
-
         it('parses fast gas price response correctly', async () => {
-            const utils = GasPriceUtils.getInstance(420000, 'https://mock-0x-gas-api.org/median');
-            const gasPrice = await utils.getGasPriceEstimationOrThrowAsync();
-            expect(gasPrice.fast).to.eq(18848500000);
+            TypeMoq.GlobalScope.using(fetchMock).with(async () => {
+                const utils = GasPriceUtils.getInstance(420000, 'https://mock-0x-gas-api.org/median');
+                const gasPrice = await utils.getGasPriceEstimationOrThrowAsync();
+                expect(gasPrice.fast).to.eq(18848500000);
+            });
         });
     });
 });
