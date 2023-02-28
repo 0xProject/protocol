ec82c42c1b89ff79ba3133d3648b95b9f11415f8
Kyu <kyuhyun217@gmail.com>
2023-02-27 18:18:56 -0800

    Skip chain id validation in `AbstractBridgeAdapter` on testnets (#668)

--- contracts/zero-ex/contracts/src/transformers/bridges/AbstractBridgeAdapter.sol
+++ contracts/zero-ex/contracts/src/transformers/bridges/AbstractBridgeAdapter.sol
@@ -23,8 +23,14 @@ abstract contract AbstractBridgeAdapter is IBridgeAdapter {
         assembly {
             chainId := chainid()
         }
-        // Allow testing on Ganache
-        if (chainId != expectedChainId && chainId != 1337) {
+        // Skip chain id validation on Ganache (1337), Anvil (31337), Goerli (5), Mumbai (80001), Base Goerli (84531)
+        bool skipValidation = (chainId == 1337 ||
+            chainId == 31337 ||
+            chainId == 5 ||
+            chainId == 80001 ||
+            chainId == 84531);
+
+        if (chainId != expectedChainId && !skipValidation) {
             revert(string(abi.encodePacked(expectedChainName, "BridgeAdapter.constructor: wrong chain ID")));
         }
     }
