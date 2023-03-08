ac7a637df5eac9e53e290df30ea5ae6850f6f3f5
Wojtek <xsohai@gmail.com>
2023-03-08 14:47:36 +0100

    [DVX-206]: Basic dashboard hero area HTML structor (#125)

--- sites/dev0x-portal/app/routes/apps._index.tsx
+++ sites/dev0x-portal/app/routes/apps._index.tsx
@@ -1,92 +1,32 @@
+import { Button } from '../components/Button';
+
 export default function Apps() {
     return (
         <div className="px-24">
-            <h3 className="font-sans text-5xl font-normal">Welcome to 0x.</h3>
-            <div className=""></div>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique est metus, eget malesuada
-                dolor vestibulum ac. Donec lacinia metus eu quam molestie ullamcorper. Phasellus molestie risus id erat
-                cursus, nec fringilla nisi gravida. Ut pretium ante vel nisi feugiat, vel accumsan nulla suscipit. Proin
-                bibendum rutrum mollis. Vestibulum venenatis tincidunt consequat. Quisque massa tellus, pretium ut
-                commodo nec, accumsan ac leo. Fusce eu velit aliquet, dictum ligula in, fringilla turpis. Aliquam quis
-                libero ante. Aenean vel tortor sem. Phasellus ante felis, aliquet et mi vitae, porta maximus tortor. Nam
-                a eros turpis. Duis molestie maximus consectetur. Pellentesque augue lectus, porttitor at aliquet et,
-                condimentum ut nunc. Proin pulvinar mauris quis hendrerit elementum. Ut convallis placerat laoreet.
-                Proin lacinia lobortis pulvinar. Morbi blandit nisi non turpis cursus, eu vestibulum lectus congue.
-                Maecenas placerat ex maximus sagittis maximus. Pellentesque molestie hendrerit enim, aliquam maximus
-                lectus scelerisque eu. Nulla vel tellus ex. Nulla at tincidunt turpis. Vivamus euismod, nunc et volutpat
-                scelerisque, arcu justo iaculis urna, sed consectetur turpis ex eu justo. Duis suscipit mauris sed
-                sapien rutrum congue. Morbi faucibus nisi congue malesuada sagittis. Cras a tempor turpis. Etiam luctus
-                tortor a augue tempor, et feugiat augue tempus. Pellentesque vestibulum arcu turpis, a vulputate odio
-                malesuada at. Nulla pharetra nulla semper laoreet aliquet. Vestibulum eget interdum neque, ut commodo
-                magna. Etiam non dolor facilisis, porttitor sapien consectetur, lobortis odio. Phasellus ultrices sem ut
-                enim rutrum rutrum. Etiam pretium sollicitudin dolor ac rhoncus. Etiam sit amet dignissim odio. Aliquam
-                sodales velit dapibus, condimentum massa vel, sodales odio. Aliquam in dolor maximus, scelerisque velit
-                id, eleifend nisl. Nullam vitae erat mollis, vestibulum tellus quis, laoreet orci. Donec tellus mauris,
-                sodales et velit sit amet, lacinia interdum felis. Quisque eros ex, ultricies non sapien ultrices,
-                pellentesque euismod nulla. Nunc odio felis, facilisis at tincidunt et, sodales pulvinar leo.
-                Pellentesque at nulla massa. Nulla ullamcorper accumsan viverra. Pellentesque enim erat, pretium viverra
-                velit ut, accumsan mattis nunc. Nulla at mauris sem. Maecenas placerat tellus a dui malesuada feugiat.
-                Nullam commodo egestas arcu fringilla porttitor. Sed ex nisl, scelerisque eu efficitur in, pharetra at
-                ipsum. Sed ultrices nulla eget leo rhoncus tempor. Cras erat nulla, blandit vel iaculis eget, gravida in
-                lacus. Mauris turpis ipsum, sollicitudin ac nunc dignissim, sodales efficitur sapien.{' '}
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique est metus, eget malesuada
-                dolor vestibulum ac. Donec lacinia metus eu quam molestie ullamcorper. Phasellus molestie risus id erat
-                cursus, nec fringilla nisi gravida. Ut pretium ante vel nisi feugiat, vel accumsan nulla suscipit. Proin
-                bibendum rutrum mollis. Vestibulum venenatis tincidunt consequat. Quisque massa tellus, pretium ut
-                commodo nec, accumsan ac leo. Fusce eu velit aliquet, dictum ligula in, fringilla turpis. Aliquam quis
-                libero ante. Aenean vel tortor sem. Phasellus ante felis, aliquet et mi vitae, porta maximus tortor. Nam
-                a eros turpis. Duis molestie maximus consectetur. Pellentesque augue lectus, porttitor at aliquet et,
-                condimentum ut nunc. Proin pulvinar mauris quis hendrerit elementum. Ut convallis placerat laoreet.
-                Proin lacinia lobortis pulvinar. Morbi blandit nisi non turpis cursus, eu vestibulum lectus congue.
-                Maecenas placerat ex maximus sagittis maximus. Pellentesque molestie hendrerit enim, aliquam maximus
-                lectus scelerisque eu. Nulla vel tellus ex. Nulla at tincidunt turpis. Vivamus euismod, nunc et volutpat
-                scelerisque, arcu justo iaculis urna, sed consectetur turpis ex eu justo. Duis suscipit mauris sed
-                sapien rutrum congue. Morbi faucibus nisi congue malesuada sagittis. Cras a tempor turpis. Etiam luctus
-                tortor a augue tempor, et feugiat augue tempus. Pellentesque vestibulum arcu turpis, a vulputate odio
-                malesuada at. Nulla pharetra nulla semper laoreet aliquet. Vestibulum eget interdum neque, ut commodo
-                magna. Etiam non dolor facilisis, porttitor sapien consectetur, lobortis odio. Phasellus ultrices sem ut
-                enim rutrum rutrum. Etiam pretium sollicitudin dolor ac rhoncus. Etiam sit amet dignissim odio. Aliquam
-                sodales velit dapibus, condimentum massa vel, sodales odio. Aliquam in dolor maximus, scelerisque velit
-                id, eleifend nisl. Nullam vitae erat mollis, vestibulum tellus quis, laoreet orci. Donec tellus mauris,
-                sodales et velit sit amet, lacinia interdum felis. Quisque eros ex, ultricies non sapien ultrices,
-                pellentesque euismod nulla. Nunc odio felis, facilisis at tincidunt et, sodales pulvinar leo.
-                Pellentesque at nulla massa. Nulla ullamcorper accumsan viverra. Pellentesque enim erat, pretium viverra
-                velit ut, accumsan mattis nunc. Nulla at mauris sem. Maecenas placerat tellus a dui malesuada feugiat.
-                Nullam commodo egestas arcu fringilla porttitor. Sed ex nisl, scelerisque eu efficitur in, pharetra at
-                ipsum. Sed ultrices nulla eget leo rhoncus tempor. Cras erat nulla, blandit vel iaculis eget, gravida in
-                lacus. Mauris turpis ipsum, sollicitudin ac nunc dignissim, sodales efficitur sapien.{' '}
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique est metus, eget malesuada
-                dolor vestibulum ac. Donec lacinia metus eu quam molestie ullamcorper. Phasellus molestie risus id erat
-                cursus, nec fringilla nisi gravida. Ut pretium ante vel nisi feugiat, vel accumsan nulla suscipit. Proin
-                bibendum rutrum mollis. Vestibulum venenatis tincidunt consequat. Quisque massa tellus, pretium ut
-                commodo nec, accumsan ac leo. Fusce eu velit aliquet, dictum ligula in, fringilla turpis. Aliquam quis
-                libero ante. Aenean vel tortor sem. Phasellus ante felis, aliquet et mi vitae, porta maximus tortor. Nam
-                a eros turpis. Duis molestie maximus consectetur. Pellentesque augue lectus, porttitor at aliquet et,
-                condimentum ut nunc. Proin pulvinar mauris quis hendrerit elementum. Ut convallis placerat laoreet.
-                Proin lacinia lobortis pulvinar. Morbi blandit nisi non turpis cursus, eu vestibulum lectus congue.
-                Maecenas placerat ex maximus sagittis maximus. Pellentesque molestie hendrerit enim, aliquam maximus
-                lectus scelerisque eu. Nulla vel tellus ex. Nulla at tincidunt turpis. Vivamus euismod, nunc et volutpat
-                scelerisque, arcu justo iaculis urna, sed consectetur turpis ex eu justo. Duis suscipit mauris sed
-                sapien rutrum congue. Morbi faucibus nisi congue malesuada sagittis. Cras a tempor turpis. Etiam luctus
-                tortor a augue tempor, et feugiat augue tempus. Pellentesque vestibulum arcu turpis, a vulputate odio
-                malesuada at. Nulla pharetra nulla semper laoreet aliquet. Vestibulum eget interdum neque, ut commodo
-                magna. Etiam non dolor facilisis, porttitor sapien consectetur, lobortis odio. Phasellus ultrices sem ut
-                enim rutrum rutrum. Etiam pretium sollicitudin dolor ac rhoncus. Etiam sit amet dignissim odio. Aliquam
-                sodales velit dapibus, condimentum massa vel, sodales odio. Aliquam in dolor maximus, scelerisque velit
-                id, eleifend nisl. Nullam vitae erat mollis, vestibulum tellus quis, laoreet orci. Donec tellus mauris,
-                sodales et velit sit amet, lacinia interdum felis. Quisque eros ex, ultricies non sapien ultrices,
-                pellentesque euismod nulla. Nunc odio felis, facilisis at tincidunt et, sodales pulvinar leo.
-                Pellentesque at nulla massa. Nulla ullamcorper accumsan viverra. Pellentesque enim erat, pretium viverra
-                velit ut, accumsan mattis nunc. Nulla at mauris sem. Maecenas placerat tellus a dui malesuada feugiat.
-                Nullam commodo egestas arcu fringilla porttitor. Sed ex nisl, scelerisque eu efficitur in, pharetra at
-                ipsum. Sed ultrices nulla eget leo rhoncus tempor. Cras erat nulla, blandit vel iaculis eget, gravida in
-                lacus. Mauris turpis ipsum, sollicitudin ac nunc dignissim, sodales efficitur sapien.{' '}
-            </p>
+            <div className="my-8">
+                <h1 className="text-grey-900 font-sans text-5xl font-normal">Welcome to 0x.</h1>
+            </div>
+            <div className="bg-grey-100 flex justify-between rounded-2xl p-8">
+                <div>
+                    <h2 className="text-grey-900 text-2.5xl mb-4 font-sans">Build a live app</h2>
+                    <p className="text-grey-400 mb-4 max-w-[266px] font-sans text-base leading-5">
+                        Create an app to get a live API key with access to multiple 0x products.
+                    </p>
+                    <Button size="sm" className="ml-auto" roundness="lg">
+                        Create an app
+                    </Button>
+                </div>
+                <div className="grid max-w-[890px] grid-cols-3 gap-x-12">
+                    <div>
+                        <h2 className="text-grey-900 text-2.5xl mb-4 font-sans">Test API key</h2>
+                        <p className="text-grey-400 mb-4 font-sans text-base leading-5">
+                            Make a sample request to any 0x product with the key below.
+                        </p>
+                        <div>Test API key</div>
+                    </div>
+                    <div className="col-span-2">Code block</div>
+                </div>
+            </div>
         </div>
     );
 }
