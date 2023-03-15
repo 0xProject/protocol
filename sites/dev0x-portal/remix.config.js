e7df006895f79b5cd6da38e9d41d12cf31d7f77c
Wojtek <xsohai@gmail.com>
2023-03-15 13:40:01 +0100

    Remove deprecated remix config option (#166)

--- sites/dev0x-portal/remix.config.js
+++ sites/dev0x-portal/remix.config.js
@@ -1,6 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-    serverBuildTarget: 'vercel',
     // When running locally in development mode, we use the built in remix
     // server. This does not understand the vercel lambda module format,
     // so we default back to the standard build output.
@@ -13,6 +12,6 @@ module.exports = {
     },
     // appDirectory: "app",
     // assetsBuildDirectory: "public/build",
-    // serverBuildPath: "api/index.js",
+    serverBuildPath: 'api/index.js',
     // publicPath: "/build/",
 };
