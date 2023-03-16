8de0b6d2e63e7f7acea5bda70858f13ad755434b
Elyas <35933488+eobbad@users.noreply.github.com>
2023-03-16 16:57:28 -0400

    reenable curve pools [LIT-920] (#207)

--- apps-node/api/src/asset-swapper/utils/market_operation_utils/curve.ts
+++ apps-node/api/src/asset-swapper/utils/market_operation_utils/curve.ts
@@ -533,6 +533,166 @@ export const CURVE_MAINNET_INFOS: { [name: string]: CurveInfo } = {
         pool: CURVE_POOLS.DOLAFRAX,
         gasSchedule: 260e3,
     }),
+    '0x447ddd4960d9fdbf6af9a790560d0af76795cb08': createCurveExchangePool({
+        tokens: ['0xae78736cd615f374d3085123a210448e74fc6393', '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0'],
+        pool: '0x447ddd4960d9fdbf6af9a790560d0af76795cb08',
+        gasSchedule: 300000,
+    }),
+    '0xc250b22d15e43d95fbe27b12d98b6098f8493eac': createCurveExchangePool({
+        tokens: ['0x808d3e6b23516967ceae4f17a5f9038383ed5311', '0xc770eefad204b5180df6a14ee197d99d808ee52d'],
+        pool: '0xc250b22d15e43d95fbe27b12d98b6098f8493eac',
+        gasSchedule: 315000,
+    }),
+    '0x9001a452d39a8710d27ed5c2e10431c13f5fba74': createCurveExchangePool({
+        tokens: ['0xd3b5d9a561c293fb42b446fe7e237daa9bf9aa84', '0xdbdb4d16eda451d0503b854cf79d55697f90c8df'],
+        pool: '0x9001a452d39a8710d27ed5c2e10431c13f5fba74',
+        gasSchedule: 310000,
+    }),
+    '0x961226b64ad373275130234145b96d100dc0b655': createCurveExchangePool({
+        tokens: ['0xadf15ec41689fc5b6dca0db7c53c9bfe7981e655', '0x3432b6a60d23ca0dfca7761b7ab56459d9c964d0'],
+        pool: '0x961226b64ad373275130234145b96d100dc0b655',
+        gasSchedule: 390000,
+    }),
+    '0x8c524635d52bd7b1bd55e062303177a7d916c046': createCurveExchangePool({
+        tokens: ['0x3432b6a60d23ca0dfca7761b7ab56459d9c964d0', '0x402f878bdd1f5c66fdaf0fababcf74741b68ac36'],
+        pool: '0x8c524635d52bd7b1bd55e062303177a7d916c046',
+        gasSchedule: 410000,
+    }),
+    '0x48ff31bbbd8ab553ebe7cbd84e1ea3dba8f54957': createCurveExchangePool({
+        tokens: ['0x31429d1856ad1377a8a0079410b297e1a9e214c2', '0x752b4c6e92d96467fe9b9a2522ef07228e00f87c'],
+        pool: '0x48ff31bbbd8ab553ebe7cbd84e1ea3dba8f54957',
+        gasSchedule: 300000,
+    }),
+    '0xf7b55c3732ad8b2c2da7c24f30a69f55c54fb717': createCurveExchangePool({
+        tokens: ['0xd533a949740bb3306d119cc777fa900ba034cd52', '0xd1b5651e55d4ceed36251c61c50c889b36f6abb5'],
+        pool: '0xf7b55c3732ad8b2c2da7c24f30a69f55c54fb717',
+        gasSchedule: 300000,
+    }),
+    '0x828b154032950c8ff7cf8085d841723db2696056': createCurveExchangePool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0xae7ab96520de3a18e5e111b5eaab095312d7fe84'],
+        pool: '0x828b154032950c8ff7cf8085d841723db2696056',
+        gasSchedule: 355000,
+    }),
+    '0xdadfd00a2bbeb1abc4936b1644a3033e1b653228': createCurveExchangePool({
+        tokens: ['0xbcb8b7fc9197feda75c101fa69d3211b5a30dcd9', '0x6021444f1706f15465bee85463bcc7d7cc17fc03'],
+        pool: '0xdadfd00a2bbeb1abc4936b1644a3033e1b653228',
+        gasSchedule: 280000,
+    }),
+    '0xba3436fd341f2c8a928452db3c5a3670d1d5cc73': createCurveExchangePool({
+        tokens: ['0x1a7e4e63778b4f12a199c062f3efdd288afcbce8', '0x1abaea1f7c830bd89acc67ec4af516284b1bc33c'],
+        pool: '0xba3436fd341f2c8a928452db3c5a3670d1d5cc73',
+        gasSchedule: 315000,
+    }),
+    '0xf9078fb962a7d13f55d40d49c8aa6472abd1a5a6': createCurveExchangePool({
+        tokens: ['0x4e3fbd56cd56c3e72c1403e103b45db9da5b9d2b', '0xf05e58fcea29ab4da01a495140b349f8410ba904'],
+        pool: '0xf9078fb962a7d13f55d40d49c8aa6472abd1a5a6',
+        gasSchedule: 300000,
+    }),
+    '0x0e9b5b092cad6f1c5e6bc7f89ffe1abb5c95f1c2': createCurveFactoryCryptoExchangePool({
+        tokens: ['0xba3335588d9403515223f109edc4eb7269a9ab5d', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'],
+        pool: '0x0e9b5b092cad6f1c5e6bc7f89ffe1abb5c95f1c2',
+        gasSchedule: 510000,
+    }),
+    '0x5b3b5df2bf2b6543f78e053bd91c4bdd820929f1': createCurveExchangePool({
+        tokens: ['0x31d4eb09a216e181ec8a43ce79226a487d6f0ba9', '0x6c3f90f043a72fa612cbac8115ee7e52bde6e490'],
+        pool: '0x5b3b5df2bf2b6543f78e053bd91c4bdd820929f1',
+        gasSchedule: 275000,
+    }),
+    '0xbcb91e689114b9cc865ad7871845c95241df4105': createCurveExchangePool({
+        tokens: ['0xf0a93d4994b3d98fb5e3a2f90dbc2d69073cb86b', '0x6c3f90f043a72fa612cbac8115ee7e52bde6e490'],
+        pool: '0xbcb91e689114b9cc865ad7871845c95241df4105',
+        gasSchedule: 440000,
+    }),
+    '0xfbdca68601f835b27790d98bbb8ec7f05fdeaa9b': createCurveExchangePool({
+        tokens: ['0x8751d4196027d4e6da63716fa7786b5174f04c15', '0x075b1bb99792c9e1041ba13afef80c91a1e70fb3'],
+        pool: '0xfbdca68601f835b27790d98bbb8ec7f05fdeaa9b',
+        gasSchedule: 330000,
+    }),
+    '0x0fafafd3c393ead5f5129cfc7e0e12367088c473': createCurveExchangePool({
+        tokens: ['0x7945b0a6674b175695e5d1d08ae1e6f13744abb0', '0x6c3f90f043a72fa612cbac8115ee7e52bde6e490'],
+        pool: '0x0fafafd3c393ead5f5129cfc7e0e12367088c473',
+        gasSchedule: 280000,
+    }),
+    '0xe6b5cc1b4b47305c58392ce3d359b10282fc36ea': createCurveExchangePool({
+        tokens: ['0x0c10bf8fcb7bf5412187a595ab97a3609160b5c6', '0x6c3f90f043a72fa612cbac8115ee7e52bde6e490'],
+        pool: '0xe6b5cc1b4b47305c58392ce3d359b10282fc36ea',
+        gasSchedule: 270000,
+    }),
+    '0xe3c190c57b5959ae62efe3b6797058b76ba2f5ef': createCurveExchangePool({
+        tokens: ['0x57ab1ec28d129707052df4df418d58a2d46d5f51', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0xe3c190c57b5959ae62efe3b6797058b76ba2f5ef',
+        gasSchedule: 400000,
+    }),
+    '0x497ce58f34605b9944e6b15ecafe6b001206fd25': createCurveExchangePool({
+        tokens: ['0x5f98805a4e8be255a32880fdec7f6728c6568ba0', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x497ce58f34605b9944e6b15ecafe6b001206fd25',
+        gasSchedule: 270000,
+    }),
+    '0x04b727c7e246ca70d496ecf52e6b6280f3c8077d': createCurveExchangePool({
+        tokens: ['0xff709449528b6fb6b88f557f7d93dece33bca78d', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x04b727c7e246ca70d496ecf52e6b6280f3c8077d',
+        gasSchedule: 270000,
+    }),
+    '0x4e43151b78b5fbb16298c1161fcbf7531d5f8d93': createCurveExchangePool({
+        tokens: ['0x056fd409e1d7a124bd7017459dfea2f387b6d5cd', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x4e43151b78b5fbb16298c1161fcbf7531d5f8d93',
+        gasSchedule: 300000,
+    }),
+    '0x33baeda08b8afacc4d3d07cf31d49fc1f1f3e893': createCurveExchangePool({
+        tokens: ['0x0000000000085d4780b73119b644ae5ecd22b376', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x33baeda08b8afacc4d3d07cf31d49fc1f1f3e893',
+        gasSchedule: 280000,
+    }),
+    '0xb30da2376f63de30b42dc055c93fa474f31330a5': createCurveExchangePool({
+        tokens: ['0xbc6da0fe9ad5f3b0d58160288917aa56653660e9', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0xb30da2376f63de30b42dc055c93fa474f31330a5',
+        gasSchedule: 265000,
+    }),
+    '0xc9c32cd16bf7efb85ff14e0c8603cc90f6f2ee49': createCurveExchangePool({
+        tokens: ['0xbea0000029ad1c77d3d5d23ba2d8893db9d1efab', '0x6c3f90f043a72fa612cbac8115ee7e52bde6e490'],
+        pool: '0xc9c32cd16bf7efb85ff14e0c8603cc90f6f2ee49',
+        gasSchedule: 300000,
+    }),
+    '0x66e335622ad7a6c9c72c98dbfcce684996a20ef9': createCurveExchangePool({
+        tokens: ['0x8d6cebd76f18e1558d4db88138e2defb3909fad6', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x66e335622ad7a6c9c72c98dbfcce684996a20ef9',
+        gasSchedule: 265000,
+    }),
+    '0x326290a1b0004eee78fa6ed4f1d8f4b2523ab669': createCurveExchangePool({
+        tokens: ['0xd7c9f0e536dc865ae858b0c0453fe76d13c3beac', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x326290a1b0004eee78fa6ed4f1d8f4b2523ab669',
+        gasSchedule: 265000,
+    }),
+    '0x84c333e94aea4a51a21f6cf0c7f528c50dc7592c': createCurveExchangePool({
+        tokens: ['0x3c20ac688410be8f391be1fb00afc5c212972f86', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x84c333e94aea4a51a21f6cf0c7f528c50dc7592c',
+        gasSchedule: 265000,
+    }),
+    '0x2863a328a0b7fc6040f11614fa0728587db8e353': createCurveExchangePool({
+        tokens: ['0x66eff5221ca926636224650fd3b9c497ff828f7d', '0x051d7e5609917bd9b73f04bac0ded8dd46a74301'],
+        pool: '0x2863a328a0b7fc6040f11614fa0728587db8e353',
+        gasSchedule: 260000,
+    }),
+    '0xf95aaa7ebb1620e46221b73588502960ef63dba0': createCurveExchangePool({
+        tokens: ['0x18084fba666a33d37592fa2633fd49a74dd93a88', '0x051d7e5609917bd9b73f04bac0ded8dd46a74301'],
+        pool: '0xf95aaa7ebb1620e46221b73588502960ef63dba0',
+        gasSchedule: 265000,
+    }),
+    '0xc3b19502f8c02be75f3f77fd673503520deb51dd': createCurveExchangePool({
+        tokens: ['0xab5eb14c09d416f0ac63661e57edb7aecdb9befa', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0xc3b19502f8c02be75f3f77fd673503520deb51dd',
+        gasSchedule: 270000,
+    }),
+    '0xe9123cbc5d1ea65301d417193c40a72ac8d53501': createCurveExchangePool({
+        tokens: ['0x94a18d9fe00bab617fad8b49b11e9f1f64db6b36', '0x6c3f90f043a72fa612cbac8115ee7e52bde6e490'],
+        pool: '0xe9123cbc5d1ea65301d417193c40a72ac8d53501',
+        gasSchedule: 275000,
+    }),
+    '0xaeda92e6a3b1028edc139a4ae56ec881f3064d4f': createCurveExchangePool({
+        tokens: ['0xa0d69e286b938e21cbf7e51d71f6a4c8918f482f', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0xaeda92e6a3b1028edc139a4ae56ec881f3064d4f',
+        gasSchedule: 265000,
+    }),
 };
 
 export const CURVE_V2_MAINNET_INFOS: { [name: string]: CurveInfo } = {
@@ -545,6 +705,160 @@ export const CURVE_V2_MAINNET_INFOS: { [name: string]: CurveInfo } = {
         tokens: [MAINNET_TOKENS.USDT, MAINNET_TOKENS.WBTC, MAINNET_TOKENS.WETH],
         pool: CURVE_V2_POOLS.tricrypto2,
         gasSchedule: 300e3,
+    }),
+    '0xd658a338613198204dca1143ac3f01a722b5d94a': createCurveExchangeV2Pool({
+        tokens: ['0x3432b6a60d23ca0dfca7761b7ab56459d9c964d0', '0xfeef77d3f69374f66429c91d732a244f074bdf74'],
+        pool: '0xd658a338613198204dca1143ac3f01a722b5d94a',
+        gasSchedule: 557341,
+    }),
+    '0x6bfe880ed1d639bf80167b93cc9c56a39c1ba2dc': createCurveExchangeV2Pool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0'],
+        pool: '0x6bfe880ed1d639bf80167b93cc9c56a39c1ba2dc',
+        gasSchedule: 565000,
+    }),
+    '0x9409280dc1e6d33ab7a8c6ec03e5763fb61772b5': createCurveExchangeV2Pool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0x5a98fcbea516cf06857215779fd812ca3bef1b32'],
+        pool: '0x9409280dc1e6d33ab7a8c6ec03e5763fb61772b5',
+        gasSchedule: 530000,
+    }),
+    '0xd51a44d3fae010294c616388b506acda1bfaae46': createCurveExchangeV2Pool({
+        tokens: [
+            '0xdac17f958d2ee523a2206206994597c13d831ec7',
+            '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+            '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+        ],
+        pool: '0xd51a44d3fae010294c616388b506acda1bfaae46',
+        gasSchedule: 460158,
+    }),
+    '0x98a7f18d4e56cfe84e3d081b40001b3d5bd3eb8b': createCurveExchangeV2Pool({
+        tokens: ['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', '0xdb25f211ab05b1c97d595516f45794528a807ad8'],
+        pool: '0x98a7f18d4e56cfe84e3d081b40001b3d5bd3eb8b',
+        gasSchedule: 400000,
+    }),
+    '0x941eb6f616114e4ecaa85377945ea306002612fe': createCurveExchangeV2Pool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0x3432b6a60d23ca0dfca7761b7ab56459d9c964d0'],
+        pool: '0x941eb6f616114e4ecaa85377945ea306002612fe',
+        gasSchedule: 589184,
+    }),
+    '0xfb8814d005c5f32874391e888da6eb2fe7a27902': createCurveExchangeV2Pool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0x73968b9a57c6e53d41345fd57a6e6ae27d6cdb2f'],
+        pool: '0xfb8814d005c5f32874391e888da6eb2fe7a27902',
+        gasSchedule: 400000,
+    }),
+    '0x6ec38b3228251a0c5d491faf66858e2e23d7728b': createCurveExchangeV2Pool({
+        tokens: ['0x64aa3364f17a4d01c6f1751fd97c2bd3d7e7f1d5', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'],
+        pool: '0x6ec38b3228251a0c5d491faf66858e2e23d7728b',
+        gasSchedule: 603747,
+    }),
+    '0x1570af3df649fc74872c5b8f280a162a3bdd4eb6': createCurveExchangeV2Pool({
+        tokens: ['0x96e61422b6a9ba0e068b6c5add4ffabc6a4aae27', '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'],
+        pool: '0x1570af3df649fc74872c5b8f280a162a3bdd4eb6',
+        gasSchedule: 400000,
+    }),
+    '0x3211c6cbef1429da3d0d58494938299c92ad5860': createCurveExchangeV2Pool({
+        tokens: ['0xaf5191b0de278c7286d6c7cc6ab6bb8a73ba2cd6', '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'],
+        pool: '0x3211c6cbef1429da3d0d58494938299c92ad5860',
+        gasSchedule: 400000,
+    }),
+    '0x21410232b484136404911780bc32756d5d1a9fa9': createCurveExchangeV2Pool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44'],
+        pool: '0x21410232b484136404911780bc32756d5d1a9fa9',
+        gasSchedule: 480000,
+    }),
+    '0x838af967537350d2c44abb8c010e49e32673ab94': createCurveExchangeV2Pool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0x9ae380f0272e2162340a5bb646c354271c0f5cfc'],
+        pool: '0x838af967537350d2c44abb8c010e49e32673ab94',
+        gasSchedule: 540000,
+    }),
+    '0xf861483fa7e511fbc37487d91b6faa803af5d37c': createCurveExchangeV2Pool({
+        tokens: ['0x853d955acef822db058eb8505911ed77f175b99e', '0x5ca135cb8527d76e932f34b5145575f9d8cbe08e'],
+        pool: '0xf861483fa7e511fbc37487d91b6faa803af5d37c',
+        gasSchedule: 490000,
+    }),
+    '0xe0e970a99bc4f53804d8145bebbc7ebc9422ba7f': createCurveExchangeV2Pool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0x2e9d63788249371f1dfc918a52f8d799f4a38c94'],
+        pool: '0xe0e970a99bc4f53804d8145bebbc7ebc9422ba7f',
+        gasSchedule: 550000,
+    }),
+    '0x799d141e83d88996c48b98a4f8eb3d96ab422dd3': createCurveExchangeV2Pool({
+        tokens: ['0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0', '0xfd56a3dcfc0690881a466ae432d71bb2db588083'],
+        pool: '0x799d141e83d88996c48b98a4f8eb3d96ab422dd3',
+        gasSchedule: 550000,
+    }),
+    '0x6e314039f4c56000f4ebb3a7854a84cc6225fb92': createCurveExchangeV2Pool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0xc55126051b22ebb829d00368f4b12bde432de5da'],
+        pool: '0x6e314039f4c56000f4ebb3a7854a84cc6225fb92',
+        gasSchedule: 430000,
+    }),
+    '0x5fae7e604fc3e24fd43a72867cebac94c65b404a': createCurveExchangeV2Pool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0xbe9895146f7af43049ca1c1ae358b0541ea49704'],
+        pool: '0x5fae7e604fc3e24fd43a72867cebac94c65b404a',
+        gasSchedule: 562744,
+    }),
+    '0x58257e4291f95165184b4bea7793a1d6f8e7b627': createCurveExchangeV2Pool({
+        tokens: ['0x1a7e4e63778b4f12a199c062f3efdd288afcbce8', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x58257e4291f95165184b4bea7793a1d6f8e7b627',
+        gasSchedule: 370000,
+    }),
+    '0x21d158d95c2e150e144c36fc64e3653b8d6c6267': createCurveExchangeV2Pool({
+        tokens: ['0xfeef77d3f69374f66429c91d732a244f074bdf74', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x21d158d95c2e150e144c36fc64e3653b8d6c6267',
+        gasSchedule: 480000,
+    }),
+    '0xbec570d92afb7ffc553bdd9d4b4638121000b10d': createCurveExchangeV2Pool({
+        tokens: ['0x4e3fbd56cd56c3e72c1403e103b45db9da5b9d2b', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0xbec570d92afb7ffc553bdd9d4b4638121000b10d',
+        gasSchedule: 455000,
+    }),
+    '0x4149d1038575ce235e03e03b39487a80fd709d31': createCurveExchangeV2Pool({
+        tokens: ['0xdbdb4d16eda451d0503b854cf79d55697f90c8df', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x4149d1038575ce235e03e03b39487a80fd709d31',
+        gasSchedule: 380000,
+    }),
+    '0x31c325a01861c7dbd331a9270296a31296d797a0': createCurveExchangeV2Pool({
+        tokens: ['0x62b9c7356a2dc64a1969e19c23e4f579f9810aa7', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x31c325a01861c7dbd331a9270296a31296d797a0',
+        gasSchedule: 450000,
+    }),
+    '0x13b876c26ad6d21cb87ae459eaf6d7a1b788a113': createCurveExchangeV2Pool({
+        tokens: ['0x3472a5a71965499acd81997a54bba8d852c6e53d', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x13b876c26ad6d21cb87ae459eaf6d7a1b788a113',
+        gasSchedule: 600000,
+    }),
+    '0xdcb11e81c8b8a1e06bf4b50d4f6f3bb31f7478c3': createCurveExchangeV2Pool({
+        tokens: ['0x045da4bfe02b320f4403674b3b7d121737727a36', '0x6c3f90f043a72fa612cbac8115ee7e52bde6e490'],
+        pool: '0xdcb11e81c8b8a1e06bf4b50d4f6f3bb31f7478c3',
+        gasSchedule: 530000,
+    }),
+    '0x6a6283ab6e31c2aec3fa08697a8f806b740660b2': createCurveExchangeV2Pool({
+        tokens: ['0x320623b8e4ff03373931769a31fc52a4e78b5d70', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x6a6283ab6e31c2aec3fa08697a8f806b740660b2',
+        gasSchedule: 582923,
+    }),
+    '0x342d1c4aa76ea6f5e5871b7f11a019a0eb713a4f': createCurveExchangeV2Pool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0x72953a5c32413614d24c29c84a66ae4b59581bbf'],
+        pool: '0x342d1c4aa76ea6f5e5871b7f11a019a0eb713a4f',
+        gasSchedule: 457316,
+    }),
+    '0x808db6e464279c6a77a1164e0b34d64bd6fb526e': createCurveExchangeV2Pool({
+        tokens: ['0xe80c0cd204d654cebe8dd64a4857cab6be8345a3', '0x836a808d4828586a69364065a1e064609f5078c7'],
+        pool: '0x808db6e464279c6a77a1164e0b34d64bd6fb526e',
+        gasSchedule: 550000,
+    }),
+    '0xfc1e8bf3e81383ef07be24c3fd146745719de48d': createCurveExchangeV2Pool({
+        tokens: ['0x64aa3364f17a4d01c6f1751fd97c2bd3d7e7f1d5', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0xfc1e8bf3e81383ef07be24c3fd146745719de48d',
+        gasSchedule: 350000,
+    }),
+    '0x867fe27fc2462cff8890b54dfd64e6d42a9d1ac8': createCurveExchangeV2Pool({
+        tokens: ['0xaf5191b0de278c7286d6c7cc6ab6bb8a73ba2cd6', '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc'],
+        pool: '0x867fe27fc2462cff8890b54dfd64e6d42a9d1ac8',
+        gasSchedule: 500000,
+    }),
+    '0x0f3159811670c117c372428d4e69ac32325e4d0f': createCurveExchangeV2Pool({
+        tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', '0xae78736cd615f374d3085123a210448e74fc6393'],
+        pool: '0x0f3159811670c117c372428d4e69ac32325e4d0f',
+        gasSchedule: 440000,
     }),
 };
 
