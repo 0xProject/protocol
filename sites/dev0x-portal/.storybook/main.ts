94fd4a8bb8ffa9cc1fa614f391d568de1e1a0adb
Dennis Simon <dennism.simon94@gmail.com>
2023-03-21 20:03:49 +0100

    [dev-portal] fix storybook tailwind class application (#244)

--- sites/dev0x-portal/.storybook/main.ts
+++ sites/dev0x-portal/.storybook/main.ts
@@ -1,32 +1,36 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)", "../app/components/**/*.stories.@(js|jsx|ts|tsx)",],
-  addons: [
-    "@storybook/addon-links",
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        backgrounds: false,
-      },
-    },
-    "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-postcss",
-      options: {
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
+    stories: [
+        '../stories/**/*.mdx',
+        '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+        '../app/components/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        {
+            name: '@storybook/addon-essentials',
+            options: {
+                backgrounds: false,
+            },
         },
-      },
+        '@storybook/addon-interactions',
+        // {
+        //   name: "@storybook/addon-postcss",
+        //   options: {
+        //     postcssLoaderOptions: {
+        //       implementation: require("postcss"),
+        //     },
+        //   },
+        // },
+        // 'storybook-addon-pseudo-states',
+    ],
+    framework: {
+        name: '@storybook/react-vite',
+        options: {},
     },
-    "storybook-addon-pseudo-states"
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
-  staticDirs: ["../public"],
+    docs: {
+        autodocs: 'tag',
+    },
+    staticDirs: ['../public'],
 };
 export default config;
