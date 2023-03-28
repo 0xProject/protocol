b8f09536a8ea94fbe1c1d9c721983818ab5b3b55
elenadimitrova <elena@arenabg.com>
2023-03-28 11:12:47 +0300

    Switch to a production version of predicting a deployment address

--- contracts/governance/test/BaseTest.t.sol
+++ contracts/governance/test/BaseTest.t.sol
@@ -164,4 +164,64 @@ contract BaseTest is Test {
                 )
             );
     }
+
+    // Sourced from https://github.com/grappafinance/core/blob/master/src/test/utils/Utilities.sol
+    function predictAddress(address _origin, uint256 _nonce) public pure returns (address) {
+        if (_nonce == 0x00) {
+            return
+                address(
+                    uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), _origin, bytes1(0x80)))))
+                );
+        }
+        if (_nonce <= 0x7f) {
+            return
+                address(
+                    uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), _origin, uint8(_nonce)))))
+                );
+        }
+        if (_nonce <= 0xff) {
+            return
+                address(
+                    uint160(
+                        uint256(
+                            keccak256(
+                                abi.encodePacked(bytes1(0xd7), bytes1(0x94), _origin, bytes1(0x81), uint8(_nonce))
+                            )
+                        )
+                    )
+                );
+        }
+        if (_nonce <= 0xffff) {
+            return
+                address(
+                    uint160(
+                        uint256(
+                            keccak256(
+                                abi.encodePacked(bytes1(0xd8), bytes1(0x94), _origin, bytes1(0x82), uint16(_nonce))
+                            )
+                        )
+                    )
+                );
+        }
+        if (_nonce <= 0xffffff) {
+            return
+                address(
+                    uint160(
+                        uint256(
+                            keccak256(
+                                abi.encodePacked(bytes1(0xd9), bytes1(0x94), _origin, bytes1(0x83), uint24(_nonce))
+                            )
+                        )
+                    )
+                );
+        }
+        return
+            address(
+                uint160(
+                    uint256(
+                        keccak256(abi.encodePacked(bytes1(0xda), bytes1(0x94), _origin, bytes1(0x84), uint32(_nonce)))
+                    )
+                )
+            );
+    }
 }
