1b47b46a62577b0a15ed729aefb93b349864f46b
Kyu <kyuhyun217@gmail.com>
2023-04-04 09:09:42 -0700

    [pool-cache] Implement initial env var configuration/validation (#340)

---
+++ apps-node/pool-cache/src/env.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+    POOL_CACHE_PORT: z.coerce.number().positive().default(3001),
+    REDIS_URL: z.string(),
+    // RPC URLs
+    ETHEREUM_RPC_URL: z.string().url().optional(),
+    POLYGON_RPC_URL: z.string().url().optional(),
+    ARBITRUM_RPC_URL: z.string().url().optional(),
+    // TODO: add more RPC URLs.
+});
+
+const parsed = envSchema.safeParse(process.env);
+
+if (!parsed.success) {
+    console.error('Invalid environment variables:', JSON.stringify(parsed.error.format(), null, 4));
+    process.exit(1);
+}
+
+if (parsed.data.NODE_ENV === 'production') {
+    const { ETHEREUM_RPC_URL, POLYGON_RPC_URL, ARBITRUM_RPC_URL } = parsed.data;
+    if ([ETHEREUM_RPC_URL, POLYGON_RPC_URL, ARBITRUM_RPC_URL].find((url) => url === undefined) !== undefined) {
+        console.error('All RPC URLs are required in production', JSON.stringify(parsed.data, null, 4));
+        process.exit(1);
+    }
+}
+
+if (parsed.data.NODE_ENV !== 'production') {
+    console.log('configured environment: ', JSON.stringify(parsed.data, null, 4));
+}
+
+export const env = parsed.data;
--- apps-node/pool-cache/src/main.ts
+++ apps-node/pool-cache/src/main.ts
@@ -4,22 +4,32 @@ import { RedisCacheClient } from './cache/redis-cache-client';
 import { EthCallPoolFetcher } from './pool-fetcher/eth-call-pool-fetcher';
 import { createPoolCacheRouter } from './routers';
 import { PoolCacheService } from './services/pool-cache-service';
+import { ChainId } from './utils/constants';
+import { env } from './env';
+
+function getRpcUrlMap(): Map<number, string> {
+    const entries: [number, string | undefined][] = [
+        [ChainId.Ethereum, env.ETHEREUM_RPC_URL],
+        [ChainId.Polygon, env.POLYGON_RPC_URL],
+        [ChainId.Arbitrum, env.ARBITRUM_RPC_URL],
+    ];
+
+    const isUrlDefined = (tuple: [number, string | undefined]): tuple is [number, string] => {
+        const [, url] = tuple;
+        return url !== undefined;
+    };
+
+    return Map(entries.filter(isUrlDefined));
+}
 
 createHTTPServer({
     router: createPoolCacheRouter(
         new PoolCacheService({
-            poolFetcher: new EthCallPoolFetcher(
-                // TODO: read from env and also verify
-                Map([
-                    [1, 'https://mainnet.infura.io/v3/1468c2ecb7f04f84a023cb71f03964c7'],
-                    [137, 'https://polygon-mainnet.infura.io/v3/1468c2ecb7f04f84a023cb71f03964c7'],
-                ]),
-            ),
-            // TODO: read from env
-            cacheClient: new RedisCacheClient('//0.0.0.0:0101'),
+            poolFetcher: new EthCallPoolFetcher(getRpcUrlMap()),
+            cacheClient: new RedisCacheClient(env.REDIS_URL),
         }),
     ),
     createContext() {
         return {};
     },
-}).listen(3000);
+}).listen(env.POOL_CACHE_PORT);
