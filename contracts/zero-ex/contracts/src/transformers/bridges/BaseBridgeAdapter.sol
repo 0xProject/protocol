047ab62891b6bf8ffa981e77a7d2bc8225e00842
Duncan Townsend <git@duncancmt.com>
2023-07-13 15:36:42 -0400

    WIP: BaseBridgeAdapter

---
+++ contracts/zero-ex/contracts/src/transformers/bridges/BaseBridgeAdapter.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+  Copyright 2023 ZeroEx Intl.
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+pragma solidity ^0.6.5;
+pragma experimental ABIEncoderV2;
+
+import "./AbstractBridgeAdapter.sol";
+import "./BridgeProtocols.sol";
+import "./mixins/MixinUniswapV3.sol";
+import "./mixins/MixinUniswapV2.sol";
+import "./mixins/MixinBalancer.sol";
+import "./mixins/MixinBalancerV2Batch.sol";
+import "./mixins/MixinCurve.sol";
+import "./mixins/MixinCurveV2.sol";
+
+contract BaseBridgeAdapter is
+    AbstractBridgeAdapter(8453, "Base"),
+    MixinUniswapV3,
+    MixinUniswapV2,
+    MixinBalancer,
+    MixinBalancerV2Batch,
+    MixinCurve,
+    MixinCurveV2
+{
+    function _trade(
+        BridgeOrder memory order,
+        IERC20Token sellToken,
+        IERC20Token buyToken,
+        uint256 sellAmount,
+        bool dryRun
+    ) internal override returns (uint256 boughtAmount, bool supportedSource) {
+        uint128 protocolId = uint128(uint256(order.source) >> 128);
+        if (protocolId == BridgeProtocols.CURVE) {
+            if (dryRun) {
+                return (0, true);
+            }
+            boughtAmount = _tradeCurve(sellToken, buyToken, sellAmount, order.bridgeData);
+        } else if (protocolId == BridgeProtocols.CURVEV2) {
+            if (dryRun) {
+                return (0, true);
+            }
+            boughtAmount = _tradeCurveV2(sellToken, buyToken, sellAmount, order.bridgeData);
+        } else if (protocolId == BridgeProtocols.UNISWAPV3) {
+            if (dryRun) {
+                return (0, true);
+            }
+            boughtAmount = _tradeUniswapV3(sellToken, sellAmount, order.bridgeData);
+        } else if (protocolId == BridgeProtocols.UNISWAPV2) {
+            if (dryRun) {
+                return (0, true);
+            }
+            boughtAmount = _tradeUniswapV2(buyToken, sellAmount, order.bridgeData);
+        } else if (protocolId == BridgeProtocols.BALANCER) {
+            if (dryRun) {
+                return (0, true);
+            }
+            boughtAmount = _tradeBalancer(sellToken, buyToken, sellAmount, order.bridgeData);
+        } else if (protocolId == BridgeProtocols.BALANCERV2BATCH) {
+            if (dryRun) {
+                return (0, true);
+            }
+            boughtAmount = _tradeBalancerV2Batch(sellAmount, order.bridgeData);
+        }
+        emit BridgeFill(order.source, sellToken, buyToken, sellAmount, boughtAmount);
+    }
+}
