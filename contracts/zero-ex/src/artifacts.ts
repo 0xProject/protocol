defdff460ad94b7bad98f7a9b1e59df60ef44334
duncancmt <1207590+duncancmt@users.noreply.github.com>
2023-07-18 17:21:43 -0400

    Base mainnet (#734)
    * Add BaseGoerliBridgeAdapter
    
    * Ignore Emacs files
    
    * Remove Balancer V1; add Solidly to BaseBridgeAdapter
    
    * Update addresses for both Base Mainnet and Goerli
    
    * Remove Base Goerli testnet
    
    * Prettier, CHANGELOG, and lowercasing addresses
    
    ---------
    
    Co-authored-by: Savarn Dontamsetti <sav.dontamsetti@gmail.com>

--- contracts/zero-ex/src/artifacts.ts
+++ contracts/zero-ex/src/artifacts.ts
@@ -7,6 +7,8 @@ import { ContractArtifact } from 'ethereum-types';
 
 import * as AffiliateFeeTransformer from '../generated-artifacts/AffiliateFeeTransformer.json';
 import * as AvalancheBridgeAdapter from '../generated-artifacts/AvalancheBridgeAdapter.json';
+import * as BaseBridgeAdapter from '../generated-artifacts/BaseBridgeAdapter.json';
+import * as BaseGoerliBridgeAdapter from '../generated-artifacts/BaseGoerliBridgeAdapter.json';
 import * as BatchFillNativeOrdersFeature from '../generated-artifacts/BatchFillNativeOrdersFeature.json';
 import * as BSCBridgeAdapter from '../generated-artifacts/BSCBridgeAdapter.json';
 import * as CeloBridgeAdapter from '../generated-artifacts/CeloBridgeAdapter.json';
@@ -79,6 +81,8 @@ export const artifacts = {
     OtcOrdersFeature: OtcOrdersFeature as ContractArtifact,
     IOtcOrdersFeature: IOtcOrdersFeature as ContractArtifact,
     AvalancheBridgeAdapter: AvalancheBridgeAdapter as ContractArtifact,
+    BaseGoerliBridgeAdapter: BaseGoerliBridgeAdapter as ContractArtifact,
+    BaseBridgeAdapter: BaseBridgeAdapter as ContractArtifact,
     BSCBridgeAdapter: BSCBridgeAdapter as ContractArtifact,
     CeloBridgeAdapter: CeloBridgeAdapter as ContractArtifact,
     EthereumBridgeAdapter: EthereumBridgeAdapter as ContractArtifact,
